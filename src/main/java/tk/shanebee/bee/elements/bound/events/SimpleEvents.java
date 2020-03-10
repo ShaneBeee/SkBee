8233e905354f62dabfd687f6013cb4048a4f2081
ShaneBee <shanebolenback@me.com>
2020-03-10 12:23:26 -0700

    SimpleEvents - change - Changed versions (forgot to do this before)

--- src/main/java/tk/shanebee/bee/elements/bound/events/SimpleEvents.java
+++ src/main/java/tk/shanebee/bee/elements/bound/events/SimpleEvents.java
@@ -15,7 +15,7 @@ public class SimpleEvents {
         Skript.registerEvent("Bound - Enter", SimpleEvent.class, EnterBoundEvent.class, "bound enter")
                 .description("Called when a player enters a bound")
                 .examples("on bound enter:", "\tif event-bound = {bounds::spawn}:", "\t\tsend \"You entered spawn!\"")
-                .since("2.7.0");
+                .since("1.0.0");
         EventValues.registerEventValue(EnterBoundEvent.class, Player.class, new Getter<Player, EnterBoundEvent>() {
             @Override
             public Player get(EnterBoundEvent event) {
@@ -33,7 +33,7 @@ public class SimpleEvents {
         Skript.registerEvent("Bound - Exit", SimpleEvent.class, ExitBoundEvent.class, "bound exit")
                 .description("Called when a player exits a bound")
                 .examples("on bound exit:", "\tsend \"You left a bound\"", "\tif event-bound = {bound}:", "\t\tsend \"You left Spawn!\"")
-                .since("2.7.0");
+                .since("1.0.0");
         EventValues.registerEventValue(ExitBoundEvent.class, Player.class, new Getter<Player, ExitBoundEvent>() {
             @Override
             public Player get(ExitBoundEvent event) {
