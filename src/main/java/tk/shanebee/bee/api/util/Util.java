e35d84a43fe5f34173e66c5ba361631a21681afa
ShaneBee <shanebolenback@me.com>
2020-03-28 21:54:31 -0700

    Util - changes - change skript error prefix

--- src/main/java/tk/shanebee/bee/api/util/Util.java
+++ src/main/java/tk/shanebee/bee/api/util/Util.java
@@ -1,12 +1,13 @@
 package tk.shanebee.bee.api.util;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.log.ErrorQuality;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class Util {
 
-    private static final String PREFIX = "&7[&bSk&3Bee&7] ";
+    private static final String PREFIX = "&7[&bSk&3Bee &cERROR&7] ";
 
     public static String getColString(String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
@@ -17,7 +18,7 @@ public class Util {
     }
 
     public static void skriptError(String error) {
-        Skript.error(getColString(PREFIX + "&7[&4ERROR&7] &4" + error));
+        Skript.error(getColString(PREFIX + error), ErrorQuality.SEMANTIC_ERROR);
     }
 
 }
