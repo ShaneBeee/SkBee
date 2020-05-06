f648e4f3c25df441280294600e8025355712123d
ShaneBeee <shanebolenback@me.com>
2020-05-06 04:46:57 -0700

    Add armor change event and expression for items

---
+++ src/main/java/tk/shanebee/bee/elements/other/events/PaperEvents.java
@@ -0,0 +1,21 @@
+package tk.shanebee.bee.elements.other.events;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.util.SimpleEvent;
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+
+public class PaperEvents {
+
+    // 1.12.2
+    static {
+        if (Skript.classExists("com.destroystokyo.paper.event.player.PlayerArmorChangeEvent")) {
+            Skript.registerEvent("Armor Change Event", SimpleEvent.class, PlayerArmorChangeEvent.class, "player change armor")
+                    .description("Called when the player themselves change their armor items. Requires Paper 1.12.2+")
+                    .examples("on player change armor:",
+                            "\tcancel event",
+                            "\tset helmet of player to pumpkin")
+                    .since("INSERT VERSION");
+        }
+    }
+
+}
