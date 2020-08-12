17cfa2111cc6ff50849ed277f557f04b6038e012
Shane Bee <shanebolenback@me.com>
2020-08-12 00:03:08 -0700

    Board - add some javadocs and warnings and stuff

--- src/main/java/tk/shanebee/bee/elements/board/objects/Board.java
+++ src/main/java/tk/shanebee/bee/elements/board/objects/Board.java
@@ -11,25 +11,56 @@ import tk.shanebee.bee.SkBee;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a team based scoreboard for a player
+ * <p>This class also has a map that holds all player scoreboards</p>
+ */
 public class Board {
 
     // STATIC STUFF
     private static final Map<Player, Board> BOARD_MAP = new HashMap<>();
 
+    /**
+     * Get the scoreboard for a specific player
+     *
+     * @param player Player to grab scoreboard for
+     * @return Scoreboard of player
+     */
     public static Board getBoard(Player player) {
         return BOARD_MAP.get(player);
     }
 
+    /**
+     * Create a scoreboard for a player
+     * <p>Useful in a join event</p>
+     *
+     * @param player Player to create scoreboard for
+     */
     public static void createBoard(Player player) {
         Board board = new Board(player, false);
         BOARD_MAP.put(player, board);
     }
 
+    /**
+     * Remove a scoreboard for a player
+     * <p>Useful when the player leaves the server</p>
+     *
+     * @param player Player to remove scoreboard for
+     */
     public static void removeBoard(Player player) {
+        if (BOARD_MAP.containsKey(player)) {
+            BOARD_MAP.get(player).clearBoard();
+        }
         BOARD_MAP.remove(player);
     }
 
+    /**
+     * Clear and remove all scoreboards
+     */
     public static void clearBoards() {
+        for (Board board : BOARD_MAP.values()) {
+            board.clearBoard();
+        }
         BOARD_MAP.clear();
     }
 
@@ -52,43 +83,74 @@ public class Board {
             board.setDisplaySlot(DisplaySlot.SIDEBAR);
             board.setDisplayName(" ");
 
-            for (int i = 0; i <15; i++) {
+            for (int i = 0; i < 15; i++) {
                 lines[i] = scoreboard.registerNewTeam("line" + (i + 1));
             }
 
-            for (int i = 0; i <15; i++) {
+            for (int i = 0; i < 15; i++) {
                 lines[i].addEntry(getColString(entries[i]));
             }
         } else {
             scoreboard = player.getScoreboard();
             board = scoreboard.getObjective("Board");
 
-            for (int i = 0; i <15; i++) {
+            for (int i = 0; i < 15; i++) {
                 lines[i] = scoreboard.getTeam("line" + (i + 1));
             }
         }
     }
 
+    /**
+     * Set the title of this scoreboard
+     *
+     * @param title Title to set
+     */
     public void setTitle(String title) {
         board.setDisplayName(getColString(title));
     }
 
+    /**
+     * Set a specific line for this scoreboard
+     * <p>Lines 1 - 15</p>
+     *
+     * @param line Line to set (1 - 15)
+     * @param text Text to put in line
+     */
     public void setLine(int line, String text) {
+        if (line > 15 || line < 1)
+            throw new IllegalArgumentException("Scoreboard lines can only be between 1 and 15");
         Team t = lines[line - 1];
         t.setPrefix(getColString(text));
         board.getScore(getColString(entries[line - 1])).setScore(line);
     }
 
+    /**
+     * Delete a line in this scoreboard
+     * <p>Lines 1 - 15</p>
+     *
+     * @param line Line to delete (1 - 15)
+     */
     public void deleteLine(int line) {
+        if (line > 15 || line < 1)
+            throw new IllegalArgumentException("Scoreboard lines can only be between 1 and 15");
         scoreboard.resetScores(getColString(entries[line - 1]));
     }
 
+    /**
+     * Clear all lines of this scoreboard
+     */
     public void clearBoard() {
         for (int i = 1; i < 16; i++) {
             deleteLine(i);
         }
     }
 
+    /**
+     * Toggle this scoreboard on or off
+     * <p>When off, will not be visible to player, but can still update</p>
+     *
+     * @param on Whether on or off
+     */
     public void toggle(boolean on) {
         if (on) {
             player.setScoreboard(this.scoreboard);
@@ -99,6 +161,11 @@ public class Board {
         }
     }
 
+    /**
+     * Check if this scoreboard is on or off
+     *
+     * @return True if on else off
+     */
     public boolean isOn() {
         return this.on;
     }
