e95fb3e084fc3cea6da5808c00ca39d252eee338
Shane Bee <shanebolenback@me.com>
2020-08-27 00:41:36 -0700

    Util - add method for converting UUID to int array (for MC 1.16+)

--- src/main/java/tk/shanebee/bee/api/util/Util.java
+++ src/main/java/tk/shanebee/bee/api/util/Util.java
@@ -5,6 +5,8 @@ import ch.njol.skript.log.ErrorQuality;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
+import java.util.UUID;
+
 public class Util {
 
     private static final String PREFIX = "&7[&bSk&3Bee&7] ";
@@ -20,6 +22,30 @@ public class Util {
 
     public static void skriptError(String error) {
         Skript.error(getColString(PREFIX_ERROR + error), ErrorQuality.SEMANTIC_ERROR);
+    }
+
+    /**
+     * Convert a UUID to an int array
+     * <p>Used for Minecraft 1.16+</p>
+     *
+     * @param uuid String UUID to convert
+     * @return int array from UUID
+     */
+    public static int[] uuidToIntArray(String uuid) {
+        return uuidToIntArray(UUID.fromString(uuid));
+    }
+
+    /**
+     * Convert a UUID to an int array
+     * <p>Used for Minecraft 1.16+</p>
+     *
+     * @param uuid UUID to convert
+     * @return int array from UUID
+     */
+    public static int[] uuidToIntArray(UUID uuid) {
+        long most = uuid.getMostSignificantBits();
+        long least = uuid.getLeastSignificantBits();
+        return new int[]{(int) (most >> 32), (int) most, (int) (least >> 32), (int) least};
     }
 
 }
