e00366f1b4e5e75fcd4de53eb997d05ac6db8ec1
Shane Bee <shanebolenback@me.com>
2020-09-01 14:15:52 -0700

    ChatReflection - changes - Add support for splitting pretty lines - Add support for 1.13.2

--- src/main/java/tk/shanebee/bee/api/reflection/ChatReflection.java
+++ src/main/java/tk/shanebee/bee/api/reflection/ChatReflection.java
@@ -14,23 +14,30 @@ public class ChatReflection {
     private static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
 
     private enum Ver {
-        V_1_14_R1("v1_14_R1", "k"),
-        V_1_15_R1("v1_15_R1", "l"),
-        V_1_16_R1("v1_16_R1", "l"),
-        V_1_16_R2("v1_16_R2", "l");
+        V_1_13_R2("v1_13_R2", "k", "a"),
+        V_1_14_R1("v1_14_R1", "k", "a"),
+        V_1_15_R1("v1_15_R1", "l", "a"),
+        V_1_16_R1("v1_16_R1", "l", "a"),
+        V_1_16_R2("v1_16_R2", "l", "a");
 
         private final String version;
         private final String pretty;
+        private final String prettySplit;
 
-        Ver(String version, String pretty) {
+        Ver(String version, String pretty, String prettySplit) {
             this.version = version;
             this.pretty = pretty;
+            this.prettySplit = prettySplit;
         }
 
-        private static String getPretty() {
+        private static String getPretty(boolean split) {
             for (Ver value : values()) {
                 if (value.version.equalsIgnoreCase(VERSION)) {
-                    return value.pretty;
+                    if (split) {
+                        return value.prettySplit;
+                    } else {
+                        return value.pretty;
+                    }
                 }
             }
             return null;
@@ -44,16 +51,23 @@ public class ChatReflection {
      * @param compound Compound to convert to pretty
      * @return Pretty string of NBTCompound
      */
-    public static String getPrettyNBT(NBTCompound compound) {
-        String prettyM = Ver.getPretty();
+    public static String getPrettyNBT(NBTCompound compound, String split) {
+        String prettyM = Ver.getPretty(split != null);
         if (prettyM == null) return null;
 
         Object nmsNBT = compound.getCompound();
         Class<?> iChatBaseComponent = ReflectionUtils.getNMSClass("IChatBaseComponent");
         Class<?> craftChatMessageClass = ReflectionUtils.getOBCClass("util.CraftChatMessage");
         try {
-            Method prettyMethod = nmsNBT.getClass().getMethod(prettyM);
-            Object prettyComponent = prettyMethod.invoke(nmsNBT);
+            Method prettyMethod;
+            Object prettyComponent;
+            if (split != null) {
+                prettyMethod = nmsNBT.getClass().getMethod(prettyM, String.class, int.class);
+                prettyComponent = prettyMethod.invoke(nmsNBT, split, 0);
+            } else {
+                prettyMethod = nmsNBT.getClass().getMethod(prettyM);
+                prettyComponent = prettyMethod.invoke(nmsNBT);
+            }
             assert craftChatMessageClass != null;
             Method fromComponent = craftChatMessageClass.getMethod("fromComponent", iChatBaseComponent);
 
