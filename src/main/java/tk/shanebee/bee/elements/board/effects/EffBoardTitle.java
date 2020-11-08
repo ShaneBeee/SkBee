a411ab3792054423ee31349a26d8711aacf8f068
ShaneBee <shanebolenback@me.com>
2020-11-07 16:34:21 -0800

    EffBoardTitle - add support for different patterns

--- src/main/java/tk/shanebee/bee/elements/board/effects/EffBoardTitle.java
+++ src/main/java/tk/shanebee/bee/elements/board/effects/EffBoardTitle.java
@@ -7,11 +7,12 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
 import tk.shanebee.bee.elements.board.objects.Board;
 
 @Name("Board - Title")
@@ -23,7 +24,8 @@ public class EffBoardTitle extends Effect {
 
     static {
         Skript.registerEffect(EffBoardTitle.class,
-                "set title of %players%'[s] [score]board[s] to %string%");
+                "set title of %players%'[s] [score]board[s] to %string%",
+                "set title of [score]board[s] of %players% to %string%");
     }
 
     private Expression<Player> players;
@@ -31,14 +33,14 @@ public class EffBoardTitle extends Effect {
 
     @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+    public boolean init(Expression<?> @NotNull [] exprs, int i, @NotNull Kleenean kleenean, @NotNull ParseResult parseResult) {
         players = (Expression<Player>) exprs[0];
         title = (Expression<String>) exprs[1];
         return true;
     }
 
     @Override
-    protected void execute(Event event) {
+    protected void execute(@NotNull Event event) {
         Player[] players = this.players.getArray(event);
         String title = this.title.getSingle(event);
         for (Player player : players) {
@@ -49,7 +51,7 @@ public class EffBoardTitle extends Effect {
     }
 
     @Override
-    public String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(@Nullable Event e, boolean d) {
         return "set title of " + this.players.toString(e, d) + " scoreboard to " + this.title.toString(e, d);
     }
 
