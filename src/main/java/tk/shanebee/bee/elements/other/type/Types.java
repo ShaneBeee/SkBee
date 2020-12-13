ba48475cd0a4997a4729e4a8ec1e74197500dfc4
ShaneBee <shanebolenback@me.com>
2020-12-12 16:19:24 -0800

    Types - fix a small conflict with SkQuery loading the same class - Force load before SkQuery to prevent this

--- src/main/java/tk/shanebee/bee/elements/other/type/Types.java
+++ src/main/java/tk/shanebee/bee/elements/other/type/Types.java
@@ -14,45 +14,52 @@ import org.bukkit.Particle;
 import org.bukkit.Particle.DustOptions;
 import org.jetbrains.annotations.Nullable;
 import tk.shanebee.bee.api.util.ParticleUtil;
+import tk.shanebee.bee.api.util.Util;
 
 public class Types {
 
     static {
         if (Skript.isRunningMinecraft(1, 13)) {
-            Classes.registerClass(new ClassInfo<>(Particle.class, "particle")
-                    .user("particles?")
-                    .name("Particle")
-                    .description("Represents a particle which can be used in the 'Particle Spawn' effect. Some particles require extra data,",
-                            "these are distinguished by their data type within the square brackets.")
-                    .usage(ParticleUtil.getNamesAsString())
-                    .examples("play 1 of soul at location of player",
-                            "play 10 of dust using dustOption(green, 10) at location of player",
-                            "play 3 of item using player's tool at location of player",
-                            "play 1 of block using dirt at location of player")
-                    .since("1.9.0")
-                    .parser(new Parser<Particle>() {
+            // Only register if no other addons have registered this class
+            if (Classes.getExactClassInfo(Particle.class) == null) {
+                Classes.registerClass(new ClassInfo<>(Particle.class, "particle")
+                        .user("particles?")
+                        .name("Particle")
+                        .description("Represents a particle which can be used in the 'Particle Spawn' effect. Some particles require extra data,",
+                                "these are distinguished by their data type within the square brackets.")
+                        .usage(ParticleUtil.getNamesAsString())
+                        .examples("play 1 of soul at location of player",
+                                "play 10 of dust using dustOption(green, 10) at location of player",
+                                "play 3 of item using player's tool at location of player",
+                                "play 1 of block using dirt at location of player")
+                        .since("1.9.0")
+                        .parser(new Parser<Particle>() {
 
-                        @Nullable
-                        @Override
-                        public Particle parse(String s, ParseContext context) {
-                            return ParticleUtil.parse(s.replace(" ", "_"));
-                        }
+                            @Nullable
+                            @Override
+                            public Particle parse(String s, ParseContext context) {
+                                return ParticleUtil.parse(s.replace(" ", "_"));
+                            }
 
-                        @Override
-                        public String toString(Particle particle, int flags) {
-                            return ParticleUtil.getName(particle);
-                        }
+                            @Override
+                            public String toString(Particle particle, int flags) {
+                                return ParticleUtil.getName(particle);
+                            }
 
-                        @Override
-                        public String toVariableNameString(Particle particle) {
-                            return "particle:" + toString(particle, 0);
-                        }
+                            @Override
+                            public String toVariableNameString(Particle particle) {
+                                return "particle:" + toString(particle, 0);
+                            }
 
-                        @Override
-                        public String getVariableNamePattern() {
-                            return "particle://s";
-                        }
-                    }));
+                            @Override
+                            public String getVariableNamePattern() {
+                                return "particle://s";
+                            }
+                        }));
+            } else {
+                Util.log("It looks like another addon registered 'particle' already. ");
+                Util.log("You may have to use their particles in SkBee's 'particle spawn' effect.");
+            }
 
             Classes.registerClass(new ClassInfo<>(DustOptions.class, "dustoption")
                     .name(ClassInfo.NO_DOC).user("dust options?"));
