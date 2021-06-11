d318a766a9b711583fcd6763e4a1dd073cae6139
ShaneBee <shanebolenback@me.com>
2021-06-11 13:53:45 -0700

    McReflection - fix noClip for 1.17

--- src/main/java/tk/shanebee/bee/api/reflection/McReflection.java
+++ src/main/java/tk/shanebee/bee/api/reflection/McReflection.java
@@ -1,5 +1,6 @@
 package tk.shanebee.bee.api.reflection;
 
+import ch.njol.skript.Skript;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -9,29 +10,21 @@ import java.lang.reflect.Method;
 
 public class McReflection {
 
-    public static void setClip(Entity entity, boolean clip) {
-        if (entity == null) return;
-        Object nmsEntity = ReflectionUtils.getNMSHandle(entity);
-        if (nmsEntity == null) return;
-        Class<?> nmsClass = ReflectionUtils.getNMSClass("Entity");
-        if (nmsClass == null) return;
-        ReflectionUtils.setField("noclip", nmsClass, nmsEntity, !clip);
-    }
+    private static final boolean NEW_NMS = Skript.isRunningMinecraft(1, 17);
 
-    @SuppressWarnings("ConstantConditions")
-    public static boolean getClip(Entity entity) {
-        if (entity == null) return false;
-        Object nmsEntity = ReflectionUtils.getNMSHandle(entity);
-        if (nmsEntity == null) return false;
-        Class<?> nmsClass = ReflectionUtils.getNMSClass("Entity");
-        if (nmsClass == null) return false;
-        return !Boolean.parseBoolean(ReflectionUtils.getField("noclip", nmsClass, nmsEntity).toString());
-    }
-
-    private static final Class<?> ChatMessage = ReflectionUtils.getNMSClass("ChatMessage");
-    private static final Method getNMSCopy;
+    private static final Class<?> ENTITY_NMS_CLASS;
+    private static final String NO_CLIP = NEW_NMS ? "P" : "noclip"; // "P" will probably change in future versions
+    private static final Class<?> CHAT_MESSAGE_CLASS;
+    private static final Method GET_NMS_COPY_METHOD;
 
     static {
+        if (NEW_NMS) {
+            ENTITY_NMS_CLASS = ReflectionUtils.getNewNMSClass("net.minecraft.world.entity.Entity");
+            CHAT_MESSAGE_CLASS = ReflectionUtils.getNewNMSClass("net.minecraft.network.chat.ChatMessage");
+        } else {
+            ENTITY_NMS_CLASS = ReflectionUtils.getNMSClass("Entity");
+            CHAT_MESSAGE_CLASS = ReflectionUtils.getNMSClass("ChatMessage");
+        }
         Method getNMSCopy1;
         try {
             Class<?> CraftItemStack = ReflectionUtils.getOBCClass("inventory.CraftItemStack");
@@ -40,11 +33,26 @@ public class McReflection {
         } catch (NoSuchMethodException ignore) {
             getNMSCopy1 = null;
         }
-        getNMSCopy = getNMSCopy1;
+        GET_NMS_COPY_METHOD = getNMSCopy1;
+    }
+
+    public static void setClip(Entity entity, boolean clip) {
+        if (entity == null || ENTITY_NMS_CLASS == null) return;
+        Object nmsEntity = ReflectionUtils.getNMSHandle(entity);
+        if (nmsEntity == null) return;
+        ReflectionUtils.setField(NO_CLIP, ENTITY_NMS_CLASS, nmsEntity, !clip);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public static boolean getClip(Entity entity) {
+        if (entity == null || ENTITY_NMS_CLASS == null) return false;
+        Object nmsEntity = ReflectionUtils.getNMSHandle(entity);
+        if (nmsEntity == null) return false;
+        return !Boolean.parseBoolean(ReflectionUtils.getField(NO_CLIP, ENTITY_NMS_CLASS, nmsEntity).toString());
     }
 
     public static String getTranslateKey(ItemStack itemStack) {
-        if (getNMSCopy == null || ChatMessage == null) return null;
+        if (GET_NMS_COPY_METHOD == null || CHAT_MESSAGE_CLASS == null) return null;
 
         ItemStack itemStackClone = itemStack.clone();
         ItemMeta itemMeta = itemStackClone.getItemMeta();
@@ -53,12 +61,12 @@ public class McReflection {
             itemStackClone.setItemMeta(itemMeta);
 
             try {
-                Object nmsItemStack = getNMSCopy.invoke(null, itemStackClone);
+                Object nmsItemStack = GET_NMS_COPY_METHOD.invoke(null, itemStackClone);
                 Method getName = nmsItemStack.getClass().getMethod("getName");
                 Object name = getName.invoke(nmsItemStack);
 
-                if (ChatMessage.isInstance(name)) {
-                    Method getKey = ChatMessage.getMethod("getKey");
+                if (CHAT_MESSAGE_CLASS.isInstance(name)) {
+                    Method getKey = CHAT_MESSAGE_CLASS.getMethod("getKey");
                     return ((String) getKey.invoke(name));
                 }
             } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ignore) {
