f09994e3e74e5b9d4c228b5aedf4bd979ca4582e
ShaneBee <shanebolenback@me.com>
2021-06-21 21:20:50 -0700

    ExprPrettyNBT - cleanup

--- src/main/java/tk/shanebee/bee/elements/nbt/expressions/ExprPrettyNBT.java
+++ src/main/java/tk/shanebee/bee/elements/nbt/expressions/ExprPrettyNBT.java
@@ -28,11 +28,9 @@ import javax.annotation.Nullable;
 public class ExprPrettyNBT extends PropertyExpression<Object, String> {
 
     static {
-        if (Skript.isRunningMinecraft(1, 13, 2)) {
-            Skript.registerExpression(ExprPrettyNBT.class, String.class, ExpressionType.PROPERTY,
-                    "pretty nbt (of|from) %nbtcompounds/strings% [(with|using) split %-string%]",
-                    "%nbtcompounds/strings%'[s] pretty nbt [(with|using) split %-string%]");
-        }
+        Skript.registerExpression(ExprPrettyNBT.class, String.class, ExpressionType.PROPERTY,
+                "pretty nbt (of|from) %nbtcompounds/strings% [(with|using) split %-string%]",
+                "%nbtcompounds/strings%'[s] pretty nbt [(with|using) split %-string%]");
     }
 
     private Expression<String> split;
@@ -49,13 +47,15 @@ public class ExprPrettyNBT extends PropertyExpression<Object, String> {
     protected String @NotNull [] get(@NotNull Event e, Object @NotNull [] source) {
         return get(source, n -> {
             String split = this.split != null ? this.split.getSingle(e) : null;
+            NBTCompound compound;
             if (n instanceof NBTCompound) {
-                return ChatReflection.getPrettyNBT(((NBTCompound) n), split);
+                compound = (NBTCompound) n;
             } else if (n instanceof String) {
-                NBTCompound compound = new NBTContainer(((String) n));
-                return ChatReflection.getPrettyNBT(compound, split);
+                compound = new NBTContainer((String) n);
+            } else {
+                return null;
             }
-            return null;
+            return ChatReflection.getPrettyNBT(compound, split);
         });
     }
 
