915835a01ce86d66936e9a87575658b22c58cfe7
ShaneBee <shanebolenback@me.com>
2021-11-25 07:36:27 -0800

    ReflectionConstants - version checks should be from highest to lowest

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -9,12 +9,12 @@ public class ReflectionConstants {
     public static String ENTITY_NO_CLIP_FIELD = get("noclip", "P", "P");
 
     private static String get(String v116, String v117, String v118) {
-        if (Skript.isRunningMinecraft(1, 16)) {
-            return v116;
+        if (Skript.isRunningMinecraft(1, 18)) {
+            return v118;
         } else if (Skript.isRunningMinecraft(1, 17)) {
             return v117;
-        } else if (Skript.isRunningMinecraft(1, 18)) {
-            return v118;
+        } else if (Skript.isRunningMinecraft(1, 16)) {
+            return v116;
         }
         throw new IllegalArgumentException("Unknown Version");
     }
