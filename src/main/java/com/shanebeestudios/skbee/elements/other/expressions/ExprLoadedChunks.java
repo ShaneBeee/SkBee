26b6268d2039c404a4abb216a9119233cfca02c3
ShaneBee <shanebolenback@me.com>
2021-11-28 20:51:59 -0800

    ExprLoadedChunks - new expression for loaded chunks in a world

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprLoadedChunks.java
@@ -0,0 +1,63 @@
+package com.shanebeestudios.skbee.elements.other.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Loaded Chunks")
+@Description("Represents all currently loaded chunks in a world.")
+@Examples({"loop loaded chunks of world of player:",
+        "set {_chunks} to loaded chunks of world \"world\""})
+@Since("INSERT VERSION")
+public class ExprLoadedChunks extends SimpleExpression<Chunk> {
+
+    static {
+        Skript.registerExpression(ExprLoadedChunks.class, Chunk.class, ExpressionType.COMBINED,
+                "[(all [[of] the]|the)] loaded chunks of %world%");
+    }
+
+    private Expression<World> world;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        world = (Expression<World>) exprs[0];
+        return true;
+    }
+
+    @Nullable
+    @Override
+    protected Chunk[] get(Event e) {
+        World world = this.world.getSingle(e);
+        if (world != null) {
+            return world.getLoadedChunks();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends Chunk> getReturnType() {
+        return Chunk.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean d) {
+        return "loaded chunks of world '" + world.toString(e, d) + "'";
+    }
+
+}
