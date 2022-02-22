88742408e6712b6c9e2d09820f92fb8897366099
ShaneBee <shanebolenback@me.com>
2022-02-21 22:31:06 -0800

    Board - Types - Fix an issue with already registered class infos - Ref #137

--- src/main/java/com/shanebeestudios/skbee/elements/board/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/board/type/Types.java
@@ -5,7 +5,8 @@ import ch.njol.skript.registrations.Classes;
 import com.shanebeestudios.skbee.api.util.EnumParser;
 import com.shanebeestudios.skbee.api.util.EnumUtils;
 import com.shanebeestudios.skbee.elements.board.objects.BeeTeam;
-import org.bukkit.scoreboard.Team;
+import org.bukkit.scoreboard.Team.Option;
+import org.bukkit.scoreboard.Team.OptionStatus;
 
 @SuppressWarnings("unused")
 public class Types {
@@ -16,21 +17,25 @@ public class Types {
                 .name("Team")
                 .description("Represents a scoreboard team."));
 
-        EnumUtils<Team.Option> TEAM_OPTIONS = new EnumUtils<>(Team.Option.class);
-        Classes.registerClass(new ClassInfo<>(Team.Option.class, "teamoption")
-                .user("team ?options?")
-                .name("Team - Option")
-                .usage(TEAM_OPTIONS.getAllNames())
-                .description("Represents an option for a team.")
-                .parser(new EnumParser<>(TEAM_OPTIONS)));
+        if (Classes.getExactClassInfo(Option.class) == null) {
+            EnumUtils<Option> TEAM_OPTIONS = new EnumUtils<>(Option.class);
+            Classes.registerClass(new ClassInfo<>(Option.class, "teamoption")
+                    .user("team ?options?")
+                    .name("Team - Option")
+                    .usage(TEAM_OPTIONS.getAllNames())
+                    .description("Represents an option for a team.")
+                    .parser(new EnumParser<>(TEAM_OPTIONS)));
+        }
 
-        EnumUtils<Team.OptionStatus> TEAM_OPTION_STATUS = new EnumUtils<>(Team.OptionStatus.class);
-        Classes.registerClass(new ClassInfo<>(Team.OptionStatus.class, "teamoptionstatus")
-                .user("team ?option ?status")
-                .name("Team - Option Status")
-                .usage(TEAM_OPTION_STATUS.getAllNames())
-                .description("Represents an option status for a team option")
-                .parser(new EnumParser<>(TEAM_OPTION_STATUS)));
+        if (Classes.getExactClassInfo(OptionStatus.class) == null) {
+            EnumUtils<OptionStatus> TEAM_OPTION_STATUS = new EnumUtils<>(OptionStatus.class);
+            Classes.registerClass(new ClassInfo<>(OptionStatus.class, "teamoptionstatus")
+                    .user("team ?option ?status")
+                    .name("Team - Option Status")
+                    .usage(TEAM_OPTION_STATUS.getAllNames())
+                    .description("Represents an option status for a team option")
+                    .parser(new EnumParser<>(TEAM_OPTION_STATUS)));
+        }
     }
 
 }
