41c7dcba4c5f80397ec47b89303a2c2e2a6fd44e
ShaneBee <shanebolenback@me.com>
2022-03-06 11:41:17 -0800

    ReflectionConstants - preset things for 1.19 - This may not be correct when 1.19 comes out, but we're just future proofing now just in case they happen to be correct

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -4,16 +4,18 @@ import ch.njol.skript.Skript;
 
 public class ReflectionConstants {
 
-    public static String MINECRAFT_KEY_GET_KEY_METHOD = get("getKey", "getKey", "a");
-    public static String TAG_VISITOR_VISIT_METHOD = get("null", "a", "a");
-    public static String ENTITY_NO_CLIP_FIELD = get("noclip", "P", "Q");
-    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "setPrefix", "b");
-    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "setSuffix", "c"); // TODO c = a guess
-    public static String NMS_ITEMSTACK_GET_HOVER_NAME_METHOD = get("getName", "getName", "v");
-    public static String NMS_CHAT_MESSAGE_GET_KEY_METHOD = get("getKey", "getKey", "i");
+    public static String MINECRAFT_KEY_GET_KEY_METHOD = get("getKey", "getKey", "a", "a");
+    public static String TAG_VISITOR_VISIT_METHOD = get("null", "a", "a", "a");
+    public static String ENTITY_NO_CLIP_FIELD = get("noclip", "P", "Q", "Q");
+    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "setPrefix", "b", "b");
+    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "setSuffix", "c", "c");
+    public static String NMS_ITEMSTACK_GET_HOVER_NAME_METHOD = get("getName", "getName", "v", "v");
+    public static String NMS_CHAT_MESSAGE_GET_KEY_METHOD = get("getKey", "getKey", "i", "i");
 
-    private static String get(String v116, String v117, String v118) {
-        if (Skript.isRunningMinecraft(1, 18)) {
+    private static String get(String v116, String v117, String v118, String v119) {
+        if (Skript.isRunningMinecraft(1, 19)) { // 1.19 might not be correct, TEST when 1.19 comes out
+            return v119;
+        } else if (Skript.isRunningMinecraft(1, 18)) {
             return v118;
         } else if (Skript.isRunningMinecraft(1, 17)) {
             return v117;
