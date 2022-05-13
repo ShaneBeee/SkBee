69e4a2390fac71610eb0c316f02c95d66524f233
ShaneBee <shanebolenback@me.com>
2022-05-13 11:25:55 -0700

    EffSendComponent - add ability to send action bar messages - Ref #158

--- src/main/java/com/shanebeestudios/skbee/elements/text/effects/EffSendComponent.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/effects/EffSendComponent.java
@@ -9,6 +9,7 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,12 +17,12 @@ import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.UUID;
-
+@SuppressWarnings("deprecation") // Paper uses their own API
 @Name("TextComponent - Send")
-@Description({"Send text components to players (or console, not sure which server version this started on). ",
+@Description({"Send text components to players/console.",
         "The optional sender (supported in Minecraft 1.16.4+) allows you to send components from a specific player.",
-        "This is useful to make sure players can block messages using 1.16.4's new player chat ignore system."})
+        "This is useful to make sure players can block messages using 1.16.4's new player chat ignore system.",
+        "You can also send action bar components to players."})
 @Examples({"set {_comp::1} to text component of \"hi player \"",
         "set {_comp::2} to text component of \"hover over me for a special message!\"",
         "set hover event of {_comp::2} to hover event to show \"OoO look ma I'm hovering!\"",
@@ -29,33 +30,23 @@ import java.util.UUID;
 @Since("1.5.0")
 public class EffSendComponent extends Effect {
 
-    private static final boolean SUPPORTS_COMMAND_SENDER;
-    private static final boolean SUPPORTS_SENDER;
-
     static {
-        SUPPORTS_COMMAND_SENDER = Skript.classExists("org.bukkit.command.CommandSender$Spigot");
-        SUPPORTS_SENDER = SUPPORTS_COMMAND_SENDER &&
-                Skript.methodExists(CommandSender.Spigot.class, "sendMessage", UUID.class, BaseComponent.class);
-        if (SUPPORTS_SENDER) {
-            Skript.registerEffect(EffSendComponent.class, "send [text] component[s] %basecomponents% [to %commandsenders%] [from %-player%]");
-        } else if (SUPPORTS_COMMAND_SENDER) {
-            Skript.registerEffect(EffSendComponent.class, "send [text] component[s] %basecomponents% [to %commandsenders%]");
-        } else {
-            Skript.registerEffect(EffSendComponent.class, "send [text] component[s] %basecomponents% [to %players%]");
-        }
+        Skript.registerEffect(EffSendComponent.class, "send [(text|1¦action[ bar])] component[s] %basecomponents% [to %commandsenders%] [from %-player%]");
     }
 
     private Expression<BaseComponent> components;
     private Expression<CommandSender> players;
     @Nullable
     private Expression<Player> sender;
+    private boolean action;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         components = (Expression<BaseComponent>) exprs[0];
         players = (Expression<CommandSender>) exprs[1];
-        sender = SUPPORTS_SENDER ? (Expression<Player>) exprs[2] : null;
+        sender = (Expression<Player>) exprs[2];
+        action = parseResult.mark == 1;
         return true;
     }
 
@@ -72,19 +63,19 @@ public class EffSendComponent extends Effect {
     }
 
     private void sendMessage(CommandSender receiver, Player sender, BaseComponent... components) {
-        if (SUPPORTS_SENDER && sender != null) {
+        if (action && receiver instanceof Player) {
+            ((Player) receiver).sendMessage(ChatMessageType.ACTION_BAR, components);
+        } else if (sender != null) {
             receiver.spigot().sendMessage(sender.getUniqueId(), components);
-        } else if (SUPPORTS_COMMAND_SENDER) {
+        } else {
             receiver.spigot().sendMessage(components);
-        } else if (receiver instanceof Player) {
-            ((Player) receiver).spigot().sendMessage(components);
         }
-
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean d) {
-        return String.format("send component[s] %s to %s %s",
+        return String.format("send %s component[s] %s to %s %s",
+                action ? "action bar" : "text",
                 components.toString(e, d),
                 players.toString(e, d),
                 sender != null ? "from " + sender.toString(e, d) : "");
