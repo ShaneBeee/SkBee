0efef91bd6100c1e2002130468511c94c5ab0942
ShaneBee <shanebolenback@me.com>
2022-05-13 11:57:41 -0700

    EffSendComponent - add ability to send normal strings as well - Ref #158

--- src/main/java/com/shanebeestudios/skbee/elements/text/effects/EffSendComponent.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/effects/EffSendComponent.java
@@ -11,30 +11,37 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("deprecation") // Paper uses their own API
 @Name("TextComponent - Send")
 @Description({"Send text components to players/console.",
         "The optional sender (supported in Minecraft 1.16.4+) allows you to send components from a specific player.",
         "This is useful to make sure players can block messages using 1.16.4's new player chat ignore system.",
-        "You can also send action bar components to players."})
+        "As of INSERT VERSION you can also send action bar components to players and you can also send normal strings."})
 @Examples({"set {_comp::1} to text component of \"hi player \"",
         "set {_comp::2} to text component of \"hover over me for a special message!\"",
         "set hover event of {_comp::2} to hover event to show \"OoO look ma I'm hovering!\"",
-        "send component {_comp::*} to player"})
+        "send component {_comp::*} to player",
+        "send components {_comp::*} and \" ooo pretty!\" to player",
+        "send actionbar component {_comp::1} to player"})
 @Since("1.5.0")
 public class EffSendComponent extends Effect {
 
     static {
-        Skript.registerEffect(EffSendComponent.class, "send [(text|1¦action[ bar])] component[s] %basecomponents% [to %commandsenders%] [from %-player%]");
+        Skript.registerEffect(EffSendComponent.class,
+                "send [(text|1¦action[[ ]bar])] component[s] %objects% [to %commandsenders%] [from %-player%]");
     }
 
-    private Expression<BaseComponent> components;
+    private Expression<Object> components;
     private Expression<CommandSender> players;
     @Nullable
     private Expression<Player> sender;
@@ -43,7 +50,7 @@ public class EffSendComponent extends Effect {
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        components = (Expression<BaseComponent>) exprs[0];
+        components = (Expression<Object>) exprs[0];
         players = (Expression<CommandSender>) exprs[1];
         sender = (Expression<Player>) exprs[2];
         action = parseResult.mark == 1;
@@ -56,9 +63,18 @@ public class EffSendComponent extends Effect {
 
         Player sender = this.sender != null ? this.sender.getSingle(e) : null;
 
-        BaseComponent[] components = this.components.getArray(e);
+        List<BaseComponent> components = new ArrayList<>();
+        for (Object comp : this.components.getArray(e)) {
+            if (comp instanceof BaseComponent) {
+                components.add(((BaseComponent) comp));
+            } else if (comp instanceof String) {
+                components.add(new TextComponent(TextComponent.fromLegacyText((String) comp)));
+            }
+        }
+
+        BaseComponent[] baseComponents = components.toArray(new BaseComponent[0]);
         for (CommandSender player : this.players.getArray(e)) {
-            sendMessage(player, sender, components);
+            sendMessage(player, sender, baseComponents);
         }
     }
 
