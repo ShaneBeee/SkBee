2e8292f8014720c3a1ad0de885a7dc7c03ba11b5
ShaneBee <shanebolenback@me.com>
2022-06-18 11:05:18 -0700

    EffDropItem - new effect to force player to drop their held item

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffDropItem.java
@@ -0,0 +1,57 @@
+package com.shanebeestudios.skbee.elements.other.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+@Name("Drop Held Item")
+@Description({"Forces the player to drop their currently held item.",
+        "By default it will drop one of their held item, or you can optionally drop the whole stack."})
+@Examples({"make player drop all of held item",
+        "force all players to drop all of their held items"})
+@Since("INSERT VERSION")
+public class EffDropItem extends Effect {
+
+    static {
+        Skript.registerEffect(EffDropItem.class, "(make|force) %players% [to] drop (|1¦all of) [their] held item[s]");
+    }
+
+    private Expression<Player> players;
+    private boolean all;
+
+    @SuppressWarnings({"NullableProblems", "unchecked"})
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.players = (Expression<Player>) exprs[0];
+        this.all = parseResult.mark == 1;
+        return true;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    protected void execute(Event event) {
+        for (Player player : this.players.getArray(event)) {
+            if (player.dropItem(all)) {
+                // Appears the server doesn't update automatically
+                player.updateInventory();
+            }
+        }
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean d) {
+        String all = this.all ? " all of" : "";
+        return "make " + this.players.toString(e, d) + " drop" + all + " held item";
+    }
+
+}
