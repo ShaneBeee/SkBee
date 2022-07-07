7db1d680c10436907a1c510352f06bb6379b3cda
ShaneBee <shanebolenback@me.com>
2022-07-07 12:33:11 -0700

    SkBee - fix an error when running older MC 1.17.1 versions - This happens when running a 1.17.1 build that was before bukkit added structures - Ref #208

--- src/main/java/com/shanebeestudios/skbee/SkBee.java
+++ src/main/java/com/shanebeestudios/skbee/SkBee.java
@@ -290,6 +290,13 @@ public class SkBee extends JavaPlugin {
             return;
         }
 
+        // This was added in Oct/2021 (so just before 1.18 came out)
+        if (!Skript.methodExists(Bukkit.class, "getStructureManager")) {
+            Util.logLoading("&cIt appears structure elements are not available on your server version.");
+            Util.logLoading("&5Structure Elements &cdisabled");
+            return;
+        }
+
         this.structureBeeManager = new StructureBeeManager();
         try {
             addon.loadClasses("com.shanebeestudios.skbee.elements.structure");
