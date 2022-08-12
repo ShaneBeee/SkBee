f3ccd3bafd181cffcfbb1ca4f0d59a9ecd91fe28
ShaneBee <shanebolenback@me.com>
2022-08-11 22:28:15 -0700

    ReflectionConstants - remove 1.16 constants

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -4,23 +4,21 @@ import ch.njol.skript.Skript;
 
 public class ReflectionConstants {
 
-    public static String MINECRAFT_KEY_GET_KEY_METHOD = get("getKey", "getKey", "a", "a");
-    public static String TAG_VISITOR_VISIT_METHOD = get("null", "a", "a", "a");
-    public static String ENTITY_NO_CLIP_FIELD = get("noclip", "P", "Q", "Q");
-    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "setPrefix", "b", "b");
-    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "setSuffix", "c", "c");
-    public static String NMS_ITEMSTACK_GET_HOVER_NAME_METHOD = get("getName", "getName", "v", "v");
-    public static String NMS_CHAT_MESSAGE_GET_KEY_METHOD = get("getKey", "getKey", "i", "i");
+    public static String MINECRAFT_KEY_GET_KEY_METHOD = get("getKey", "a", "a");
+    public static String TAG_VISITOR_VISIT_METHOD = get("a", "a", "a");
+    public static String ENTITY_NO_CLIP_FIELD = get("P", "Q", "Q");
+    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "b", "b");
+    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "c", "c");
+    public static String NMS_ITEMSTACK_GET_HOVER_NAME_METHOD = get("getName", "v", "v");
+    public static String NMS_CHAT_MESSAGE_GET_KEY_METHOD = get("getKey", "i", "i");
 
-    private static String get(String v116, String v117, String v118, String v119) {
-        if (Skript.isRunningMinecraft(1, 19)) { // 1.19 might not be correct, TEST when 1.19 comes out
+    private static String get(String v117, String v118, String v119) {
+        if (Skript.isRunningMinecraft(1, 19)) {
             return v119;
         } else if (Skript.isRunningMinecraft(1, 18)) {
             return v118;
         } else if (Skript.isRunningMinecraft(1, 17)) {
             return v117;
-        } else if (Skript.isRunningMinecraft(1, 16)) {
-            return v116;
         }
         throw new IllegalArgumentException("Unknown Version");
     }
