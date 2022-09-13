968680dfafd66727d1c439c9b60d9663b35f135c
ShaneBee <shanebolenback@me.com>
2022-09-13 11:25:13 -0700

    CondHasNBTTag - add condition to check if a tag exists

---
+++ src/main/java/com/shanebeestudios/skbee/elements/nbt/conditions/CondHasNBTTag.java
@@ -0,0 +1,56 @@
+package com.shanebeestudios.skbee.elements.nbt.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.conditions.base.PropertyCondition.PropertyType;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.api.NBT.NBTApi;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+@Name("NBT - Has Tag")
+@Description("Check if an NBT Compound contains a tag.")
+@Examples({"if nbt compound of player has tag \"custom;points\":",
+        "if nbt item compound of player's tool doesn't have nbt tag \"health;score\":"})
+@Since("INSERT VERSION")
+public class CondHasNBTTag extends Condition {
+
+    static {
+        PropertyCondition.register(CondHasNBTTag.class, PropertyType.HAVE,
+                "[nbt] tag %string%", "nbtcompounds");
+    }
+
+    private Expression<NBTCompound> compounds;
+    private Expression<String> tag;
+
+    @SuppressWarnings({"unchecked", "NullableProblems"})
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.compounds = (Expression<NBTCompound>) exprs[0];
+        this.tag = (Expression<String>) exprs[1];
+        setNegated(matchedPattern == 1);
+        return true;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public boolean check(Event event) {
+        String tag = this.tag.getSingle(event);
+        return compounds.check(event, compound -> NBTApi.hasTag(compound, tag), isNegated());
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean d) {
+        String has = isNegated() ? " has tag " : " doesn't have tag ";
+        return this.compounds.toString(e, d) + has + this.tag.toString(e, d);
+    }
+
+}
