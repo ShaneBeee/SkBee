3a66e23da9b2ce6b5db13ec560f7c880d90f4356
ShaneBee <shanebolenback@me.com>
2022-09-18 07:29:19 -0700

    CondBoundContainsLocation - fix NPE - Ref #259

--- src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundContainsLocation.java
+++ src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundContainsLocation.java
@@ -7,11 +7,12 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.elements.bound.objects.Bound;
 import org.bukkit.Location;
 import org.bukkit.event.Event;
-import com.shanebeestudios.skbee.elements.bound.objects.Bound;
+import org.jetbrains.annotations.NotNull;
 
 @Name("Bound - Contains Location")
 @Description("Check if a location is within the bounds of a bounding box.")
@@ -31,22 +32,25 @@ public class CondBoundContainsLocation extends Condition {
     private Expression<Bound> bound;
     private Expression<Location> locations;
 
-    @SuppressWarnings({"unchecked", "null"})
+    @SuppressWarnings({"unchecked", "null", "NullableProblems"})
     @Override
-    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean kleenean, ParseResult parseResult) {
         bound = (Expression<Bound>) exprs[1];
         locations = (Expression<Location>) exprs[0];
         setNegated(matchedPattern == 1);
         return true;
     }
 
+    @SuppressWarnings("NullableProblems")
     @Override
     public boolean check(Event event) {
-        return locations.check(event, location -> bound.getSingle(event).isInRegion(location), isNegated());
+        Bound bound = this.bound.getSingle(event);
+        if (bound == null) return false;
+        return locations.check(event, bound::isInRegion, isNegated());
     }
 
     @Override
-    public String toString(Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         return PropertyCondition.toString(this, PropertyCondition.PropertyType.BE, e, d, locations,
                 "in the bound " + bound.toString(e, d));
     }
