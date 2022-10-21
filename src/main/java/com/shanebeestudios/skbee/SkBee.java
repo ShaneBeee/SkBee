8b4b3926a020949dbce54767b5a25fa6466a9c8f
ShaneBee <shanebolenback@me.com>
2022-10-21 08:15:57 -0700

    SkBee - fix error when another addon loads text component syntax - Ref #272

--- src/main/java/com/shanebeestudios/skbee/SkBee.java
+++ src/main/java/com/shanebeestudios/skbee/SkBee.java
@@ -266,6 +266,12 @@ public class SkBee extends JavaPlugin {
             Util.logLoading("&7- Text components require a PaperMC server.");
             return;
         }
+        if (Classes.getClassInfoNoError("textcomponent") != null) {
+            Util.logLoading("&5Text Component Elements &cdisabled");
+            Util.logLoading("&7It appears another Skript addon may have registered Text Component syntax.");
+            Util.logLoading("&7To use SkBee Text Components, please remove the addon which has registered Text Components already.");
+            return;
+        }
         try {
             addon.loadClasses("com.shanebeestudios.skbee.elements.text");
             Util.logLoading("&5Text Component Elements &asuccessfully loaded");
