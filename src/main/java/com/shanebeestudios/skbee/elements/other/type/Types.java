7a6525ffb469d08b3809f82f7bcb46b8f4a1eb18
ShaneBee <shanebolenback@me.com>
2022-12-20 07:15:32 -0800

    other/Types - add toString for DustOption

--- src/main/java/com/shanebeestudios/skbee/elements/other/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/type/Types.java
@@ -9,8 +9,8 @@ import ch.njol.skript.lang.function.Parameter;
 import ch.njol.skript.lang.function.SimpleJavaFunction;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.registrations.DefaultClasses;
-import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Color;
+import ch.njol.skript.util.SkriptColor;
 import ch.njol.skript.util.Timespan;
 import ch.njol.util.StringUtils;
 import com.shanebeestudios.skbee.api.particle.ParticleUtil;
@@ -200,7 +200,37 @@ public class Types {
         }
 
         Classes.registerClass(new ClassInfo<>(DustOptions.class, "dustoption")
-                .name(ClassInfo.NO_DOC).user("dust ?options?"));
+                .name(ClassInfo.NO_DOC).user("dust ?options?")
+                .parser(new Parser<>() {
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public boolean canParse(ParseContext context) {
+                        return false;
+                    }
+
+                    @Override
+                    public @NotNull String toString(DustOptions dustOption, int flags) {
+                        org.bukkit.Color bukkitColor = dustOption.getColor();
+                        int red = bukkitColor.getRed();
+                        int green = bukkitColor.getGreen();
+                        int blue = bukkitColor.getBlue();
+                        SkriptColor skriptColor = SkriptColor.fromBukkitColor(bukkitColor);
+
+                        String color;
+                        //noinspection ConstantConditions
+                        if (skriptColor != null) {
+                            color = skriptColor.toString();
+                        } else {
+                            color = String.format("rgb(%s,%s,%s)", red, green, blue);
+                        }
+                        return "dustOption(color=" + color + ",size=" + dustOption.getSize() + ")";
+                    }
+
+                    @Override
+                    public @NotNull String toVariableNameString(DustOptions o) {
+                        return toString(o, 0);
+                    }
+                }));
         Classes.registerClass(new ClassInfo<>(DustTransition.class, "dusttransition")
                 .name(ClassInfo.NO_DOC).user("dust ?transitions?"));
         Classes.registerClass(new ClassInfo<>(Vibration.class, "vibration")
