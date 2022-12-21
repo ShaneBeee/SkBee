1c9e1d084dfa1e7310d7f9f3412aca0a73e16c3f
ShaneBee <shanebolenback@me.com>
2022-12-20 22:29:57 -0800

    ExprNoClip - clean up and move to other package

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -7,7 +7,7 @@ public class ReflectionConstants {
     // net.minecraft.nbt.TagVisitor -> visitString
     public static String TAG_VISITOR_VISIT_METHOD = get("a", "a", "a");
     // net.minecraft.world.entity.Entity -> noPhysics
-    public static String ENTITY_NO_CLIP_FIELD = get("P", "Q", "Q");
+    public static String ENTITY_NO_PHYSICS_FIELD = get("P", "Q", "Q");
     // net.minecraft.world.scores.PlayerTeam -> setPlayerPrefix
     public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "b", "b");
     // net.minecraft.world.scores.PlayerTeam -> setPlayerSuffix
--- src/main/java/com/shanebeestudios/skbee/api/util/EntityUtils.java
+++ src/main/java/com/shanebeestudios/skbee/api/util/EntityUtils.java
@@ -11,19 +11,19 @@ public class EntityUtils {
 
     private static final Class<?> ENTITY_NMS_CLASS = ReflectionUtils.getNMSClass("Entity", "net.minecraft.world.entity");
 
-    public static void setClip(Entity entity, boolean clip) {
+    public static void setNoPhysics(Entity entity, boolean clip) {
         if (entity == null || ENTITY_NMS_CLASS == null) return;
         Object nmsEntity = ReflectionUtils.getNMSEntity(entity);
         if (nmsEntity == null) return;
-        ReflectionUtils.setField(ReflectionConstants.ENTITY_NO_CLIP_FIELD, ENTITY_NMS_CLASS, nmsEntity, !clip);
+        ReflectionUtils.setField(ReflectionConstants.ENTITY_NO_PHYSICS_FIELD, ENTITY_NMS_CLASS, nmsEntity, clip);
     }
 
     @SuppressWarnings("ConstantConditions")
-    public static boolean getClip(Entity entity) {
+    public static boolean getNoPhysics(Entity entity) {
         if (entity == null || ENTITY_NMS_CLASS == null) return false;
         Object nmsEntity = ReflectionUtils.getNMSEntity(entity);
         if (nmsEntity == null) return false;
-        return !Boolean.parseBoolean(ReflectionUtils.getField(ReflectionConstants.ENTITY_NO_CLIP_FIELD, ENTITY_NMS_CLASS, nmsEntity).toString());
+        return Boolean.parseBoolean(ReflectionUtils.getField(ReflectionConstants.ENTITY_NO_PHYSICS_FIELD, ENTITY_NMS_CLASS, nmsEntity).toString());
     }
 
 }
