65f57d69fe82c99978d07e502420a57a2dc43176
ShaneBee <shanebolenback@me.com>
2023-01-09 20:51:03 -0800

    ExprNearestEntity - add number of nearest

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNearestEntity.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNearestEntity.java
@@ -18,6 +18,7 @@ import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,35 +30,41 @@ import java.util.stream.Collectors;
         "damage nearest mob in radius 5 around player",
         "set {_near} to nearest entity in radius 50 around {_loc}",
         "set {_near} to nearest entity in radius 100 around location(100,100,100,world \"world\")",
-        "teleport player to nearest player in radius 10 around player"})
+        "teleport player to nearest player in radius 10 around player",
+        "damage 10 nearest entity in radius 10 around player by 2"})
 @Since("INSERT VERSION")
 @SuppressWarnings("NullableProblems")
 public class ExprNearestEntity extends SimpleExpression<Entity> {
 
     static {
         Skript.registerExpression(ExprNearestEntity.class, Entity.class, ExpressionType.COMBINED,
-                "nearest %entitydata% in radius %number% (of|around) %location/entity%");
+                "[num:%number%] nearest %entitydata% in radius %number% (of|around) %location/entity%");
     }
 
+    private Expression<Number> number;
     private Expression<EntityData<?>> entityData;
     private Expression<Number> radius;
     private Expression<Object> location;
+    private boolean isSingle;
 
     @SuppressWarnings({"NullableProblems", "unchecked"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        this.entityData = (Expression<EntityData<?>>) exprs[0];
-        this.radius = (Expression<Number>) exprs[1];
-        this.location = (Expression<Object>) exprs[2];
+        this.number = (Expression<Number>) exprs[0];
+        this.entityData = (Expression<EntityData<?>>) exprs[1];
+        this.radius = (Expression<Number>) exprs[2];
+        this.location = (Expression<Object>) exprs[3];
+        this.isSingle = !parseResult.hasTag("num");
         return true;
     }
 
     @Override
     protected @Nullable Entity[] get(Event event) {
+        Number number = this.number.getSingle(event);
         EntityData<?> entityData = this.entityData.getSingle(event);
         Number radius = this.radius.getSingle(event);
         Object object = this.location.getSingle(event);
-        if (entityData == null || radius == null || object == null) return null;
+        if (number == null || entityData == null || radius == null || object == null) return null;
         double rad = radius.doubleValue();
 
         List<Entity> nearby;
@@ -66,24 +73,28 @@ public class ExprNearestEntity extends SimpleExpression<Entity> {
         } else {
             nearby = getNearby((Location) object, entityData, rad, null);
         }
-        if (nearby.size() > 0) {
-            return new Entity[]{nearby.get(0)};
+
+        List<Entity> entities = new ArrayList<>();
+        for (int i = 0; i < number.intValue(); i++) {
+            if (i < nearby.size()) {
+                entities.add(nearby.get(i));
+            }
         }
-        return null;
+        return entities.toArray(new Entity[0]);
     }
 
     private List<Entity> getNearby(Location location, EntityData<?> entityData, double radius, Object exclude) {
         World world = location.getWorld();
         return world.getNearbyEntitiesByType(entityData.getType(), location, radius)
                 .stream()
-                .sorted(Comparator.comparing(entity -> entity.getLocation().distance(location)))
+                .sorted(Comparator.comparing(entity -> entity.getLocation().distanceSquared(location)))
                 .filter(entity -> entity != exclude)
                 .collect(Collectors.toList());
     }
 
     @Override
     public boolean isSingle() {
-        return true;
+        return this.isSingle;
     }
 
     @Override
@@ -93,10 +104,11 @@ public class ExprNearestEntity extends SimpleExpression<Entity> {
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean d) {
+        String num = this.number != null ? this.number.toString(e, d) + " " : "";
         String data = this.entityData.toString(e, d);
         String radius = this.radius.toString(e, d);
         String loc = this.location.toString(e, d);
-        return "nearest " + data + " in radius " + radius + " around " + loc;
+        return num + "nearest " + data + " in radius " + radius + " around " + loc;
     }
 
 }
