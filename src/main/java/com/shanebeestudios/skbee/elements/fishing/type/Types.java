0da0547150a428896879e79cdffccf67ace063f3
ShaneBee <shanebolenback@me.com>
2023-02-13 11:39:39 -0800

    ExprFishHookState - add type/expression for hooked state of fish hook

--- src/main/java/com/shanebeestudios/skbee/elements/fishing/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/fishing/type/Types.java
@@ -4,6 +4,7 @@ import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.registrations.Classes;
 import com.shanebeestudios.skbee.api.util.EnumUtils;
 import com.shanebeestudios.skbee.api.util.Util;
+import org.bukkit.entity.FishHook.HookState;
 import org.bukkit.event.player.PlayerFishEvent;
 
 public class Types {
@@ -21,6 +22,19 @@ public class Types {
         } else {
             Util.logLoading("It looks like another addon registered 'fishingstate' already.");
             Util.logLoading("You may have to use their fishing states in SkBee's 'Fish Event State' expression.");
+        }
+
+        if (Classes.getExactClassInfo(HookState.class) == null) {
+            EnumUtils<HookState> FISH_HOOK_STATE_ENUM = new EnumUtils<>(HookState.class);
+            Classes.registerClass(new ClassInfo<>(HookState.class, "fishhookstate")
+                    .user("fish ?hook ?states?")
+                    .name("Fish Hook State")
+                    .usage(FISH_HOOK_STATE_ENUM.getAllNames())
+                    .since("INSERT VERSION")
+                    .parser(FISH_HOOK_STATE_ENUM.getParser()));
+        } else {
+            Util.logLoading("It looks like another addon registered 'fishhookstate' already.");
+            Util.logLoading("You may have to use their fish hook states in SkBee's fish hook state expression.");
         }
     }
 
