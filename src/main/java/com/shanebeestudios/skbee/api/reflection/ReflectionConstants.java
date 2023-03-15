d42f14277973d78511c6b44b659bb2806b93e486
ShaneBee <shanebolenback@me.com>
2023-03-15 13:34:14 -0700

    ReflectionConstants - update for Minecraft 1.19.4

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -5,16 +5,18 @@ import ch.njol.skript.Skript;
 public class ReflectionConstants {
 
     // net.minecraft.nbt.TextComponentTagVisitor -> visit(Tag)
-    public static String TAG_VISITOR_VISIT_METHOD = get("a", "a", "a");
+    public static String TAG_VISITOR_VISIT_METHOD = get("a", "a", "a", "a");
     // net.minecraft.world.entity.Entity -> noPhysics
-    public static String ENTITY_NO_PHYSICS_FIELD = get("P", "Q", "Q");
+    public static String ENTITY_NO_PHYSICS_FIELD = get("P", "Q", "Q", "ae");
     // net.minecraft.world.scores.PlayerTeam -> setPlayerPrefix
-    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "b", "b");
+    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "b", "b", "b");
     // net.minecraft.world.scores.PlayerTeam -> setPlayerSuffix
-    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "c", "c");
+    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "c", "c", "c");
 
-    private static String get(String v117, String v118, String v119) {
-        if (Skript.isRunningMinecraft(1, 19)) {
+    private static String get(String v117, String v118, String v119, String v1194) {
+        if (Skript.isRunningMinecraft(1,19,4)) {
+            return v1194;
+        } else if (Skript.isRunningMinecraft(1, 19)) {
             return v119;
         } else if (Skript.isRunningMinecraft(1, 18)) {
             return v118;
