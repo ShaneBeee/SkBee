18be599bc0a6702fea53c24f827c721cfc1bc30f
ShaneBee <shanebolenback@me.com>
2023-03-19 19:44:11 -0700

    Initial work on Display Entity Transformations

--- src/main/java/com/shanebeestudios/skbee/elements/display/types/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/display/types/Types.java
@@ -13,11 +13,18 @@ import org.bukkit.entity.Display;
 import org.bukkit.entity.Display.Billboard;
 import org.bukkit.entity.ItemDisplay.ItemDisplayTransform;
 import org.bukkit.entity.TextDisplay.TextAligment;
+import org.bukkit.util.Transformation;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 public class Types {
 
     public static final String McWIKI = "See <link>https://minecraft.fandom.com/wiki/Display#Entity_data</link> for more details.";
+    public static ClassInfo<Transformation> TRANSFORMATION;
+    public static ClassInfo<Quaternionf> VECTOR4;
+
 
     // TYPES
     static {
@@ -63,12 +70,26 @@ public class Types {
                 .since("INSERT VERSION")
                 .parser(TRANSFORM_ENUM.getParser()));
 
+        TRANSFORMATION = new ClassInfo<>(Transformation.class, "transformation")
+                .user("transformations?")
+                .name("DisplayEntity - Transformation")
+                .description("Represents a transformation of a Display Entity.", McWIKI)
+                .since("INSERT VERSION");
+        Classes.registerClass(TRANSFORMATION);
+
         Classes.registerClass(new ClassInfo<>(Color.class, "bukkitcolor")
                 .user("bukkit ?colors?")
                 .name("Bukkit Color")
                 .description("Represents a Bukkit color. This is different than a Skript color",
                         "as it adds an alpha channel.")
                 .since("INSERT VERSION"));
+
+        VECTOR4 = new ClassInfo<>(Quaternionf.class, "vector4")
+                .user("vector4s?")
+                .name("Vector4")
+                .description("Represents a Quaternion (like a vector but with 4 values).")
+                .since("INSERT VERSION");
+        Classes.registerClass(VECTOR4);
     }
 
     // FUNCTIONS
@@ -122,6 +143,58 @@ public class Types {
                         "Number values must be between 0 and 255.")
                 .examples("set {_color} to bukkitColor(50,155,100,10)")
                 .since("INSERT VERSION"));
+
+        Functions.registerFunction(new SimpleJavaFunction<>("vector4", new Parameter[]{
+                new Parameter<>("x", DefaultClasses.NUMBER, true, null),
+                new Parameter<>("y", DefaultClasses.NUMBER, true, null),
+                new Parameter<>("z", DefaultClasses.NUMBER, true, null),
+                new Parameter<>("w", DefaultClasses.NUMBER, true, null)
+        }, VECTOR4, true) {
+            @SuppressWarnings("NullableProblems")
+            @Override
+            public @Nullable Quaternionf[] executeSimple(Object[][] params) {
+                float x = ((Number) params[0][0]).floatValue();
+                float y = ((Number) params[1][0]).floatValue();
+                float z = ((Number) params[2][0]).floatValue();
+                float w = ((Number) params[3][0]).floatValue();
+                return new Quaternionf[]{new Quaternionf(x, y, z, w)};
+            }
+        }
+                .description("Creates a new Vector4(Quaternion).")
+                .examples("set {_v} to vector4(1,0,0,0)")
+                .since("INSERT VERSION"));
+
+        Functions.registerFunction(new SimpleJavaFunction<>("transformation", new Parameter[]{
+                new Parameter<>("translation", DefaultClasses.VECTOR, true, null),
+                new Parameter<>("scale", DefaultClasses.VECTOR, true, null),
+                new Parameter<>("leftRotation", VECTOR4, true, null),
+                new Parameter<>("rightRotation", VECTOR4, true, null)
+        }, TRANSFORMATION, true) {
+            @SuppressWarnings("NullableProblems")
+            @Override
+            public Transformation[] executeSimple(Object[][] params) {
+                Vector3f translation = convert((Vector) params[0][0]);
+                Vector3f scale = convert((Vector) params[1][0]);
+                Quaternionf leftRotation = (Quaternionf) params[2][0];
+                Quaternionf rightRotation = (Quaternionf) params[3][0];
+                return new Transformation[]{new Transformation(translation, leftRotation, scale, rightRotation)};
+            }
+        }
+                .description("Creates a new Transformation")
+                .examples("on load:",
+                        "\tset {_trans} to vector(0,1,0)",
+                        "\tset {_scale} to vector(1,1,1)",
+                        "\tset {_lr} to vector4(1,1,1,1)",
+                        "\tset {_rr} to vector4(2,2,2,2)",
+                        "\tset {_transform} to transformation({_trans}, {_scale}, {_lr}, {_rr})")
+                .since("INSERT VERSION"));
+    }
+
+    private static Vector3f convert(Vector vector) {
+        double x = vector.getX();
+        double y = vector.getY();
+        double z = vector.getZ();
+        return new Vector3f((float) x, (float) y, (float) z);
     }
 
 }
