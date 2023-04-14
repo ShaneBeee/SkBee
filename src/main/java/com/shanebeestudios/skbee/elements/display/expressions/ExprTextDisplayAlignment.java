2129823ec81d0f9dac16f74f8c69ca3d075d41f1
ShaneBee <shanebolenback@me.com>
2023-04-14 09:24:33 -0700

    Display - fixed error due to spigot changing name of class - Ref #377

--- src/main/java/com/shanebeestudios/skbee/elements/display/expressions/ExprTextDisplayAlignment.java
+++ src/main/java/com/shanebeestudios/skbee/elements/display/expressions/ExprTextDisplayAlignment.java
@@ -10,7 +10,7 @@ import ch.njol.util.coll.CollectionUtils;
 import com.shanebeestudios.skbee.elements.display.types.Types;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TextDisplay;
-import org.bukkit.entity.TextDisplay.TextAligment;
+import org.bukkit.entity.TextDisplay.TextAlignment;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,15 +19,15 @@ import org.jetbrains.annotations.Nullable;
 @Description({"Represents the text alignment of a Text Display Entity.", Types.McWIKI})
 @Examples("set display text alignment of {_display} to left aligned")
 @Since("2.8.0")
-public class ExprTextDisplayAlignment extends SimplePropertyExpression<Entity, TextAligment> {
+public class ExprTextDisplayAlignment extends SimplePropertyExpression<Entity, TextAlignment> {
 
     static {
-        register(ExprTextDisplayAlignment.class, TextAligment.class,
+        register(ExprTextDisplayAlignment.class, TextAlignment.class,
                 "[display] text alignment", "entities");
     }
 
     @Override
-    public @Nullable TextAligment convert(Entity entity) {
+    public @Nullable TextAlignment convert(Entity entity) {
         if (entity instanceof TextDisplay textDisplay) return textDisplay.getAlignment();
         return null;
     }
@@ -35,14 +35,14 @@ public class ExprTextDisplayAlignment extends SimplePropertyExpression<Entity, T
     @SuppressWarnings("NullableProblems")
     @Override
     public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
-        if (mode == ChangeMode.SET) return CollectionUtils.array(TextAligment.class);
+        if (mode == ChangeMode.SET) return CollectionUtils.array(TextAlignment.class);
         return null;
     }
 
     @SuppressWarnings({"NullableProblems", "ConstantValue"})
     @Override
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
-        if (delta != null && delta[0] instanceof TextAligment textAligment) {
+        if (delta != null && delta[0] instanceof TextAlignment textAligment) {
             for (Entity entity : getExpr().getArray(event)) {
                 if (entity instanceof TextDisplay textDisplay) {
                     textDisplay.setAlignment(textAligment);
@@ -52,8 +52,8 @@ public class ExprTextDisplayAlignment extends SimplePropertyExpression<Entity, T
     }
 
     @Override
-    public @NotNull Class<? extends TextAligment> getReturnType() {
-        return TextAligment.class;
+    public @NotNull Class<? extends TextAlignment> getReturnType() {
+        return TextAlignment.class;
     }
 
     @Override
