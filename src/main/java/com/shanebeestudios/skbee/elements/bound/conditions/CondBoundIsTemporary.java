418af1cb17c3c5eba1fa0b7472bc1355381eb362
DelayedGaming <simmz0403@gmail.com>
2023-05-19 02:25:41 +0800

    Add docs

--- src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundIsTemporary.java
+++ src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundIsTemporary.java
@@ -1,8 +1,16 @@
 package com.shanebeestudios.skbee.elements.bound.conditions;
 
 import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import com.shanebeestudios.skbee.elements.bound.objects.Bound;
 
+@Name("Bound - Is Temporary")
+@Description("Check if a bound is temporary.")
+@Examples("if bound with id \"temporary-bound\" is temporary:")
+@Since("INSERT VERSION")
 public class CondBoundIsTemporary extends PropertyCondition<Bound> {
 
     static {
