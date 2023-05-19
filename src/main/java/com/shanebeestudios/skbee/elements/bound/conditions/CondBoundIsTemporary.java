e1850516c1cf108501f790716df5b8e496e098fa
DelayedGaming <72163224+DelayedGaming@users.noreply.github.com>
2023-05-19 08:42:21 +0800

    Change pattern for CondBoundIsTemporary

--- src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundIsTemporary.java
+++ src/main/java/com/shanebeestudios/skbee/elements/bound/conditions/CondBoundIsTemporary.java
@@ -14,7 +14,7 @@ import com.shanebeestudios.skbee.elements.bound.objects.Bound;
 public class CondBoundIsTemporary extends PropertyCondition<Bound> {
 
     static {
-        register(CondBoundIsTemporary.class, "temporary", "bound");
+        register(CondBoundIsTemporary.class, "a temporary bound", "bound");
     }
 
     @Override
