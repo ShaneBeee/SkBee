acf1ab8f412608ea34c5d368977752c86271076d
ShaneBee <shanebolenback@me.com>
2023-06-03 17:30:39 -0700

    BoundBorderListener - change it up - refactor how things are done with a different method that returns, and we cancel event - handle mounting an entity in a bound - handle being pushed out of a bound, and bring your vehicle with you

--- src/main/java/com/shanebeestudios/skbee/api/listener/BoundBorderListener.java
+++ src/main/java/com/shanebeestudios/skbee/api/listener/BoundBorderListener.java
@@ -7,11 +7,14 @@ import com.shanebeestudios.skbee.elements.bound.config.BoundConfig;
 import com.shanebeestudios.skbee.elements.bound.objects.Bound;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.jetbrains.annotations.NotNull;
+import org.spigotmc.event.entity.EntityMountEvent;
 
 public class BoundBorderListener implements Listener {
 
@@ -26,7 +29,14 @@ public class BoundBorderListener implements Listener {
         Player player = event.getPlayer();
         Location from = event.getFrom();
         Location to = event.getTo();
-        boundMove(player, from, to);
+        if (preventBoundMovement(player, from, to)) {
+            event.setCancelled(true);
+            Entity vehicle = player.getVehicle();
+            if (vehicle != null) {
+                vehicle.removePassenger(player);
+                vehicle.teleport(from);
+            }
+        }
     }
 
     @EventHandler
@@ -34,32 +44,45 @@ public class BoundBorderListener implements Listener {
         Player player = event.getPlayer();
         Location from = event.getFrom();
         Location to = event.getTo();
-        boundMove(player, from, to);
+        if (preventBoundMovement(player, from, to)) {
+            event.setCancelled(true);
+        }
     }
 
-    private void boundMove(Player player, Location from, Location to) {
-        if (to == null || to.equals(from)) {
-            return;
+    @EventHandler
+    private void onMount(EntityMountEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            Location from = player.getLocation();
+            Location to = event.getMount().getLocation();
+            if (preventBoundMovement(player, from, to)) {
+                event.setCancelled(true);
+            }
         }
-        // Only detect movement not head movement
-        if (from.getX() != to.getX() || from.getY() != to.getY() || from.getZ() != to.getZ() || from.getWorld() != to.getWorld()) {
-            for (Bound bound : boundConfig.getBounds()) {
-                if (bound.isInRegion(to) && !bound.isInRegion(from)) {
-                    EnterBoundEvent enterEvent = new EnterBoundEvent(bound, player);
-                    Bukkit.getPluginManager().callEvent(enterEvent);
-                    if (enterEvent.isCancelled()) {
-                        player.teleport(from);
-                    }
+    }
+
+    private boolean preventBoundMovement(@NotNull Player player, @NotNull Location from, @NotNull Location to) {
+        // Only detect body movement not head movement
+        from.setPitch(to.getPitch());
+        from.setYaw(to.getYaw());
+        if (to.equals(from)) return false;
+
+        for (Bound bound : boundConfig.getBounds()) {
+            if (bound.isInRegion(to) && !bound.isInRegion(from)) {
+                EnterBoundEvent enterEvent = new EnterBoundEvent(bound, player);
+                Bukkit.getPluginManager().callEvent(enterEvent);
+                if (enterEvent.isCancelled()) {
+                    return true;
                 }
-                if (!bound.isInRegion(to) && bound.isInRegion(from)) {
-                    ExitBoundEvent exitEvent = new ExitBoundEvent(bound, player);
-                    Bukkit.getPluginManager().callEvent(exitEvent);
-                    if (exitEvent.isCancelled()) {
-                        player.teleport(from);
-                    }
+            }
+            if (!bound.isInRegion(to) && bound.isInRegion(from)) {
+                ExitBoundEvent exitEvent = new ExitBoundEvent(bound, player);
+                Bukkit.getPluginManager().callEvent(exitEvent);
+                if (exitEvent.isCancelled()) {
+                    return true;
                 }
             }
         }
+        return false;
     }
 
 }
