2cf01d59d050721e8f40dec9cc77737e00353f6a
ShaneBee <shanebolenback@me.com>
2023-06-14 18:08:50 -0700

    ExprArmorTrim - add new expression and rename other one

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprArmorTrim.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprArmorTrim.java
@@ -1,63 +1,61 @@
 package com.shanebeestudios.skbee.elements.other.expressions;
 
-import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import ch.njol.util.coll.CollectionUtils;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
 import com.shanebeestudios.skbee.elements.other.type.Types;
 import org.bukkit.event.Event;
-import org.bukkit.inventory.meta.ArmorMeta;
 import org.bukkit.inventory.meta.trim.ArmorTrim;
+import org.bukkit.inventory.meta.trim.TrimMaterial;
+import org.bukkit.inventory.meta.trim.TrimPattern;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Name("ArmorTrim - Item")
-@Description({"Represents the armor trim of an item. You can get, set, add or delete/reset.",
-        "Requires Minecraft 1.19.4+"})
-@Examples({"add armorTrim(gold_material, eye_pattern) to armor trim of player's tool",
-        "set armor trim of player's helmet to armorTrim(gold_material, eye_pattern)",
-        "delete armor trim of player's leggings",
-        "reset armor trim of player's boots"})
+@Name("ArmorTrim - Create")
+@Description("Create a new armor trim to be applied to an item. Requires MC 1.19.4+")
+@Examples({"set {_trim} to armor trim using gold_material with eye_pattern",
+        "add armor trim using gold_material with eye_pattern to armor trim of player's helmet"})
 @Since("INSERT VERSION")
-public class ExprArmorTrim extends SimplePropertyExpression<ItemType, ArmorTrim> {
+public class ExprArmorTrim extends SimpleExpression<ArmorTrim> {
 
     static {
         if (Types.HAS_ARMOR_TRIM) {
-            register(ExprArmorTrim.class, ArmorTrim.class, "armor trim", "itemtypes");
+            Skript.registerExpression(ExprArmorTrim.class, ArmorTrim.class, ExpressionType.COMBINED,
+                    "[new] armor trim (of|from|using) %trimmaterial% (and|with) %trimpattern%");
         }
     }
 
+    private Expression<TrimMaterial> trimMaterial;
+    private Expression<TrimPattern> trimPattern;
+
+    @SuppressWarnings({"unchecked", "NullableProblems"})
     @Override
-    public @Nullable ArmorTrim convert(ItemType itemType) {
-        if (itemType.getItemMeta() instanceof ArmorMeta armorMeta) {
-            return armorMeta.getTrim();
-        }
-        return null;
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.trimMaterial = (Expression<TrimMaterial>) exprs[0];
+        this.trimPattern = (Expression<TrimPattern>) exprs[1];
+        return true;
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
-    public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
-        if (mode == ChangeMode.DELETE || mode == ChangeMode.RESET) return CollectionUtils.array();
-        if (mode == ChangeMode.SET || mode == ChangeMode.ADD) return CollectionUtils.array(ArmorTrim.class);
-        return null;
+    protected @Nullable ArmorTrim[] get(Event event) {
+        TrimMaterial trimMaterial = this.trimMaterial.getSingle(event);
+        TrimPattern trimPattern = this.trimPattern.getSingle(event);
+        if (trimMaterial == null || trimPattern == null) return null;
+
+        return new ArmorTrim[]{new ArmorTrim(trimMaterial, trimPattern)};
     }
 
-    @SuppressWarnings({"NullableProblems", "ConstantValue"})
     @Override
-    public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
-        ArmorTrim trim = null;
-        if (delta != null && delta[0] instanceof ArmorTrim armorTrim) trim = armorTrim;
-        for (ItemType itemType : getExpr().getArray(event)) {
-            if (itemType.getItemMeta() instanceof ArmorMeta armorMeta) {
-                armorMeta.setTrim(trim);
-                itemType.setItemMeta(armorMeta);
-            }
-        }
+    public boolean isSingle() {
+        return true;
     }
 
     @Override
@@ -66,8 +64,8 @@ public class ExprArmorTrim extends SimplePropertyExpression<ItemType, ArmorTrim>
     }
 
     @Override
-    protected @NotNull String getPropertyName() {
-        return "armor trim";
+    public @NotNull String toString(@Nullable Event e, boolean d) {
+        return "armor trim using " + this.trimMaterial.toString(e, d) + " and " + this.trimPattern.toString(e, d);
     }
 
 }
