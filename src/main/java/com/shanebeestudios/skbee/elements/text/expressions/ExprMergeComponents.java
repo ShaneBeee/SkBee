1849316e77ff2c588a492fe0ec6649d4e81bfe60
ShaneBee <shanebolenback@me.com>
2023-07-08 13:49:37 -0700

    ExprMergeComponents - add strings as an option

--- src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprMergeComponents.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprMergeComponents.java
@@ -16,24 +16,24 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
 
 @Name("Text Component - Merge Components")
-@Description("Merge multiple components into one.")
+@Description("Merge multiple components into one. If adding strings/texts, they will be converted into components.")
 @Examples({"set {_t} to merge components {_t::*}",
         "set {_t} to merge components {_t::*} joined with newline"})
-@Since("2.4.0, 2.8.5 (delimiter)")
+@Since("2.4.0, 2.8.5 (delimiter), INSERT VERSION (strings)")
 public class ExprMergeComponents extends SimpleExpression<ComponentWrapper> {
 
     static {
         Skript.registerExpression(ExprMergeComponents.class, ComponentWrapper.class, ExpressionType.SIMPLE,
-                "merge components %textcomponents% [[join[ed]] with %-string%]");
+                "merge components %textcomponents/strings% [[join[ed]] with %-string%]");
     }
 
-    private Expression<ComponentWrapper> components;
+    private Expression<?> components;
     private Expression<String> delimiter;
 
     @SuppressWarnings({"NullableProblems", "unchecked"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        this.components = (Expression<ComponentWrapper>) exprs[0];
+        this.components = exprs[0];
         this.delimiter = (Expression<String>) exprs[1];
         return true;
     }
@@ -43,7 +43,13 @@ public class ExprMergeComponents extends SimpleExpression<ComponentWrapper> {
     protected @Nullable ComponentWrapper[] get(Event event) {
         if (this.components == null)
             return null;
-        ComponentWrapper[] components = this.components.getArray(event);
+        Object[] array = this.components.getArray(event);
+        ComponentWrapper[] components = new ComponentWrapper[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] instanceof ComponentWrapper componentWrapper) components[i] = componentWrapper;
+            else if (array[i] instanceof String string) components[i] = ComponentWrapper.fromText(string);
+            else ComponentWrapper.empty();
+        }
         if (this.delimiter != null)
             return new ComponentWrapper[]{ComponentWrapper.fromComponents(components, this.delimiter.getSingle(event))};
         return new ComponentWrapper[]{ComponentWrapper.fromComponents(components)};
