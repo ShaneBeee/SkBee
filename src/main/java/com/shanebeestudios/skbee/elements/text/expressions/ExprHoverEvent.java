8b3c8e1ae027d7ff6a7c405f2aec43b9ce421dc4
ShaneBee <shanebolenback@me.com>
2023-07-08 14:35:25 -0700

    ExprHoverEvent - finally fixed not needing 2 patterns

--- src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprHoverEvent.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprHoverEvent.java
@@ -10,50 +10,44 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.skript.log.ErrorQuality;
 import ch.njol.util.Kleenean;
+import ch.njol.util.StringUtils;
 import com.shanebeestudios.skbee.api.wrapper.ComponentWrapper;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.nbt.api.BinaryTagHolder;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.event.HoverEvent.Action;
+import net.kyori.adventure.text.event.HoverEvent.ShowItem;
 import org.bukkit.event.Event;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 @SuppressWarnings("rawtypes")
 @Name("Text Component - Hover Event")
-@Description({"Create a new hover event. Can show text or an item to a player. 'showing %itemtype%' requires Minecraft 1.18.2+",
-        "When showing an ItemType from a variable, use `...showing item {var}`, this is just a weird quirk of this."})
+@Description({"Create a new hover event. Can show text or an item to a player.",
+        "'showing %itemtype%' requires Minecraft 1.18.2+"})
 @Examples({"set {_t} to text component from \"Check out my cool tool!\"",
         "set hover event of {_t} to a new hover event showing player's tool",
         "send component {_t} to player"})
 @Since("1.5.0")
 public class ExprHoverEvent extends SimpleExpression<HoverEvent> {
 
-    private static final boolean HAS_ITEM_META_NBT = Skript.methodExists(ItemMeta.class, "getAsString");
-
     static {
         Skript.registerExpression(ExprHoverEvent.class, HoverEvent.class, ExpressionType.COMBINED,
-                "[a] [new] hover event showing %strings%",
-                "[a] [new] hover event showing [item] %itemtype%");
+                // TODO scheduled for removal of "item" (july 8/2023)
+                "[a] [new] hover event showing [item] %strings/itemtype%");
     }
 
-    private int pattern;
-    private Expression<Object> object;
+    private Expression<?> object;
 
-    @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
-        if (matchedPattern == 1 && !HAS_ITEM_META_NBT) {
-            Skript.error("'showing itemtype' requires MC 1.18.2+",
-                    ErrorQuality.SEMANTIC_ERROR);
-            return false;
+        this.object = exprs[0];
+        if (parseResult.expr.contains("showing item")) {
+            Skript.warning("'item' is no longer required and subject for removal.");
         }
-        this.pattern = matchedPattern;
-        object = (Expression<Object>) exprs[0];
         return true;
     }
 
@@ -61,25 +55,16 @@ public class ExprHoverEvent extends SimpleExpression<HoverEvent> {
     @Nullable
     @Override
     protected HoverEvent[] get(Event event) {
-        if (object == null) return null;
-
-        if (pattern == 0) {
-            String[] string = ((String[]) this.object.getArray(event));
-            Component texts = Component.empty();
-            for (int i = 0; i < string.length; i++) {
-                Component component = ComponentWrapper.fromText(string[i] + (i < (string.length - 1) ? "\n" : "")).getComponent();
-                texts = texts.append(component);
-            }
-            return new HoverEvent[]{HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, texts)};
-        } else if (pattern == 1) {
-            ItemType itemType = (ItemType) object.getSingle(event);
-            if (itemType == null) return null;
-
+        if (this.object.isSingle() && this.object.getSingle(event) instanceof ItemType itemType) {
             Key key = itemType.getMaterial().key();
             int amount = itemType.getAmount();
             BinaryTagHolder nbt = BinaryTagHolder.binaryTagHolder(itemType.getItemMeta().getAsString());
-            HoverEvent.ShowItem showItem = HoverEvent.ShowItem.of(key, amount, nbt);
-            return new HoverEvent[]{HoverEvent.hoverEvent(HoverEvent.Action.SHOW_ITEM, showItem)};
+            ShowItem showItem = ShowItem.of(key, amount, nbt);
+            return new HoverEvent[]{HoverEvent.hoverEvent(Action.SHOW_ITEM, showItem)};
+        } else if (this.object.getArray(event) instanceof String[] strings) {
+            String join = StringUtils.join(strings, "\n");
+            Component texts = ComponentWrapper.fromText(join).getComponent();
+            return new HoverEvent[]{HoverEvent.hoverEvent(Action.SHOW_TEXT, texts)};
         }
         return null;
     }
