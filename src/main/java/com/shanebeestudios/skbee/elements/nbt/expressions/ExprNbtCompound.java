d360f34b30faad0a74aae70046c2fe0f244dac53
ShaneBee <shanebolenback@me.com>
2023-07-23 12:25:54 -0700

    ExprNbtCompound - remove deprecated "item" option

--- src/main/java/com/shanebeestudios/skbee/elements/nbt/expressions/ExprNbtCompound.java
+++ src/main/java/com/shanebeestudios/skbee/elements/nbt/expressions/ExprNbtCompound.java
@@ -39,8 +39,8 @@ import javax.annotation.Nullable;
         "other entities.",
         "\n'full nbt compound' from an item will return a copy of the FULL NBT of an item (this includes id, count and 'tag' compound).",
         "Modifying this will have no effect on the original item. This is useful for serializing items.",
-        "\n'nbt [item] compound' from an item will be the original. This will return the 'tag' portion of an items full NBT.",
-        "Modifying this will modify the original item. The '[item]' portion is deprecated and will be removed in a future version.",
+        "\n'nbt compound' from an item will be the original. This will return the 'tag' portion of an items full NBT.",
+        "Modifying this will modify the original item.",
         "\nNBT from a file will need to be saved manually using",
         "the 'NBT - Save File effect'. If the file does not yet exist, a new file will be created."})
 @Examples({"set {_n} to nbt compound of player's tool",
@@ -52,9 +52,8 @@ import javax.annotation.Nullable;
 public class ExprNbtCompound extends PropertyExpression<Object, NBTCompound> {
 
     static {
-        // Will remove '[item]' in the future (oct 24/2022)
         Skript.registerExpression(ExprNbtCompound.class, NBTCompound.class, ExpressionType.PROPERTY,
-                "[:full] nbt [:item] [compound] [:copy] (of|from) %blocks/entities/itemtypes/itemstacks/slots/strings/chunks%",
+                "[:full] nbt [compound] [:copy] (of|from) %blocks/entities/itemtypes/itemstacks/slots/strings/chunks%",
                 "nbt [compound] [:copy] (of|from) file[s] %strings%");
     }
 
@@ -68,9 +67,6 @@ public class ExprNbtCompound extends PropertyExpression<Object, NBTCompound> {
         isFullItem = parseResult.hasTag("full");
         isCopy = parseResult.hasTag("copy");
         isFile = matchedPattern == 1;
-        if (parseResult.hasTag("item")) {
-            Skript.warning("'nbt item compound' has been deprecated ... please just use 'nbt compound'");
-        }
         return true;
     }
 
