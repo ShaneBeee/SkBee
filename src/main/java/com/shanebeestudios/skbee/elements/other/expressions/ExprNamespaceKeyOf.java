24c441aca7d246c605fc1bf41524bba303d7278e
Fusezion <fusezionstream@gmail.com>
2023-08-09 16:47:01 -0400

    ExprNamespaceKeyOf - use 'this' in respective places

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNamespaceKeyOf.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNamespaceKeyOf.java
@@ -15,38 +15,38 @@ import org.jetbrains.annotations.Nullable;
 @Description({"Gets the namespace or namespace key of a NamespacedKey object."})
 @Examples({"set {_items::*} to namespacedkey of every sword",
 		"loop {_items::*}:",
-		"\tbroadcast loop-value",
-		"\tbroadcast namespace of loop-value",
-		"\tbroadcast namespace key of loop-value"})
+        "\tbroadcast loop-value",
+        "\tbroadcast namespace of loop-value",
+        "\tbroadcast namespace key of loop-value"})
 @Since("INSERT VERSION")
 public class ExprNamespaceKeyOf extends SimplePropertyExpression<NamespacedKey, String> {
 
-	static {
-		register(ExprNamespaceKeyOf.class, String.class, "namespace [:key]", "namespacedkeys");
-	}
+    static {
+        register(ExprNamespaceKeyOf.class, String.class, "namespace [:key]", "namespacedkeys");
+    }
 
-	private boolean key;
+    private boolean key;
 
-	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		key = parseResult.hasTag("key");
-		return super.init(exprs, matchedPattern, isDelayed, parseResult);
-	}
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.key = parseResult.hasTag("key");
+        return super.init(exprs, matchedPattern, isDelayed, parseResult);
+    }
 
-	@Override
-	@Nullable
-	public String convert(NamespacedKey namespacedKey) {
-		return key ? namespacedKey.getKey() : namespacedKey.getNamespace();
-	}
+    @Override
+    @Nullable
+    public String convert(NamespacedKey namespacedKey) {
+        return this.key ? namespacedKey.getKey() : namespacedKey.getNamespace();
+    }
 
-	@Override
-	public Class<? extends String> getReturnType() {
-		return String.class;
-	}
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
 
-	@Override
-	protected String getPropertyName() {
-		return "namespace" + (key ? " key" : "");
-	}
+    @Override
+    protected String getPropertyName() {
+        return "namespace" + (this.key ? " key" : "");
+    }
 
 }
