5012d0ce364f3fe865978c10c94e1f6d1cb6aab0
Fusezion <fusezionstream@gmail.com>
2023-08-19 10:06:07 -0400

    SecClickEventCallback - Some minor cleanup changes (#475)
    * SecClickEventCallback - Cleanup
    Changed from EffectSection to Section
    - Unsure if this is to add a custom error message, but it shouldn't be done.
    Clean up some internals to be less messy
    - Remove some messy code caused by EffectSection
    Add another alternative to lifetime wording
    - Duration makes as much sense as lifetime sometimes more sense
    Add more examples on top of existing to showcase 'uses' and 'lifetime'
    
    * SecClickEventCallback - revert event rename
    
    * SecClickEventCallback - revert some other unnecessary changes
    
    * SecClickEventCallback - remove some extra whitespace

--- src/main/java/com/shanebeestudios/skbee/elements/text/sections/SecClickEventCallback.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/sections/SecClickEventCallback.java
@@ -6,8 +6,8 @@ import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.EffectSection;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.Section;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.Trigger;
 import ch.njol.skript.lang.TriggerItem;
@@ -31,24 +31,30 @@ import java.util.List;
 @Description({"Create a click event, that when clicked will run the code in this section.",
         "\nNOTE: Internally this just makes the player run a special command",
         "so you will see console messages where a player runs the command \"/paper callback\" in your console.",
-        "\nNOTE: Paper didn't make this command avaiable by default",
+        "\nNOTE: Paper didn't make this command available by default",
         "so you'll have to give your players the permission `bukkit.command.paper.callback`.",
         "\n`uses` = The amount of times the player can click this. Defaults to 1. Use `-1` for unlimited uses.",
         "\n`lifetime` = How long the player has til they can't click it. Defaults to 12 hours."})
-@Examples({"set {_t} to mini message from \"JOIN US AT SPAWN FOR A SPECIAL EVENT\"",
-        "create callback for {_t}:",
+@Examples({"set {_t} to mini message from \"JOIN US AT SPAWN FOR A SPECIAL EVENT (10 SECONDS REMAINING!)\"",
+        "create callback for {_t} with a duration of 10 seconds:",
         "\tteleport player to spawn of world \"world\"",
         "send component {_t} to all players",
         "",
         "set {_t} to text component from \"&cDONT CLICK ME\"",
-        "create callback for {_t}:",
+        "create callback for {_t} with (size of players) uses:",
         "\tkill player",
         "\tbroadcast \"&b%player% &eclicked it &cAND DIED&7!!!\"",
-        "send component {_t} to all players"})
+        "send component {_t} to all players",
+        "",
+        "set {_t} to text component from \"Hey you! Click this for a free item.\"",
+        "set {_t2} to text component from \"&e&lONE TIME USE!\"",
+        "create a callback for {_t}:",
+        "\tgive player random item out of available item types",
+        "send components (merge components {_t} and {_t2} with \" \") to players"})
 @Since("INSERT VERSION")
-public class SecClickEventCallback extends EffectSection {
+public class SecClickEventCallback extends Section {
 
-    static class ComponentCallbackEvent extends PlayerEvent {
+    private static class ComponentCallbackEvent extends PlayerEvent {
 
         public ComponentCallbackEvent(Player player) {
             super(player);
@@ -64,57 +70,49 @@ public class SecClickEventCallback extends EffectSection {
     static {
         Skript.registerSection(SecClickEventCallback.class,
                 "create [a] [new] [click event] callback for %textcomponent% " +
-                        "[with %-number% use[s]] [[and] with lifetime of %-timespan%]");
+                        "[with %-number% use[s]] [[and] with [a] (lifetime|duration) of %-timespan%]");
     }
 
     private Expression<ComponentWrapper> component;
     private Expression<Number> uses;
-    private Expression<Timespan> timeLimit;
+    private Expression<Timespan> lifeTime;
     private Trigger trigger;
 
-    @SuppressWarnings({"NullableProblems", "unchecked"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult,
-                        @Nullable SectionNode sectionNode, @Nullable List<TriggerItem> triggerItems) {
+                        SectionNode sectionNode, List<TriggerItem> triggerItems) {
         this.component = (Expression<ComponentWrapper>) exprs[0];
         this.uses = (Expression<Number>) exprs[1];
-        this.timeLimit = (Expression<Timespan>) exprs[2];
-        if (sectionNode != null) {
-            this.trigger = loadCode(sectionNode, "callback", ComponentCallbackEvent.class);
-        } else {
-            Skript.error("`click event callback` can only be used as a section.");
-            return false;
-        }
+        this.lifeTime = (Expression<Timespan>) exprs[2];
+        this.trigger = loadCode(sectionNode, "callback", ComponentCallbackEvent.class);
         return true;
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     protected @Nullable TriggerItem walk(Event event) {
         ComponentWrapper component = this.component.getSingle(event);
-        if (this.trigger != null && component != null) {
-            Object localVars = Variables.copyLocalVariables(event);
-
+        if (component != null) {
             int uses = 1;
             if (this.uses != null) {
                 Number usesNum = this.uses.getSingle(event);
                 if (usesNum != null) uses = usesNum.intValue();
             }
 
-            Duration timeLimit = ClickCallback.DEFAULT_LIFETIME;
-            if (this.timeLimit != null) {
-                Timespan timespan = this.timeLimit.getSingle(event);
-                if (timespan != null) timeLimit = Duration.ofMillis(timespan.getTicks_i() * 50);
+            Duration lifeTime = ClickCallback.DEFAULT_LIFETIME;
+            if (this.lifeTime != null) {
+                Timespan lifeTimeSpan = this.lifeTime.getSingle(event);
+                if (lifeTimeSpan != null) lifeTime = Duration.ofMillis(lifeTimeSpan.getMilliSeconds());
             }
 
+            Object localVariables = Variables.copyLocalVariables(event);
             component.setClickEvent(ClickEvent.callback(audience -> {
                 Player player = audience instanceof Player p ? p : null;
                 ComponentCallbackEvent callbackEvent = new ComponentCallbackEvent(player);
-                Variables.setLocalVariables(callbackEvent, localVars);
+                Variables.setLocalVariables(callbackEvent, localVariables);
                 TriggerItem.walk(this.trigger, callbackEvent);
                 Variables.setLocalVariables(event, Variables.copyLocalVariables(callbackEvent));
                 Variables.removeLocals(callbackEvent);
-            }, ClickCallback.Options.builder().uses(uses).lifetime(timeLimit).build()));
+            }, ClickCallback.Options.builder().uses(uses).lifetime(lifeTime).build()));
         }
         return super.walk(event, false);
     }
@@ -122,7 +120,7 @@ public class SecClickEventCallback extends EffectSection {
     @Override
     public @NotNull String toString(@Nullable Event e, boolean d) {
         String uses = this.uses != null ? (" with " + this.uses.toString(e, d) + " uses") : "";
-        String time = this.timeLimit != null ? (" with lifetime of " + this.timeLimit.toString(e, d)) : "";
+        String time = this.lifeTime != null ? (" with lifetime of " + this.lifeTime.toString(e, d)) : "";
         return "create click event callback for " + this.component.toString(e, d) + uses + time;
     }
 
