8bf7e153173ecd40a03656a9e723c4d67e49d347
ShaneBee <shanebolenback@me.com>
2023-09-03 17:49:04 -0700

    CondPlayerIsConnected - add new condition to check if a player/offlineplayer is connected

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/conditions/CondPlayerIsConnected.java
@@ -0,0 +1,40 @@
+package com.shanebeestudios.skbee.elements.other.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+@Name("OfflinePlayer is Connected")
+@Description({"Checks whether the connection to this player is still valid.",
+        " This will return true as long as this specific instance of the player is still connected.",
+        "This will return false after this instance has disconnected, even if the same player has reconnected since.",
+        "\nNOTE: This will be better for while loops to prevent stacking loops if the player relogs within your wait time.",
+        "\nRequires PaperMC 1.20.1[build-161]+"})
+@Examples({"while player is connected:",
+        "\tgive player a diamond",
+        "\twait 1 minute"})
+@Since("INSERT VERSION")
+public class CondPlayerIsConnected extends PropertyCondition<OfflinePlayer> {
+
+    static {
+       if (Skript.methodExists(OfflinePlayer.class, "isConnected")) {
+           register(CondPlayerIsConnected.class, PropertyType.BE, "connected", "offlineplayers");
+       }
+    }
+
+    @Override
+    public boolean check(OfflinePlayer offlinePlayer) {
+        return offlinePlayer.isConnected();
+    }
+
+    @Override
+    protected @NotNull String getPropertyName() {
+        return "connected";
+    }
+
+}
