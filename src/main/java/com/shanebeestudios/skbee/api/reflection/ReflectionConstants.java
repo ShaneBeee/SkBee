f75200d3aca39220792ce0f847e9319924321994
ShaneBee <shanebolenback@me.com>
2023-09-12 23:56:30 -0700

    ReflectionConstants - prep for 1.20.2 - also a little cleanup of constants that haven't changed between versions

--- src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
+++ src/main/java/com/shanebeestudios/skbee/api/reflection/ReflectionConstants.java
@@ -8,23 +8,24 @@ import ch.njol.skript.Skript;
 public class ReflectionConstants {
 
     // net.minecraft.nbt.TextComponentTagVisitor -> visit(Tag)
-    public static String TAG_VISITOR_VISIT_METHOD = get("a", "a", "a", "a");
+    public static String TAG_VISITOR_VISIT_METHOD = "a";
     // net.minecraft.world.entity.Entity -> noPhysics
-    public static String ENTITY_NO_PHYSICS_FIELD = get("P", "Q", "Q", "ae");
+    public static String ENTITY_NO_PHYSICS_FIELD = get("Q", "Q", "ae", "af");
     // net.minecraft.world.scores.PlayerTeam -> setPlayerPrefix
-    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = get("setPrefix", "b", "b", "b");
+    public static String NMS_SCOREBOARD_TEAM_SET_PREFIX_METHOD = "b";
     // net.minecraft.world.scores.PlayerTeam -> setPlayerSuffix
-    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = get("setSuffix", "c", "c", "c");
+    public static String NMS_SCOREBOARD_TEAM_SET_SUFFIX_METHOD = "c";
 
-    private static String get(String v117, String v118, String v119, String v1194) {
-        if (Skript.isRunningMinecraft(1,19,4)) {
+    @SuppressWarnings("SameParameterValue")
+    private static String get(String v118, String v119, String v1194, String v1202) {
+        if (Skript.isRunningMinecraft(1, 20, 2)) {
+            return v1202;
+        } else if (Skript.isRunningMinecraft(1, 19, 4)) {
             return v1194;
         } else if (Skript.isRunningMinecraft(1, 19)) {
             return v119;
         } else if (Skript.isRunningMinecraft(1, 18)) {
             return v118;
-        } else if (Skript.isRunningMinecraft(1, 17)) {
-            return v117;
         }
         throw new IllegalArgumentException("Unknown Version: " + Skript.getMinecraftVersion());
     }
