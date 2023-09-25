d2a459eb1d7a813d6a4e81a2c762ed9fa4ef0915
Fusezion <fusezionstream@gmail.com>
2023-09-25 01:13:47 -0400

    Comparators - remove reviewer notes

--- src/main/java/com/shanebeestudios/skbee/elements/other/type/Comparators.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/type/Comparators.java
@@ -5,12 +5,9 @@ import org.bukkit.inventory.meta.trim.TrimPattern;
 import org.skriptlang.skript.lang.comparator.Comparator;
 import org.skriptlang.skript.lang.comparator.Relation;
 
-// Reviewer note: re-added this class since I was not sure if we wanted to add it to 'Types'
 public class Comparators {
 
     static {
-        // Reviewer note: Skript has updated their comparators in 2.7, allowing us to use a lambda over a new class
-        // I couldn't find any other comparators in SkBee, so I haven't updated them if they exist
         if (Types.HAS_ARMOR_TRIM) {
             register(TrimPattern.class, TrimPattern.class, (pattern1,pattern2) -> Relation.get(pattern1.equals(pattern2)));
             register(TrimMaterial.class, TrimMaterial.class, (material1, material2) -> Relation.get(material1.equals(material2)));
