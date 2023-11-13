8339497e5112cefe36fa69b88c826220bdc86cf3
ShaneBee <shanebolenback@me.com>
2023-11-13 09:20:15 -0800

    EffBlockRandomlyTick - add effect to randomly tick block

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffBlockRandomlyTick.java
@@ -0,0 +1,54 @@
+package com.shanebeestudios.skbee.elements.other.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Random Tick Block")
+@Description({"Causes the block to be ticked randomly.",
+        "This will tick the block the same way Minecraft randomly ticks according to the randomTickSpeed gamerule.",
+        "Requires Paper 1.19+"})
+@Examples("random tick blocks in radius 3 around target block")
+@Since("INSERT VERSION")
+public class EffBlockRandomlyTick extends Effect {
+
+    static {
+        Skript.registerEffect(EffBlockRandomlyTick.class, "random[ly] tick %blocks%");
+    }
+
+    private Expression<Block> blocks;
+
+    @SuppressWarnings({"unchecked", "NullableProblems"})
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        if (!Skript.methodExists(Block.class, "randomTick")) {
+            Skript.error("`random tick` effect requires Paper 1.19+");
+            return false;
+        }
+        this.blocks = (Expression<Block>) exprs[0];
+        return true;
+    }
+
+    @Override
+    protected void execute(@NotNull Event event) {
+        for (Block block : this.blocks.getArray(event)) {
+            block.randomTick();
+        }
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean d) {
+        return "randomly tick " + this.blocks.toString(e, d);
+    }
+
+}
