bbcbb30c6033d3dc6ce2327bac89b76f7934273e
ShaneBee <shanebolenback@me.com>
2023-12-18 10:14:06 -0800

    CondServerTickFrozen - add option for running normally

--- src/main/java/com/shanebeestudios/skbee/elements/tickmanager/conditions/CondServerTickFrozen.java
+++ src/main/java/com/shanebeestudios/skbee/elements/tickmanager/conditions/CondServerTickFrozen.java
@@ -11,38 +11,49 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import com.shanebeestudios.skbee.api.util.Util;
 import org.bukkit.Bukkit;
+import org.bukkit.ServerTickManager;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Name("Server Tick - Is Frozen")
-@Description({"Checks if the server is currently frozen.", Util.MCWIKI_TICK_COMMAND, "Requires Minecraft 1.20.4+"})
+@Name("Server Tick - Is Frozen/Running Normally")
+@Description({"Checks if the server is currently frozen/running normally.",
+        "When the server is running normally it indicates that the server is not currently frozen.",
+        Util.MCWIKI_TICK_COMMAND, "Requires Minecraft 1.20.4+"})
 @Examples({"if server is frozen:",
-        "\tteleport all players to spawn of world \"world\""})
+        "\tteleport all players to spawn of world \"world\"",
+        "if server is running normally:",
+        "\tkill all sheep"})
 @Since("INSERT VERSION")
 public class CondServerTickFrozen extends Condition {
 
     static {
-        Skript.registerCondition(CondServerTickFrozen.class, "(server|game) (is|neg:(isn't|is not)) frozen");
+        Skript.registerCondition(CondServerTickFrozen.class,
+                "(server|game) (is|neg:(isn't|is not)) [currently] (frozen|normal:running normally)");
     }
+
+    private boolean normal;
 
     @SuppressWarnings("NullableProblems")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         setNegated(parseResult.hasTag("neg"));
+        this.normal = parseResult.hasTag("normal");
         return true;
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
     public boolean check(Event event) {
-        return !isNegated() == Bukkit.getServerTickManager().isFrozen();
+        ServerTickManager tickManager = Bukkit.getServerTickManager();
+        return !isNegated() == (this.normal ? tickManager.isRunningNormally() : tickManager.isFrozen());
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
         String is = isNegated() ? "is not" : "is";
-        return "server " + is + " frozen";
+        String normal = this.normal ? " running normally" : " frozen";
+        return "server " + is + normal;
     }
 
 }
