ddd4a0d3736e5befecc137a3729c730241597ac3
ShaneBee <shanebolenback@me.com>
2023-12-18 00:16:01 -0800

    ExprServerTickFrozenState - add expression to freeze server

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprServerTickFrozenState.java
@@ -0,0 +1,77 @@
+package com.shanebeestudios.skbee.elements.other.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import com.shanebeestudios.skbee.api.util.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Server Tick - Frozen State")
+@Description({"Get/set the frozen tick state of the server.", Util.MCWIKI_TICK_COMMAND, "Requires Minecraft 1.20.4+"})
+@Examples({"set server frozen state to true",
+        "if server frozen state = false:"})
+@Since("INSERT VERSION")
+public class ExprServerTickFrozenState extends SimpleExpression<Boolean> {
+
+    static {
+        if (Skript.classExists("org.bukkit.ServerTickManager")) {
+            Skript.registerExpression(ExprServerTickFrozenState.class, Boolean.class, ExpressionType.SIMPLE,
+                    "server (frozen|freeze) state");
+        }
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        return true;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    protected @Nullable Boolean[] get(Event event) {
+        return new Boolean[]{Bukkit.getServerTickManager().isFrozen()};
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
+        if (mode == ChangeMode.SET) return CollectionUtils.array(Boolean.class);
+        return null;
+    }
+
+    @SuppressWarnings({"ConstantValue", "NullableProblems"})
+    @Override
+    public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+        if (mode == ChangeMode.SET && delta != null && delta[0] instanceof Boolean frozen) {
+            Bukkit.getServerTickManager().setFrozen(frozen);
+        }
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends Boolean> getReturnType() {
+        return Boolean.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "server frozen state";
+    }
+
+}
