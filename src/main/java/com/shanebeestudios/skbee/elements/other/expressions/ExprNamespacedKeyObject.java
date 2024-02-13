4b2c67acf42cd82288b54875f9bbf7d68622483f
ShaneBee <shanebolenback@me.com>
2024-02-13 09:32:55 -0800

    NamespacedKey - stage 1 cleanup, join 2 namespaced key expressions

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNamespacedKeyObject.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprNamespacedKeyObject.java
@@ -1,5 +1,6 @@
 package com.shanebeestudios.skbee.elements.other.expressions;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.bukkitutil.EntityUtils;
 import ch.njol.skript.doc.Description;
@@ -9,37 +10,51 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.entity.EntityData;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.util.LiteralUtils;
 import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.api.util.Util;
 import org.bukkit.Keyed;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-@Name("NamespacedKey - From Object")
-@Description("Get the namespaced key of an object.")
+@Name("NamespacedKey - Get")
+@Description({"Get the namespaced key of an object or string.",
+        "\nNote when getting key from string:",
+        "NamespacedKeys are a string based key which consists of two components - a namespace and a key.",
+        "\nNamespaces may only contain lowercase alphanumeric characters, periods, underscores, and hyphens.",
+        "Minecraft generally uses the \"minecraft\" namespace for built in objects.",
+        "\nIf a namespace is not provided, the SkBee config namespace will be used by default -> \"skbee:your_key\"",
+        "\nKeys may only contain lowercase alphanumeric characters, periods, underscores, hyphens, and forward slashes.",
+        "\nKeep an eye on your console when using namespaced keys as errors will spit out when they're invalid."})
 @Examples({"set {_key} to mc key of target block",
         "set {_key} to namespaced key of player's tool",
-        "set {_key} to minecraft key of biome at player"})
+        "set {_key} to minecraft key of biome at player",
+        "set {_n} to namespaced key from \"minecraft:log\"",
+        "set {_custom} to namespaced key from \"my_server:custom_log\"",
+        "set {_n} to namespaced key from \"le_test\""})
 @Since("2.6.0")
 public class ExprNamespacedKeyObject extends SimplePropertyExpression<Object, NamespacedKey> {
 
     static {
-        register(ExprNamespacedKeyObject.class, NamespacedKey.class,
-                "(mc|minecraft|namespaced|resource)[ ](key|id[entifier]|location)", "objects");
+        Skript.registerExpression(ExprNamespacedKeyObject.class, NamespacedKey.class, ExpressionType.COMBINED,
+                "(mc:(minecraft|mc)|namespaced|resource)[ ](key|id[entifier]|location)[s] [(from|of)] %objects%");
     }
+
+    private boolean useMinecraftNamespace;
 
     @SuppressWarnings("NullableProblems")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.useMinecraftNamespace = parseResult.hasTag("mc");
         Expression<Object> objects = LiteralUtils.defendExpression(exprs[0]);
         setExpr(objects);
         return LiteralUtils.canInitSafely(objects);
@@ -48,7 +63,13 @@ public class ExprNamespacedKeyObject extends SimplePropertyExpression<Object, Na
     @SuppressWarnings("ConstantValue")
     @Override
     public @Nullable NamespacedKey convert(Object object) {
-        if (object instanceof Keyed keyed) {
+        if (object instanceof String string) {
+            if (this.useMinecraftNamespace) {
+                return Util.getMCNamespacedKey(string, true);
+            } else {
+                return Util.getNamespacedKey(string, true);
+            }
+        } else if (object instanceof Keyed keyed) {
             return keyed.getKey();
         } else if (object instanceof Block block) {
             return block.getBlockData().getMaterial().getKey();
