acc6d08a9f2f7663c288dee740459d048e10888b
ShaneBee <shanebolenback@me.com>
2024-02-14 08:35:20 -0800

    DamageSource/Types - do classinfo check

--- src/main/java/com/shanebeestudios/skbee/elements/damagesource/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/damagesource/type/Types.java
@@ -11,22 +11,26 @@ public class Types {
 
     static {
         if (Skript.classExists("org.bukkit.damage.DamageSource")) {
-            RegistryWrapper<DamageType> DAMAGE_TYPE_REGISTRY = RegistryWrapper.wrap(DamageType.class);
-            Classes.registerClass(new ClassInfo<>(DamageType.class, "damagetype")
-                    .user("damage ?types?")
-                    .name("Damage Type")
-                    .description("Represents a type of damage.",
-                            "See <link>https://minecraft.wiki/w/Damage_type</link> for more details.",
-                            "Requires MC 1.20.4+")
-                    .usage(DAMAGE_TYPE_REGISTRY.getNames())
-                    .since("INSERT VERSION")
-                    .parser(DAMAGE_TYPE_REGISTRY.getParser()));
+            if (Classes.getExactClassInfo(DamageType.class) == null) {
+                RegistryWrapper<DamageType> DAMAGE_TYPE_REGISTRY = RegistryWrapper.wrap(DamageType.class);
+                Classes.registerClass(new ClassInfo<>(DamageType.class, "damagetype")
+                        .user("damage ?types?")
+                        .name("Damage Type")
+                        .description("Represents a type of damage.",
+                                "See <link>https://minecraft.wiki/w/Damage_type</link> for more details.",
+                                "Requires MC 1.20.4+")
+                        .usage(DAMAGE_TYPE_REGISTRY.getNames())
+                        .since("INSERT VERSION")
+                        .parser(DAMAGE_TYPE_REGISTRY.getParser()));
+            }
 
-            Classes.registerClass(new ClassInfo<>(DamageSource.class, "damagesource")
-                    .user("damage ?sources?")
-                    .name("Damage Source")
-                    .description("Represents a source of damage. Requires MC 1.20.4+")
-                    .since("INSERT VERSION"));
+            if (Classes.getExactClassInfo(DamageSource.class) == null) {
+                Classes.registerClass(new ClassInfo<>(DamageSource.class, "damagesource")
+                        .user("damage ?sources?")
+                        .name("Damage Source")
+                        .description("Represents a source of damage. Requires MC 1.20.4+")
+                        .since("INSERT VERSION"));
+            }
         }
     }
 
