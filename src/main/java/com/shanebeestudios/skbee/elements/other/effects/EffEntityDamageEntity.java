6e7670757d9477aa039a4b711aa6fa116f397658
ShaneBee <shanebolenback@me.com>
2024-02-23 15:49:58 -0800

    EffEntityDamageEntity - remove fake damage cause

--- src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffEntityDamageEntity.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffEntityDamageEntity.java
@@ -9,32 +9,27 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
-import com.shanebeestudios.skbee.api.util.EntityUtils;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Entity Damage Entity")
-@Description({"Make an entity damage another entity by a given amount.",
-        "Optional fake damage cause which you can retrieve with Skript's `last damage cause` expression."})
+@Description("Make an entity damage another entity by a given amount.")
 @Examples({"make last spawned entity damage player by 10",
-        "make target entity damage player by 1 with fake damage cause poison"})
-@Since("2.8.0, 2.18.0 (damage cause)")
+        "make target entity damage player by 1"})
+@Since("2.8.0")
 public class EffEntityDamageEntity extends Effect {
 
     static {
         Skript.registerEffect(EffEntityDamageEntity.class,
-                "make %entity% damage %livingentities% by %number% [with fake [damage] cause %-damagecause%]");
+                "make %entity% damage %livingentities% by %number%");
     }
 
     private Expression<LivingEntity> victims;
     private Expression<Entity> attacker;
     private Expression<Number> damageAmount;
-    private Expression<DamageCause> damageCause;
 
     @SuppressWarnings({"NullableProblems", "unchecked"})
     @Override
@@ -42,7 +37,6 @@ public class EffEntityDamageEntity extends Effect {
         this.attacker = (Expression<Entity>) exprs[0];
         this.victims = (Expression<LivingEntity>) exprs[1];
         this.damageAmount = (Expression<Number>) exprs[2];
-        this.damageCause = (Expression<DamageCause>) exprs[3];
         return true;
     }
 
@@ -55,15 +49,7 @@ public class EffEntityDamageEntity extends Effect {
         Entity attacker = this.attacker.getSingle(event);
         if (attacker == null) return;
 
-        DamageCause damageCause = null;
-        if (this.damageCause != null) {
-            damageCause = this.damageCause.getSingle(event);
-        }
         for (LivingEntity victim : this.victims.getArray(event)) {
-            if (damageCause != null) {
-                EntityDamageEvent entityDamageEvent = EntityUtils.createEntityDamageEvent(attacker, damageCause, damageAmount);
-                victim.setLastDamageCause(entityDamageEvent);
-            }
             victim.damage(damageAmount, attacker);
         }
     }
@@ -74,8 +60,7 @@ public class EffEntityDamageEntity extends Effect {
         String attacker = this.attacker.toString(e, d);
         String victim = this.victims.toString(e, d);
         String amount = this.damageAmount.toString(e, d);
-        String fake = this.damageCause != null ? ("with fake damage cause " + this.damageCause.toString(e, d)) : "";
-        return String.format("make %s damage %s by %s %s", attacker, victim, amount, fake);
+        return String.format("make %s damage %s by %s", attacker, victim, amount);
     }
 
 }
