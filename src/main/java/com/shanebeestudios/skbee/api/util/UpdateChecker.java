15395f26a400dec51f9252efc55f8995f5271765
ShaneBee <shanebolenback@me.com>
2024-03-15 10:47:44 -0700

    UpdateChecker - more cleanup - no longer static, you happy @BaeFell ?

--- src/main/java/com/shanebeestudios/skbee/api/util/UpdateChecker.java
+++ src/main/java/com/shanebeestudios/skbee/api/util/UpdateChecker.java
@@ -10,13 +10,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 /**
  * Utility class to check for plugin updates
@@ -25,28 +25,28 @@ import java.util.function.Consumer;
 public class UpdateChecker implements Listener {
 
     private final SkBee plugin;
-    private Version UPDATE_VERSION;
+    private final Version pluginVersion;
+    private Version currentUpdateVersion;
 
     public UpdateChecker(SkBee plugin) {
         this.plugin = plugin;
+        this.pluginVersion = new Version(plugin.getDescription().getVersion());
+
         Config config = plugin.getPluginConfig();
-        String pluginVersion = plugin.getDescription().getVersion();
         if (config.SETTINGS_UPDATE_CHECKER_ENABLED) {
-            setupListener(pluginVersion);
-            checkUpdate(pluginVersion, config.SETTINGS_UPDATE_CHECKER_ASYNC);
+            setupJoinListener();
+            checkUpdate(config.SETTINGS_UPDATE_CHECKER_ASYNC);
         }
     }
 
-    private void setupListener(String pluginVersion) {
+    private void setupJoinListener() {
         Bukkit.getPluginManager().registerEvents(new Listener() {
             @EventHandler
             private void onJoin(PlayerJoinEvent event) {
                 Player player = event.getPlayer();
                 if (!player.hasPermission("skbee.update.check")) return;
 
-                CompletableFuture<Version> updateVersion = getUpdateVersion(pluginVersion);
-
-                Bukkit.getScheduler().runTaskLater(UpdateChecker.this.plugin, () -> updateVersion.thenApply(version -> {
+                Bukkit.getScheduler().runTaskLater(UpdateChecker.this.plugin, () -> getUpdateVersion(true).thenApply(version -> {
                     Util.sendColMsg(player, "&7[&bSk&3Bee&7] update available: &a" + version);
                     Util.sendColMsg(player, "&7[&bSk&3Bee&7] download at &bhttps://github.com/ShaneBeee/SkBee/releases");
                     return true;
@@ -55,38 +55,62 @@ public class UpdateChecker implements Listener {
         }, this.plugin);
     }
 
-    private void checkUpdate(String version, boolean async) {
-        if (async) {
-            Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> checkForUpdate(version));
-        } else {
-            checkForUpdate(version);
-        }
-    }
-
-    public void checkForUpdate(String pluginVersion) {
+    private void checkUpdate(boolean async) {
         Util.log("Checking for update...");
-        getLatestReleaseVersion(version -> {
-            Version plugVer = new Version(pluginVersion);
-            if (version.compareTo(plugVer) <= 0) {
-                Util.logLoading("&aPlugin is up to date!");
-            } else {
-                Util.logLoading("&cPlugin is not up to date!");
-                Util.logLoading(" - Current version: &cv%s", pluginVersion);
-                Util.logLoading(" - Available update: &av%s", version);
-                Util.logLoading(" - Download available at: https://github.com/ShaneBeee/SkBee/releases");
-                UPDATE_VERSION = version;
-            }
+        getUpdateVersion(async).thenApply(version -> {
+            Util.logLoading("&cPlugin is not up to date!");
+            Util.logLoading(" - Current version: &cv%s", this.pluginVersion);
+            Util.logLoading(" - Available update: &av%s", version);
+            Util.logLoading(" - Download available at: https://github.com/ShaneBeee/SkBee/releases");
+            return true;
+        }).exceptionally(throwable -> {
+            Util.logLoading("&aPlugin is up to date!");
+            return true;
         });
     }
 
+    private CompletableFuture<Version> getUpdateVersion(boolean async) {
+        CompletableFuture<Version> future = new CompletableFuture<>();
+        if (this.currentUpdateVersion != null) {
+            future.complete(this.currentUpdateVersion);
+        } else {
+            getLatestReleaseVersion(async).thenApply(version -> {
+                if (version.compareTo(this.pluginVersion) <= 0) {
+                    future.cancel(true);
+                } else {
+                    this.currentUpdateVersion = version;
+                    future.complete(this.currentUpdateVersion);
+                }
+                return true;
+            });
+        }
+        return future;
+    }
+
+    private CompletableFuture<Version> getLatestReleaseVersion(boolean async) {
+        CompletableFuture<Version> future = new CompletableFuture<>();
+        if (async) {
+            Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+                Version lastest = getLastestVersionFromGitHub();
+                if (lastest == null) future.cancel(true);
+                future.complete(lastest);
+            });
+        } else {
+            Version latest = getLastestVersionFromGitHub();
+            if (latest == null) future.cancel(true);
+            future.complete(latest);
+        }
+        return future;
+    }
+
     @SuppressWarnings("CallToPrintStackTrace")
-    private void getLatestReleaseVersion(final Consumer<Version> consumer) {
+    private @Nullable Version getLastestVersionFromGitHub() {
         try {
             URL url = new URL("https://api.github.com/repos/ShaneBeee/SkBee/releases/latest");
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
             JsonObject jsonObject = new Gson().fromJson(reader, JsonObject.class);
             String tag_name = jsonObject.get("tag_name").getAsString();
-            consumer.accept(new Version(tag_name));
+            return new Version(tag_name);
         } catch (IOException e) {
             if (this.plugin.getPluginConfig().SETTINGS_DEBUG) {
                 e.printStackTrace();
@@ -94,24 +118,7 @@ public class UpdateChecker implements Listener {
                 Util.logLoading("&cChecking for update failed!");
             }
         }
-    }
-
-    private CompletableFuture<Version> getUpdateVersion(String currentVersion) {
-        CompletableFuture<Version> future = new CompletableFuture<>();
-        if (UPDATE_VERSION != null) {
-            future.complete(UPDATE_VERSION);
-        } else {
-            Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> getLatestReleaseVersion(version -> {
-                Version plugVer = new Version(currentVersion);
-                if (version.compareTo(plugVer) <= 0) {
-                    future.cancel(true);
-                } else {
-                    UPDATE_VERSION = version;
-                    future.complete(UPDATE_VERSION);
-                }
-            }));
-        }
-        return future;
+        return null;
     }
 
 }
