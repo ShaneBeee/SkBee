9dfec28aa77a2e2b1b8305714503c1c9a3bbeace
honkling <untitledgeese@gmail.com>
2024-03-25 11:33:10 -0500

    fix: print to stderr instead of stdout

--- src/main/java/com/shanebeestudios/skbee/Main.java
+++ src/main/java/com/shanebeestudios/skbee/Main.java
@@ -14,7 +14,7 @@ public class Main {
             var game = games[(int) Math.floor(Math.random() * games.length)];
             game.getConstructor().newInstance();
         } catch (ReflectiveOperationException ignored) {
-            System.out.println("we failed to pick a game :( defaulting to pong");
+            System.err.println("we failed to pick a game :( defaulting to pong");
             new Pong();
         }
     }
