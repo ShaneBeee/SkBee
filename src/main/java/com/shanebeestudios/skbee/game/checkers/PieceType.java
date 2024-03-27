b3465cad31d61f0c46691fb5f21ade5a9cfbcadb
ShaneBee <shanebolenback@me.com>
2024-03-27 11:34:27 -0700

    PieceType - tint internally - the images nearly doubled the size of skbee, this cuts down the size dramatically

--- src/main/java/com/shanebeestudios/skbee/game/checkers/PieceType.java
+++ src/main/java/com/shanebeestudios/skbee/game/checkers/PieceType.java
@@ -2,22 +2,55 @@ package com.shanebeestudios.skbee.game.checkers;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public enum PieceType {
-    WORKER_BEE(
-            new ImageIcon(Game.class.getResource("/assets/workerbeeRED.png")).getImage(),
-            new ImageIcon(Game.class.getResource("/assets/workerbeeBLUE.png")).getImage()
-    ),
-    QUEEN_BEE(
-            new ImageIcon(Game.class.getResource("/assets/queenbeeRED.png")).getImage(),
-            new ImageIcon(Game.class.getResource("/assets/queenbeeBLUE.png")).getImage()
-    );
+    WORKER_BEE("/assets/workerbee.png", "/assets/workerbee.png"),
+    QUEEN_BEE("/assets/queenbee.png", "/assets/queenbee.png");
 
-    public Image red;
-    public Image blue;
+    public final Image red;
+    public final Image blue;
 
-    PieceType(Image red, Image blue) {
-        this.red = red;
-        this.blue = blue;
+    PieceType(String red, String blue) {
+        BufferedImage buffRed = tint(loadImage(red), new Color(248, 57, 57));
+        this.red = new ImageIcon(buffRed).getImage();
+
+        BufferedImage buffBlue = tint(loadImage(blue), new Color(54, 54, 246));
+        this.blue = new ImageIcon(buffBlue).getImage();
     }
+
+    // Found this on stack overflow
+    // https://stackoverflow.com/questions/4248104/applying-a-tint-to-an-image-in-java
+
+    public BufferedImage loadImage(String location) {
+        URL url = Game.class.getResource(location);
+        assert url != null;
+        ImageIcon icon = new ImageIcon(url);
+        Image image = icon.getImage();
+
+        // Create empty BufferedImage, sized to Image
+        BufferedImage buffImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw Image into BufferedImage
+        Graphics g = buffImage.getGraphics();
+        g.drawImage(image, 0, 0, null);
+        return buffImage;
+    }
+
+    public static BufferedImage tint(BufferedImage image, Color color) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                Color pixelColor = new Color(image.getRGB(x, y), true);
+                int r = (pixelColor.getRed() + color.getRed()) / 2;
+                int g = (pixelColor.getGreen() + color.getGreen()) / 2;
+                int b = (pixelColor.getBlue() + color.getBlue()) / 2;
+                int a = pixelColor.getAlpha();
+                int rgba = (a << 24) | (r << 16) | (g << 8) | b;
+                image.setRGB(x, y, rgba);
+            }
+        }
+        return image;
+    }
+
 }
