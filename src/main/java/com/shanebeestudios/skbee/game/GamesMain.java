48649e163cbc016b42cc9042029fd2d7ca88e8de
ShaneBee <shanebolenback@me.com>
2024-03-27 11:45:16 -0700

    GamesMain - fix up randomizer - Old way seem to favor pong too often

--- src/main/java/com/shanebeestudios/skbee/game/GamesMain.java
+++ src/main/java/com/shanebeestudios/skbee/game/GamesMain.java
@@ -3,6 +3,8 @@ package com.shanebeestudios.skbee.game;
 import com.shanebeestudios.skbee.game.checkers.Checkers;
 import com.shanebeestudios.skbee.game.pong.Pong;
 
+import java.util.Random;
+
 public class GamesMain {
     public static void main(String[] args) {
         var games = new Class<?>[] {
@@ -11,7 +13,7 @@ public class GamesMain {
         };
 
         try {
-            var game = games[(int) Math.floor(Math.random() * games.length)];
+            var game = games[new Random().nextInt(games.length)];
             game.getConstructor().newInstance();
         } catch (ReflectiveOperationException ignored) {
             System.err.println("we failed to pick a game :( defaulting to pong");
