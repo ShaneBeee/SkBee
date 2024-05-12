e9962256295a0275988bf317e86f28463d60ef2a
ShaneBee <shanebolenback@me.com>
2024-05-11 20:40:23 -0700

    ExprDamageSourceEvent - add death event - this was recently added to Bukkit

--- src/main/java/com/shanebeestudios/skbee/elements/damagesource/expressions/ExprDamageSourceEvent.java
+++ src/main/java/com/shanebeestudios/skbee/elements/damagesource/expressions/ExprDamageSourceEvent.java
@@ -14,28 +14,38 @@ import ch.njol.util.Kleenean;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("DamageSource - Event Value")
-@Description("Get the damage source of a damage event.")
+@Description("Get the damage source of a damage/death event.")
 @Examples({"on damage of player:",
-        "\tif damage type of damage source = arrow:",
-        "\t\tbroadcast \"OUCHIE\"",
-        "\tif causing entity of damage source is a chicken:",
-        "\t\tbroadcast \"YOU JERK\""})
+    "\tif damage type of damage source = arrow:",
+    "\t\tbroadcast \"OUCHIE\"",
+    "\tif causing entity of damage source is a chicken:",
+    "\t\tbroadcast \"YOU JERK\""})
 @Since("3.3.0")
 public class ExprDamageSourceEvent extends SimpleExpression<DamageSource> {
 
+    private static final boolean DEATH_EVENT_HAS_DAMAGE_SOURCE = Skript.methodExists(EntityDeathEvent.class, "getDamageSource");
+
     static {
         Skript.registerExpression(ExprDamageSourceEvent.class, DamageSource.class, ExpressionType.SIMPLE,
-                "damage source");
+            "[the] damage source");
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        return ParserInstance.get().isCurrentEvent(EntityDamageEvent.class);
+        if (DEATH_EVENT_HAS_DAMAGE_SOURCE) {
+            if (ParserInstance.get().isCurrentEvent(EntityDamageEvent.class, EntityDeathEvent.class)) return true;
+            Skript.error("'damage source' can only be used in a death/damage event");
+            return false;
+        }
+        if (ParserInstance.get().isCurrentEvent(EntityDamageEvent.class)) return true;
+        Skript.error("'damage source' can only be used in a damage event");
+        return false;
     }
 
     @SuppressWarnings("NullableProblems")
@@ -43,6 +53,8 @@ public class ExprDamageSourceEvent extends SimpleExpression<DamageSource> {
     protected DamageSource @Nullable [] get(Event event) {
         if (event instanceof EntityDamageEvent entityDamageEvent) {
             return new DamageSource[]{entityDamageEvent.getDamageSource()};
+        } else if (DEATH_EVENT_HAS_DAMAGE_SOURCE && event instanceof EntityDeathEvent entityDeathEvent) {
+            return new DamageSource[]{entityDeathEvent.getDamageSource()};
         }
         return null;
     }
