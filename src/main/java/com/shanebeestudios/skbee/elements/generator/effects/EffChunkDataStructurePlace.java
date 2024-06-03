2239fb2187ad9bef38d60c62983d1299700420ac
ShaneBee <shanebolenback@me.com>
2024-06-03 16:36:26 -0700

    EffChunkDataStructurePlace - use chunk pos instead of world pos

--- src/main/java/com/shanebeestudios/skbee/elements/generator/effects/EffChunkDataStructurePlace.java
+++ src/main/java/com/shanebeestudios/skbee/elements/generator/effects/EffChunkDataStructurePlace.java
@@ -9,25 +9,27 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
-import com.shanebeestudios.skbee.api.structure.StructureWrapper;
 import com.shanebeestudios.skbee.api.generator.event.BlockPopulateEvent;
+import com.shanebeestudios.skbee.api.structure.StructureWrapper;
+import com.shanebeestudios.skbee.api.util.MathUtil;
 import org.bukkit.event.Event;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 @Name("ChunkGenerator - Structure Place")
 @Description({"Place a structure in a block populator.",
-        "Due to the chunk not being finalized yet,",
-        "the standard structure place effect will not work during generation.",
-        "Since the chunk isn't finalized yet, we use a vector instead of a location,",
-        "but it's treated the same as a location."})
-@Examples("place chunkdata structure {-s} at vector({_x}, {_y}, {_z})")
+    "Due to the chunk not being finalized yet,",
+    "the standard structure place effect will not work during generation.",
+    "Since the chunk isn't finalized yet, we use a vector instead of a location,",
+    "the vector will represent a chunk position not a world position."})
+@Examples({"place chunkdata structure {-s} at vector({_x}, {_y}, {_z})",
+    "place chunkdata structure {-s} at vector(0,64,0)"})
 @Since("3.5.0")
 public class EffChunkDataStructurePlace extends Effect {
 
     static {
         Skript.registerEffect(EffChunkDataStructurePlace.class,
-                "place chunk[ ]data structure %structure% at %vector%");
+            "place chunk[ ]data structure %structure% at %vector%");
     }
 
     private Expression<StructureWrapper> structure;
@@ -44,12 +46,14 @@ public class EffChunkDataStructurePlace extends Effect {
     @SuppressWarnings("NullableProblems")
     @Override
     protected void execute(Event event) {
-        if (!(event instanceof BlockPopulateEvent populateEvent)) return;
+        if (!(event instanceof BlockPopulateEvent popEvent)) return;
         StructureWrapper structure = this.structure.getSingle(event);
         Vector vector = this.vector.getSingle(event);
         if (structure == null || vector == null) return;
 
-        structure.place(populateEvent.getLimitedRegion(), vector);
+        vector.setX((popEvent.getChunkX() << 4) + MathUtil.clamp(vector.getBlockX(), 0, 15));
+        vector.setZ((popEvent.getChunkZ() << 4) + MathUtil.clamp(vector.getBlockZ(), 0, 15));
+        structure.place(popEvent.getLimitedRegion(), vector);
     }
 
     @Override
