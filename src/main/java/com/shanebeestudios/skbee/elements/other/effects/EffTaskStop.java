95dc95029a3e43214f80056646e3fbf91fe84258
shanebeee <shanebolenback@me.com>
2024-06-30 22:28:45 -0700

    EffTaskStop - rework and cleanup - Ref #594 (hopefully it actually fixes this issue)

--- src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffTaskStop.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffTaskStop.java
@@ -8,29 +8,31 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.TriggerSection;
-import ch.njol.skript.lang.parser.ParserInstance;
 import ch.njol.util.Kleenean;
 import com.shanebeestudios.skbee.elements.other.sections.SecRunTaskLater;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 @Name("Task - Cancel Task")
-@Description("Stop tasks.")
+@Description({"Stop tasks.",
+    "`stop all tasks` = Will stop all currently running tasks created with a task section.",
+    "`stop current task` = Will stop the task section this effect is in.",
+    "`stop task with id` = Will stop any task from an ID."})
 @Examples({"run 0 ticks later repeating every second:",
-        "\tset {-id} to current task id",
-        "\tadd 1 to {_a}",
-        "\tif {_a} > 10:",
-        "\t\tstop current task",
-        "",
-        "on unload:",
-        "\tstop all tasks",
-        "",
-        "on break:",
-        "\tstop task with id {-id}"})
+    "\tset {-id} to current task id",
+    "\tadd 1 to {_a}",
+    "\tif {_a} > 10:",
+    "\t\tstop current task",
+    "",
+    "on unload:",
+    "\tstop all tasks",
+    "",
+    "on break:",
+    "\tstop task with id {-id}"})
 @Since("3.3.0")
 public class EffTaskStop extends Effect {
 
@@ -38,11 +40,12 @@ public class EffTaskStop extends Effect {
 
     static {
         Skript.registerEffect(EffTaskStop.class,
-                "stop all tasks", "stop current task", "stop task[s] with id[s] %numbers%");
+            "(stop|cancel) all tasks", "(stop|cancel) current task", "(stop|cancel) task[s] with id[s] %numbers%");
     }
 
     private int pattern;
     private Expression<Number> ids;
+    private SecRunTaskLater currentTask;
 
     @SuppressWarnings({"NullableProblems", "unchecked"})
     @Override
@@ -50,13 +53,13 @@ public class EffTaskStop extends Effect {
         this.pattern = matchedPattern;
         if (matchedPattern == 2) {
             this.ids = (Expression<Number>) exprs[0];
-        }
-        if (matchedPattern == 1) {
-            for (TriggerSection currentSection : ParserInstance.get().getCurrentSections()) {
-                if (currentSection instanceof SecRunTaskLater) return true;
+        } else if (matchedPattern == 1) {
+            List<SecRunTaskLater> currentSections = getParser().getCurrentSections(SecRunTaskLater.class);
+            if (currentSections.isEmpty()) {
+                Skript.error("'" + parseResult.expr + "' can only be used in a run task section.");
+                return false;
             }
-            Skript.error("'stop current task' can only be used in a run task section.");
-            return false;
+            this.currentTask = currentSections.getLast();
         }
         return true;
     }
@@ -66,16 +69,7 @@ public class EffTaskStop extends Effect {
     protected void execute(Event event) {
         switch (this.pattern) {
             case 0 -> SecRunTaskLater.cancelTasks();
-            case 1 -> {
-                TriggerSection parent = getParent();
-                while (parent != null) {
-                    if (parent instanceof SecRunTaskLater runTaskLater) {
-                        runTaskLater.stopCurrentTask();
-                        return;
-                    }
-                    parent = parent.getParent();
-                }
-            }
+            case 1 -> this.currentTask.stopCurrentTask();
             case 2 -> {
                 for (Number id : this.ids.getArray(event)) {
                     SCHEDULER.cancelTask(id.intValue());
@@ -84,9 +78,8 @@ public class EffTaskStop extends Effect {
         }
     }
 
-    @SuppressWarnings("DataFlowIssue")
     @Override
-    public @NotNull String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         return switch (this.pattern) {
             case 1 -> "stop current task";
             case 2 -> "stop task[s] with id[s] " + this.ids.toString(e, d);
