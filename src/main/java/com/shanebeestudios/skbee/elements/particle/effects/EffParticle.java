b2032dd09f2fc9aa84263896dab70ddc3952b2d8
shanebeee <shanebolenback@me.com>
2024-07-02 11:01:17 -0700

    EffParticle - doc cleanup and add "delta" as an option for offset

--- src/main/java/com/shanebeestudios/skbee/elements/particle/effects/EffParticle.java
+++ src/main/java/com/shanebeestudios/skbee/elements/particle/effects/EffParticle.java
@@ -21,40 +21,39 @@ import org.jetbrains.annotations.Nullable;
 
 @Name("Particle Spawn")
 @Description({"Spawn a particle. This system is more inline with how Bukkit deals with particles, hence the choices in the pattern.",
-        "Some particles may be affected differently by these values, so let's break them down:",
-        "\nfirst number = count, how many particles to spawn at once. (use '0' if you notice the particle kinda flies away.)",
-        "\nparticle = the particle to spawn.",
-        "\nusing = the data used for this particle (some particles like 'block', 'item' and 'dust' require more data).",
-        "\nlocation = where you are going to spawn the particle.",
-        "\noffset = a vector with the maximum random offset. The position of each particle will be randomized positively and negatively by the offset parameters on each axis.",
-        "I believe some particles use the offset to set color. I'm not positive on this.",
-        "\nextra = the extra data for this particle, depends on the particle used (normally speed).",
-        "\nforce = whether to send the particle to players within an extended range and encourage ",
-        "their client to render it regardless of settings (this only works when not using `for player[s]`) (default = false)",
-        "\nfor %players% = will only send this particle to a player, not the whole server.",
-        "\nRequires Minecraft 1.13+"})
+    "Some particles may be affected differently by these values, so let's break them down:",
+    "`first number` = count, how many particles to spawn at once. (use '0' if you notice the particle kinda flies away.)",
+    "`particle` = the particle to spawn.",
+    "`using` = the data used for this particle (some particles like 'block', 'item' and 'dust' require more data).",
+    "`location` = where you are going to spawn the particle.",
+    "`delta` = a vector with the maximum random offset. The position of each particle will be randomized positively and negatively by the offset parameters on each axis.",
+    "Some particles use the delta to set color/direction if count is set to 0.",
+    "`extra` = the extra data for this particle, depends on the particle used (normally speed).",
+    "`forc`e = whether to send the particle to players within an extended range and encourage ",
+    "their client to render it regardless of settings (this only works when not using `for player[s]`) (default = false)",
+    "`for %players%` = will only send this particle to a player, not the whole server."})
 @Examples({"make 3 of item particle using diamond at location of player",
-        "make 1 of block particle using dirt at location of target block",
-        "make 10 of poof at player with offset vector(2, 2, 2) with extra 0.5",
-        "draw 20 of dust using dustOption(blue, 10) at location above target block",
-        "draw 1 of dust_color_transition using dustTransition(blue, green, 3) at location of player",
-        "draw 1 of vibration using vibration({loc1}, {loc2}, 1 second) at {loc1} with force",
-        "make 0 of shriek using 1 above target block of player",
-        "make 1 of sculk_charge using 0.1 at {_loc} with force"})
+    "make 1 of block particle using dirt at location of target block",
+    "make 10 of poof at player with offset vector(2, 2, 2) with extra 0.5",
+    "draw 20 of dust using dustOption(blue, 10) at location above target block",
+    "draw 1 of dust_color_transition using dustTransition(blue, green, 3) at location of player",
+    "draw 1 of vibration using vibration({loc1}, {loc2}, 1 second) at {loc1} with force",
+    "make 0 of shriek using 1 above target block of player",
+    "make 1 of sculk_charge using 0.1 at {_loc} with force"})
 @Since("1.9.0")
 public class EffParticle extends Effect {
 
     static {
         Skript.registerEffect(EffParticle.class,
-                "(lerp|draw|make) %number% [of] %particle% [particle] [using %-itemtype/blockdata/dustoption/dusttransition/vibration" +
-                        "/number/color/bukkitcolor%] %directions% %locations% [with offset %-vector%] [with extra %-number%] [(1:with force)] [(for|to) %-players%]");
+            "(lerp|draw|make) %number% [of] %particle% [particle] [using %-itemtype/blockdata/dustoption/dusttransition/vibration" +
+                "/number/color/bukkitcolor%] %directions% %locations% [with (delta|offset) %-vector%] [with extra %-number%] [(1:with force)] [(for|to) %-players%]");
     }
 
     private Expression<Number> count;
     private Expression<Particle> particle;
     private Expression<Location> location;
     @Nullable
-    private Expression<Vector> offset;
+    private Expression<Vector> delta;
     private Expression<Number> extra;
     @Nullable
     private Expression<Object> data;
@@ -69,7 +68,7 @@ public class EffParticle extends Effect {
         particle = (Expression<Particle>) exprs[1];
         data = (Expression<Object>) exprs[2];
         location = Direction.combine((Expression<? extends Direction>) exprs[3], (Expression<? extends Location>) exprs[4]);
-        offset = (Expression<Vector>) exprs[5];
+        delta = (Expression<Vector>) exprs[5];
         extra = (Expression<Number>) exprs[6];
         players = (Expression<Player>) exprs[7];
         force = parseResult.mark == 1;
@@ -86,10 +85,10 @@ public class EffParticle extends Effect {
         Particle particle = this.particle.getSingle(event);
         if (particle == null) return;
 
-        Vector offset = new Vector(0, 0, 0);
-        if (this.offset != null) {
-            Vector offsetSingle = this.offset.getSingle(event);
-            if (offsetSingle != null) offset = offsetSingle;
+        Vector delta = new Vector(0, 0, 0);
+        if (this.delta != null) {
+            Vector offsetSingle = this.delta.getSingle(event);
+            if (offsetSingle != null) delta = offsetSingle;
         }
         double extra = 1;
         if (this.extra != null) {
@@ -101,19 +100,19 @@ public class EffParticle extends Effect {
         Player[] players = this.players != null ? this.players.getArray(event) : null;
 
         for (Location location : this.location.getArray(event)) {
-            ParticleUtil.spawnParticle(particle, players, location, count, data, offset, extra, force);
+            ParticleUtil.spawnParticle(particle, players, location, count, data, delta, extra, force);
         }
     }
 
     @Override
-    public @NotNull String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         String data = this.data != null ? "using " + this.data.toString(e, d) : "";
-        String offset = this.offset != null ? "with offset " + this.offset.toString(e, d) : "";
+        String offset = this.delta != null ? "with delta " + this.delta.toString(e, d) : "";
         String extra = this.extra != null ? "with extra " + this.extra.toString(e, d) : "";
         String players = this.players != null ? "to " + this.players.toString(e, d) : "";
 
         return String.format("draw %s of %s %s %s %s %s %s", this.count.toString(e, d), this.particle.toString(e, d),
-                data, this.location.toString(e, d), offset, extra, players);
+            data, this.location.toString(e, d), offset, extra, players);
     }
 
 }
