cb06b9b8ea448c5d91745f348acb5bf169f3be4b
shanebeee <shanebolenback@me.com>
2024-07-05 16:23:15 -0700

    ExprBlockInChunk - code cleanup

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprBlockInChunk.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprBlockInChunk.java
@@ -21,31 +21,31 @@ import org.jetbrains.annotations.Nullable;
 @Name("Block in Chunk")
 @Description("Represents a block in a chunk. X/Z coords will be a value from 0 to 15.")
 @Examples({"set block at 1,1,1 in chunk at player to stone",
-        "set {_b} to block at 8,64,8 in chunk at player"})
+    "set {_b} to block at 8,64,8 in chunk at player"})
 @Since("1.13.0")
 public class ExprBlockInChunk extends SimpleExpression<Block> {
 
     static {
         Skript.registerExpression(ExprBlockInChunk.class, Block.class, ExpressionType.COMBINED,
-                "block at %number%,[ ]%number%,[ ]%number% (in|of) %chunk%");
+            "block at %number%,[ ]%number%,[ ]%number% (in|of) %chunk%");
     }
 
     private Expression<Chunk> chunk;
-    private Expression<Number> x,y,z;
+    private Expression<Number> x, y, z;
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "NullableProblems"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        x = (Expression<Number>) exprs[0];
-        y = (Expression<Number>) exprs[1];
-        z = (Expression<Number>) exprs[2];
-        chunk = (Expression<Chunk>) exprs[3];
+        this.x = (Expression<Number>) exprs[0];
+        this.y = (Expression<Number>) exprs[1];
+        this.z = (Expression<Number>) exprs[2];
+        this.chunk = (Expression<Chunk>) exprs[3];
         return true;
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
-    protected Block[] get(Event e) {
+    protected Block @Nullable [] get(Event e) {
         Chunk chunk = this.chunk.getSingle(e);
         if (chunk == null) return null;
 
@@ -68,7 +68,7 @@ public class ExprBlockInChunk extends SimpleExpression<Block> {
         if (y < WorldUtils.getMinHeight(world) || y > WorldUtils.getMaxHeight(world)) {
             return null;
         }
-        return new Block[]{chunk.getBlock(x,y,z)};
+        return new Block[]{chunk.getBlock(x, y, z)};
     }
 
     @Override
@@ -82,12 +82,12 @@ public class ExprBlockInChunk extends SimpleExpression<Block> {
     }
 
     @Override
-    public @NotNull String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         return String.format("Block at %s,%s,%s in chunk %s",
-                this.x.toString(e,d),
-                this.y.toString(e,d),
-                this.z.toString(e,d),
-                this.chunk.toString(e,d));
+            this.x.toString(e, d),
+            this.y.toString(e, d),
+            this.z.toString(e, d),
+            this.chunk.toString(e, d));
     }
 
 }
