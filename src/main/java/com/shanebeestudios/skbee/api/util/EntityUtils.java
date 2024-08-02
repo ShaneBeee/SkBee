0693517d6f5cda6d1797c53254cdb20772423cdc
shanebeee <shanebolenback@me.com>
2024-08-02 14:35:02 -0700

    SecAttributeModifier - add ability to use on entities

--- src/main/java/com/shanebeestudios/skbee/api/util/EntityUtils.java
+++ src/main/java/com/shanebeestudios/skbee/api/util/EntityUtils.java
@@ -1,8 +1,12 @@
 package com.shanebeestudios.skbee.api.util;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.entity.EntityData;
 import com.shanebeestudios.skbee.api.reflection.ReflectionConstants;
 import com.shanebeestudios.skbee.api.reflection.ReflectionUtils;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +18,7 @@ import java.util.function.Predicate;
  */
 public class EntityUtils {
 
+    public static final boolean HAS_TRANSIENT = Skript.methodExists(AttributeInstance.class, "addTransientModifier", AttributeModifier.class);
     private static final Class<?> ENTITY_NMS_CLASS = ReflectionUtils.getNMSClass("net.minecraft.world.entity.Entity");
 
     public static void setNoPhysics(Entity entity, boolean clip) {
@@ -45,6 +50,28 @@ public class EntityUtils {
             }
             return true;
         };
+    }
+
+    /**
+     * Check if an entity already has an instance of an attribute modifier
+     *
+     * @param entity    Entity to check for modifier
+     * @param attribute Attribute to check for modifier
+     * @param modifier  Modifier to compare
+     * @return True if modifier with same id/name already exists else false
+     */
+    public static boolean hasAttributeModifier(LivingEntity entity, Attribute attribute, AttributeModifier modifier) {
+        AttributeInstance attributeInstance = entity.getAttribute(attribute);
+        if (attributeInstance == null) return false;
+
+        for (AttributeModifier attributeInstanceModifier : attributeInstance.getModifiers()) {
+            if (ItemUtils.HAS_KEY && attributeInstanceModifier.getKey().equals(modifier.getKey())) {
+                return true;
+            } else if (attributeInstanceModifier.getName().equalsIgnoreCase(modifier.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/main/java/com/shanebeestudios/skbee/elements/other/sections/SecAttributeModifier.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/sections/SecAttributeModifier.java
@@ -1,4 +1,4 @@
-package com.shanebeestudios.skbee.elements.itemcomponent.sections;
+package com.shanebeestudios.skbee.elements.other.sections;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
@@ -12,12 +12,15 @@ import ch.njol.skript.lang.Section;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.TriggerItem;
 import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.api.util.EntityUtils;
 import com.shanebeestudios.skbee.api.util.ItemUtils;
 import com.shanebeestudios.skbee.api.util.Util;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.EquipmentSlotGroup;
@@ -32,12 +35,14 @@ import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("DataFlowIssue")
-@Name("ItemComponent - Attribute Modifier Apply")
-@Description({"Apply an attribute modifier to an item.",
+@Name("Attribute Modifier Apply")
+@Description({"Apply an attribute modifier to an item or living entity.",
     "If running Minecraft 1.21+ use `id`",
     "If running Minecraft 1.20.6 and below, use `name` and `uuid` (uuid is optional, will default to random).",
     "See [**McWiki Component**](https://minecraft.wiki/w/Data_component_format#attribute_modifiers) and " +
         "[**McWiki Modifiers**](https://minecraft.wiki/w/Attribute#Modifiers) for further details.",
+    "",
+    "`transient` = Non-persisent attribute modifier, will not save to the entity's NBT (Requires PaperMC).",
     "",
     "**Entries/Sections**:",
     "- `attribute` = The attribute this modifier is to act upon.",
@@ -69,9 +74,9 @@ public class SecAttributeModifier extends Section {
     static {
         VALIDATIOR.addEntryData(new ExpressionEntryData<>("attribute", null, false, Attribute.class));
         if (ItemUtils.HAS_EQUIPMENT_SLOT_GROUP) {
-            VALIDATIOR.addEntryData(new ExpressionEntryData<>("slot", null, false, EquipmentSlotGroup.class));
+            VALIDATIOR.addEntryData(new ExpressionEntryData<>("slot", null, true, EquipmentSlotGroup.class));
         } else {
-            VALIDATIOR.addEntryData(new ExpressionEntryData<>("slot", null, false, EquipmentSlot.class));
+            VALIDATIOR.addEntryData(new ExpressionEntryData<>("slot", null, true, EquipmentSlot.class));
         }
         if (ItemUtils.HAS_KEY) {
             VALIDATIOR.addEntryData(new ExpressionEntryData<>("id", null, false, String.class));
@@ -81,10 +86,11 @@ public class SecAttributeModifier extends Section {
         }
         VALIDATIOR.addEntryData(new ExpressionEntryData<>("amount", null, false, Number.class));
         VALIDATIOR.addEntryData(new ExpressionEntryData<>("operation", null, false, Operation.class));
-        Skript.registerSection(SecAttributeModifier.class, "apply attribute modifier to %itemtypes%");
+        Skript.registerSection(SecAttributeModifier.class, "apply [:transient] attribute modifier to %itemtypes/livingentities%");
     }
 
-    private Expression<ItemType> items;
+    private boolean trans;
+    private Expression<?> objects;
     private Expression<Attribute> attribute;
     private Expression<?> slot;
     private Expression<String> id;
@@ -99,7 +105,12 @@ public class SecAttributeModifier extends Section {
         EntryContainer container = VALIDATIOR.build().validate(sectionNode);
         if (container == null) return false;
 
-        this.items = (Expression<ItemType>) exprs[0];
+        this.trans = parseResult.hasTag("transient");
+        if (this.trans && !EntityUtils.HAS_TRANSIENT) {
+            Skript.error("'transient' requires a PaperMC server.");
+            return false;
+        }
+        this.objects = exprs[0];
         this.attribute = (Expression<Attribute>) container.getOptional("attribute", false);
         this.slot = (Expression<?>) container.getOptional("slot", false);
         if (ItemUtils.HAS_KEY) {
@@ -112,7 +123,7 @@ public class SecAttributeModifier extends Section {
         }
         this.amount = (Expression<Number>) container.getOptional("amount", false);
         this.operation = (Expression<Operation>) container.getOptional("operation", false);
-        return this.attribute != null && this.slot != null && this.amount != null;
+        return this.attribute != null && this.amount != null;
     }
 
     @SuppressWarnings({"NullableProblems", "removal"})
@@ -121,9 +132,9 @@ public class SecAttributeModifier extends Section {
         Attribute attribute = this.attribute.getSingle(event);
         Number amountNum = this.amount.getSingle(event);
         Operation operation = this.operation.getSingle(event);
-        Object slot = this.slot.getSingle(event);
+        Object slot = this.slot != null ? this.slot.getSingle(event) : null;
 
-        if (attribute == null || slot == null || amountNum == null || operation == null)
+        if (attribute == null || amountNum == null || operation == null)
             return super.walk(event, false);
 
         AttributeModifier attributeModifier;
@@ -134,11 +145,8 @@ public class SecAttributeModifier extends Section {
             NamespacedKey namespacedKey = Util.getNamespacedKey(id, false);
             if (namespacedKey == null) return super.walk(event, false);
 
-            if (slot instanceof EquipmentSlotGroup slotGroup) {
-                attributeModifier = new AttributeModifier(namespacedKey, amountNum.doubleValue(), operation, slotGroup);
-            } else {
-                return super.walk(event, false);
-            }
+            EquipmentSlotGroup slotGroup = slot instanceof EquipmentSlotGroup esg ? esg : EquipmentSlotGroup.ANY;
+            attributeModifier = new AttributeModifier(namespacedKey, amountNum.doubleValue(), operation, slotGroup);
         } else {
             String name = this.name.getSingle(event);
             String uuidString = this.uuid != null ? this.uuid.getSingle(event) : null;
@@ -150,30 +158,43 @@ public class SecAttributeModifier extends Section {
             }
             if (name == null) return super.walk(event, false);
 
-            if (slot instanceof EquipmentSlotGroup slotGroup) {
-                attributeModifier = new AttributeModifier(uuid, name, amountNum.doubleValue(), operation, slotGroup);
-            } else if (slot instanceof EquipmentSlot equipmentSlot) {
+            if (slot instanceof EquipmentSlot equipmentSlot) {
                 attributeModifier = new AttributeModifier(uuid, name, amountNum.doubleValue(), operation, equipmentSlot);
+            } else if (ItemUtils.HAS_EQUIPMENT_SLOT_GROUP && slot instanceof EquipmentSlotGroup slotGroup) {
+                attributeModifier = new AttributeModifier(uuid, name, amountNum.doubleValue(), operation, slotGroup);
             } else {
-                return super.walk(event, false);
+                attributeModifier = new AttributeModifier(uuid, name, amountNum.doubleValue(), operation);
             }
+
         }
 
-        for (ItemType itemType : this.items.getArray(event)) {
-            ItemMeta itemMeta = itemType.getItemMeta();
+        for (Object object : this.objects.getArray(event)) {
+            if (object instanceof ItemType itemType) {
+                ItemMeta itemMeta = itemType.getItemMeta();
 
-            if (!ItemUtils.hasAttributeModifier(itemMeta, attribute, attributeModifier)) {
-                itemMeta.addAttributeModifier(attribute, attributeModifier);
+                if (!ItemUtils.hasAttributeModifier(itemMeta, attribute, attributeModifier)) {
+                    itemMeta.addAttributeModifier(attribute, attributeModifier);
+                }
+
+                itemType.setItemMeta(itemMeta);
+            } else if (object instanceof LivingEntity entity) {
+                AttributeInstance attributeInstance = entity.getAttribute(attribute);
+                if (!EntityUtils.hasAttributeModifier(entity, attribute, attributeModifier)) {
+                    if (this.trans) {
+                        attributeInstance.addTransientModifier(attributeModifier);
+                    } else {
+                        attributeInstance.addModifier(attributeModifier);
+                    }
+                }
             }
-
-            itemType.setItemMeta(itemMeta);
         }
         return super.walk(event, false);
     }
 
     @Override
     public @NotNull String toString(Event e, boolean d) {
-        return "apply attribute modifier to " + this.items.toString(e, d);
+        String t = this.trans ? " transient " : " ";
+        return "apply" + t + "attribute modifier to " + this.objects.toString(e, d);
     }
 
 }
