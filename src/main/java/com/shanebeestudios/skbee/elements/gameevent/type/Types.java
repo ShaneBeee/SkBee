1767a3bbf01095ebfc9d6404a0d218cde6e283ec
shanebeee <shanebolenback@me.com>
2024-08-09 08:31:25 -0700

    gameevent/types - update class to use RegistryClassInfo - Also update wiki link, appears it was changed

--- src/main/java/com/shanebeestudios/skbee/elements/gameevent/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/gameevent/type/Types.java
@@ -1,69 +1,23 @@
 package com.shanebeestudios.skbee.elements.gameevent.type;
 
-import ch.njol.skript.classes.ClassInfo;
-import ch.njol.skript.classes.Parser;
-import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
-import ch.njol.util.StringUtils;
+import com.shanebeestudios.skbee.api.wrapper.RegistryClassInfo;
 import org.bukkit.GameEvent;
-import org.bukkit.NamespacedKey;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.bukkit.Registry;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@SuppressWarnings("deprecation") // GameEvent.values() <-- paper did this I'm assuming
 public class Types {
 
-    private static final Map<String, GameEvent> GAME_EVENT_MAP = new HashMap<>();
-
     static {
-        List<String> names = new ArrayList<>();
-        for (GameEvent gameEvent : GameEvent.values()) {
-            NamespacedKey key = gameEvent.getKey();
-            GAME_EVENT_MAP.put(key.toString(), gameEvent);
-            GAME_EVENT_MAP.put(key.getKey(), gameEvent);
-            names.add(key.getKey());
-        }
-        Collections.sort(names);
-        String namesString = StringUtils.join(names, ", ");
 
-        Classes.registerClass(new ClassInfo<>(GameEvent.class, "gameevent")
+        Classes.registerClass(RegistryClassInfo.create(Registry.GAME_EVENT, GameEvent.class, "gameevent")
             .name("Game Event")
             .user("game ?events?")
             .description("Represents a Minecraft 'GameEvent', mainly used by Skulk Sensors. Requires MC 1.17+.",
-                "See [**GameEvents**](https://minecraft.wiki/w/Sculk_Sensor#Vibration_amplitudes) on McWiki for more details.",
+                "See [**GameEvents**](https://minecraft.wiki/w/Sculk_Sensor#Vibration_frequencies) on McWiki for more details.",
                 "NOTE: These are auto-generated and may differ between server versions.")
-            .usage(namesString)
             .after("itemtype")
             .examples("")
-            .since("1.14.0")
-            .parser(new Parser<>() {
-
-                @SuppressWarnings("NullableProblems")
-                @Nullable
-                @Override
-                public GameEvent parse(String string, ParseContext context) {
-                    if (string.contains(" "))
-                        string = string.replace(" ", "_");
-                    if (GAME_EVENT_MAP.containsKey(string)) return GAME_EVENT_MAP.get(string);
-                    return null;
-                }
-
-                @Override
-                public @NotNull String toString(GameEvent gameEvent, int flags) {
-                    return gameEvent.getKey().getKey();
-                }
-
-                @Override
-                public @NotNull String toVariableNameString(GameEvent gameEvent) {
-                    return "gameevent:" + gameEvent.getKey().getKey();
-                }
-            }));
+            .since("1.14.0"));
     }
 
 }
