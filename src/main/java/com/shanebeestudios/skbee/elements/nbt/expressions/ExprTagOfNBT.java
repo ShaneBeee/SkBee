814380b7160a51ce9eacb2c2a71f860cfdd9b21c
shanebeee <shanebolenback@me.com>
2024-08-08 17:54:45 -0700

    ExprTagOfNBT - update docs

--- src/main/java/com/shanebeestudios/skbee/elements/nbt/expressions/ExprTagOfNBT.java
+++ src/main/java/com/shanebeestudios/skbee/elements/nbt/expressions/ExprTagOfNBT.java
@@ -25,18 +25,21 @@ import java.util.ArrayList;
 @Name("NBT - Tag")
 @Description({"Get/set/delete the value of the specified tag of an NBT compound. Also supports getting nested tags using a semi colon as a delimiter.",
     "If the return value is a list, you can use it as a list, as it will automatically split it for ya.",
-    "\nNOTE: `uuid tag` will set an int array tag (This is how MC stores uuids). On return it'll convert back to a uuid.",
-    "\nNOTE: Entities/blocks can not natively hold custom NBT tags. SkBee allows you to put custom nbt",
+    "**NOTES**:",
+    "- `uuid tag` will set an int array tag (This is how MC stores uuids). On return it'll convert back to a uuid.",
+    "- Entities/blocks can not natively hold custom NBT tags. SkBee allows you to put custom nbt",
     "data in the \"custom\" compound tag of a block/entity's NBT compound. Due to Minecraft not supporting this, I had to use some hacky methods to make this happen.",
     "That said, this system is a tad convoluted, see the SkBee WIKI for more details.",
-    "\nNOTE 1.20.5+: All custom data on items must be stored in the \"minecraft:custom_data\" compound " +
+    "- 1.20.5+: All custom data on items must be stored in the \"minecraft:custom_data\" compound " +
         "(see examples and [**McWiki**](https://minecraft.wiki/w/Data_component_format#custom_data)).",
-    "For more info regarding 1.20.5+ components please see [**Data Component Format**](https://minecraft.wiki/w/Data_component_format) on McWiki.",
-    "\nADD: You can add numbers to number type tags, you can also add numbers/strings/compounds to lists type tags.",
-    "\nREMOVE: You can remove numbers from number type tags, you can also remove numbers/strings from lists type tags.",
-    "(You can NOT remove compounds from lists type tags)",
-    "\nGET FROM LIST BY POSITION: You can get the element of a list by adding `[%number%]` to the end of a tag",
-    "(This only works for getters, not set/add/remove/delete) see examples."})
+    "- For more info regarding 1.20.5+ components please see [**Data Component Format**](https://minecraft.wiki/w/Data_component_format) on McWiki.",
+    "- GET FROM LIST BY POSITION: You can get the element of a list by adding `[%number%]` to the end of a tag",
+    "(This only works for getters, not set/add/remove/delete) see examples.",
+    "",
+    "**CHANGERS**:",
+    "- ADD: You can add numbers to number type tags, you can also add numbers/strings/compounds to lists type tags.",
+    "- REMOVE: You can remove numbers from number type tags, you can also remove numbers/strings from lists type tags." +
+        "(You can NOT remove compounds from lists type tags)"})
 @Examples({"# Getting the value of a simple tag",
     "set {_damage} to int tag \"minecraft:damage\" of nbt of player's tool",
     "set {_sheared} to byte tag \"Sheared\" of nbt of target entity",
@@ -83,18 +86,19 @@ import java.util.ArrayList;
     "set {_b} to byte tag \"someBytes[10]\" of {_nbt}",
     "",
     "# Minecraft 1.20.5+ item component stuff",
-    "set int tag \"minecraft:max_damage\" of component nbt of player's tool to 500",
-    "set int tag \"minecraft:max_stack_size\" of component nbt of player's tool to 25",
-    "set byte tag \"minecraft:enchantment_glint_override\" of component nbt of player's tool to 1",
-    "set compound tag \"minecraft:fire_resistant\" of component nbt of player's tool to nbt from \"{}\"",
-    "set string tag \"minecraft:rarity\" of component nbt of player's tool to \"epic\"",
+    "set int tag \"minecraft:max_damage\" of nbt of player's tool to 500",
+    "set int tag \"minecraft:max_stack_size\" of nbt of player's tool to 25",
+    "set byte tag \"minecraft:enchantment_glint_override\" of nbt of player's tool to 1",
+    "set compound tag \"minecraft:fire_resistant\" of nbt of player's tool to nbt from \"{}\"",
+    "set string tag \"minecraft:rarity\" of nbt of player's tool to \"epic\"",
     "",
     "# All custom data must be within the \"minecraft:custom_data\" compound",
     "# See NBT Compound expression for futher details on the `component nbt` expression",
-    "set byte tag \"Points\" of nbt of player's tool to 10",
-    "set int tag \"MyBloop\" of nbt of player's tool to 152",
-    "set byte tag \"minecraft:custom_data;Points\" of component nbt of player's tool to 10",
-    "set int tag \"minecraft:custom_data;MyBloop\" of component nbt of player's tool to 152"})
+    "set byte tag \"Points\" of custom nbt of player's tool to 10",
+    "set int tag \"MyBloop\" of custom nbt of player's tool to 152",
+    "# These examples will do the same as above",
+    "set byte tag \"minecraft:custom_data;Points\" of nbt of player's tool to 10",
+    "set int tag \"minecraft:custom_data;MyBloop\" of nbt of player's tool to 152"})
 @Since("1.0.0")
 public class ExprTagOfNBT extends SimpleExpression<Object> {
 
