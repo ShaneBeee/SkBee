1de9274dd3507db207b1c38e2dd478b51c063681
shanebeee <shanebolenback@me.com>
2024-09-08 09:49:31 -0700

    ItemFlags - add support for set, code cleanup, doc update

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprItemFlags.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprItemFlags.java
@@ -1,7 +1,7 @@
 package com.shanebeestudios.skbee.elements.other.expressions;
 
 import ch.njol.skript.aliases.ItemType;
-import ch.njol.skript.classes.Changer;
+import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -11,26 +11,28 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import com.shanebeestudios.skbee.api.util.ItemUtils;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Name("ItemFlag - ItemFlags of Items")
-@Description("Get/Set the ItemFlags of an item.")
+@Description({"Get/Set the ItemFlags of an item. Delete/reset will remove all item flags.",
+    "As of Minecraft 1.20.5, the `hide attributes` flag doesn't work as intended. ",
+    "See [**SkBee Wiki**](https://github.com/ShaneBeee/SkBee/wiki/Tricks-Hide-Attribute-Modifiers) for more info."})
 @Examples({"set {_flags::*} to item flags of player's tool",
-        "add hide enchants to item flags of player's tool",
-        "add hide attributes to item flags of player's tool",
-        "add hide enchants and hide attributes to item flags of player's tool",
-        "remove hide enchants from item flags of player's tool",
-        "remove hide attributes from item flags of player's tool",
-        "delete item flags of player's tool",
-        "reset item flags of player's tool"})
+    "set item flags of player's tool to hide enchants",
+    "add hide enchants to item flags of player's tool",
+    "add hide attributes to item flags of player's tool",
+    "add hide enchants and hide attributes to item flags of player's tool",
+    "remove hide enchants from item flags of player's tool",
+    "remove hide attributes from item flags of player's tool",
+    "delete item flags of player's tool",
+    "reset item flags of player's tool"})
 @Since("3.4.0")
 public class ExprItemFlags extends PropertyExpression<ItemType, ItemFlag> {
 
@@ -57,36 +59,41 @@ public class ExprItemFlags extends PropertyExpression<ItemType, ItemFlag> {
 
     @SuppressWarnings("NullableProblems")
     @Override
-    public Class<?> @Nullable [] acceptChange(Changer.ChangeMode mode) {
-        if (mode == Changer.ChangeMode.ADD || mode == Changer.ChangeMode.REMOVE) {
-            return CollectionUtils.array(ItemFlag[].class);
-        } else if (mode == Changer.ChangeMode.DELETE || mode == Changer.ChangeMode.RESET) {
-            return CollectionUtils.array();
-        }
-        return null;
+    public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
+        return switch (mode) {
+            case SET, ADD, REMOVE -> CollectionUtils.array(ItemFlag.class);
+            case DELETE, RESET -> CollectionUtils.array();
+            default -> null;
+        };
     }
 
     @SuppressWarnings({"NullableProblems", "ConstantValue"})
     @Override
-    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
-        if (mode == Changer.ChangeMode.DELETE || mode == Changer.ChangeMode.RESET) {
-            for (ItemType itemType : getExpr().getArray(event)) {
-                ItemMeta itemMeta = itemType.getItemMeta();
-                Set<ItemFlag> itemFlags = itemMeta.getItemFlags();
-                itemMeta.removeItemFlags(itemFlags.toArray(new ItemFlag[0]));
-                itemType.setItemMeta(itemMeta);
-            }
-        } else if (mode == Changer.ChangeMode.ADD || mode == Changer.ChangeMode.REMOVE) {
+    public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+        if (mode == ChangeMode.SET) {
             if (delta != null && delta instanceof ItemFlag[] itemFlags) {
                 for (ItemType itemType : getExpr().getArray(event)) {
-                    ItemMeta itemMeta = itemType.getItemMeta();
-                    if (mode == Changer.ChangeMode.ADD) {
+                    ItemUtils.modifyItemMeta(itemType, itemMeta -> {
+                        itemMeta.removeItemFlags(ItemFlag.values());
                         itemMeta.addItemFlags(itemFlags);
-                    } else {
-                        itemMeta.removeItemFlags(itemFlags);
-                    }
-                    itemType.setItemMeta(itemMeta);
+                    });
                 }
+            }
+        } else if (mode == ChangeMode.ADD || mode == ChangeMode.REMOVE) {
+            if (delta != null && delta instanceof ItemFlag[] itemFlags) {
+                for (ItemType itemType : getExpr().getArray(event)) {
+                    ItemUtils.modifyItemMeta(itemType, itemMeta -> {
+                        if (mode == ChangeMode.ADD) {
+                            itemMeta.addItemFlags(itemFlags);
+                        } else {
+                            itemMeta.removeItemFlags(itemFlags);
+                        }
+                    });
+                }
+            }
+        } else if (mode == ChangeMode.DELETE || mode == ChangeMode.RESET) {
+            for (ItemType itemType : getExpr().getArray(event)) {
+                ItemUtils.modifyItemMeta(itemType, itemMeta -> itemMeta.removeItemFlags(ItemFlag.values()));
             }
         }
     }
--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprItemFlagsItem.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprItemFlagsItem.java
@@ -11,28 +11,30 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.api.util.ItemUtils;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("ItemFlag - Item with ItemFlags")
-@Description("Get an item with ItemFlags.")
+@Description({"Get an item with ItemFlags.",
+    "As of Minecraft 1.20.5, the `hide attributes` flag doesn't work as intended. ",
+    "See [**SkBee Wiki**](https://github.com/ShaneBeee/SkBee/wiki/Tricks-Hide-Attribute-Modifiers) for more info."})
 @Examples({"set {_sword} to diamond sword with all item flags",
-        "set {_sword} to diamond sword of sharpness 3 with hide enchants item flag",
-        "set {_sword} to diamond sword of sharpness 3 with item flag hide enchants",
-        "give player fishing rod of lure 10 with item flag hide enchants",
-        "give player potion of extended regeneration with hide enchants itemflag",
-        "give player netherite leggings with itemflag hide attributes"})
+    "set {_sword} to diamond sword of sharpness 3 with hide enchants item flag",
+    "set {_sword} to diamond sword of sharpness 3 with item flag hide enchants",
+    "give player fishing rod of lure 10 with item flag hide enchants",
+    "give player potion of extended regeneration with hide enchants itemflag",
+    "give player netherite leggings with itemflag hide attributes"})
 @Since("3.4.0")
 public class ExprItemFlagsItem extends SimpleExpression<ItemType> {
 
     static {
         Skript.registerExpression(ExprItemFlagsItem.class, ItemType.class, ExpressionType.COMBINED,
-                "%itemtype% with all item[ ]flags",
-                "%itemtype% with item[ ]flag[s] %itemflags%",
-                "%itemtype% with %itemflags% item[ ]flag[s]");
+            "%itemtype% with all item[ ]flags",
+            "%itemtype% with item[ ]flag[s] %itemflags%",
+            "%itemtype% with %itemflags% item[ ]flag[s]");
     }
 
     private int pattern;
@@ -54,13 +56,12 @@ public class ExprItemFlagsItem extends SimpleExpression<ItemType> {
     @Override
     protected ItemType @Nullable [] get(Event event) {
         ItemType itemType = this.itemType.getSingle(event);
+        ItemFlag[] flags = this.pattern == 0 ? ItemFlag.values() : this.itemFlags.getArray(event);
         if (itemType == null) return null;
 
         itemType = itemType.clone();
-        ItemMeta itemMeta = itemType.getItemMeta();
-        ItemFlag[] flags = this.pattern == 0 ? ItemFlag.values() : this.itemFlags.getArray(event);
-        itemMeta.addItemFlags(flags);
-        itemType.setItemMeta(itemMeta);
+        ItemUtils.modifyItemMeta(itemType, itemMeta -> itemMeta.addItemFlags(flags));
+
         return new ItemType[]{itemType};
     }
 
