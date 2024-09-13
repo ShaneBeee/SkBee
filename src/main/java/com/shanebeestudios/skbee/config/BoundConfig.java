134a9dfb61e914d6c975171b2ff818776fd5a6a6
shanebeee <shanebolenback@me.com>
2024-09-13 11:42:43 -0700

    BoundConfig - more cleanup

--- src/main/java/com/shanebeestudios/skbee/config/BoundConfig.java
+++ src/main/java/com/shanebeestudios/skbee/config/BoundConfig.java
@@ -1,15 +1,13 @@
 package com.shanebeestudios.skbee.config;
 
-import ch.njol.skript.Skript;
 import com.shanebeestudios.skbee.SkBee;
 import com.shanebeestudios.skbee.api.bound.Bound;
-import com.shanebeestudios.skbee.api.util.Util;
-import com.shanebeestudios.skbee.api.util.WorldUtils;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,8 +16,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BoundConfig {
-
-    private static final String UPDATED_18_HEIGHTS = "updated_18_heights";
 
     private final SkBee plugin;
     private File boundFile;
@@ -56,26 +52,6 @@ public class BoundConfig {
         }
     }
 
-    private void update18Heights() {
-        Util.log("Updating bounds:");
-        for (Bound bound : boundsMap.values()) {
-            int lesserY = bound.getLesserY();
-            int greaterY = bound.getGreaterY();
-            World world = bound.getWorld();
-            if (lesserY == 0 && world != null) {
-                int minHeight = WorldUtils.getMinHeight(world);
-                int maxHeight = WorldUtils.getMaxHeight(world);
-                if (greaterY == 255 || greaterY == maxHeight) {
-                    bound.setGreaterY(maxHeight);
-                    bound.setLesserY(minHeight);
-                    Util.log("Updating bound with id '%s'", bound.getId());
-                }
-            }
-        }
-        boundConfig.set(UPDATED_18_HEIGHTS, true);
-        saveAllBounds();
-    }
-
     public void saveBound(Bound bound) {
         boundsMap.put(bound.getId(), bound);
         if (!bound.isTemporary()) {
@@ -96,7 +72,7 @@ public class BoundConfig {
         return boundsMap.containsKey(id);
     }
 
-    public Bound getBoundFromID(String id) {
+    public @Nullable Bound getBoundFromID(String id) {
         if (boundsMap.containsKey(id))
             return boundsMap.get(id);
         return null;
@@ -111,6 +87,7 @@ public class BoundConfig {
         saveConfig();
     }
 
+    @SuppressWarnings("CallToPrintStackTrace")
     private void saveConfig() {
         try {
             boundConfig.save(boundFile);
