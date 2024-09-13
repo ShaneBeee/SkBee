da4bd81765fb5d0e8b937352a13f8d7c8948a013
shanebeee <shanebolenback@me.com>
2024-09-13 11:52:21 -0700

    EffSecBoundCopy - change copy method and cleanup code

--- src/main/java/com/shanebeestudios/skbee/elements/bound/sections/EffSecBoundCopy.java
+++ src/main/java/com/shanebeestudios/skbee/elements/bound/sections/EffSecBoundCopy.java
@@ -19,6 +19,7 @@ import com.shanebeestudios.skbee.api.event.bound.BoundCreateEvent;
 import com.shanebeestudios.skbee.config.BoundConfig;
 import com.shanebeestudios.skbee.elements.bound.expressions.ExprLastCreatedBound;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -27,18 +28,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Name("Bound - Copy Bound")
 @Description("Create an exact replica of an existing bound with a new id.")
 @Examples({"create a copy of bound with id \"some_bound\" with id \"some_bound_copy\"",
-        "create a copy of bound with id \"some_bound\" with id \"some_bound_copy\":",
-        "\tresize the bound between {_pos1} and {_pos2}",
-        "\tadd {_player} to bound owners of bound"})
+    "create a copy of bound with id \"some_bound\" with id \"some_bound_copy\":",
+    "\tresize the bound between {_pos1} and {_pos2}",
+    "\tadd {_player} to bound owners of bound"})
 @Since("2.15.0")
 public class EffSecBoundCopy extends EffectSection {
 
-    private static final BoundConfig boundConfig;
+    private static final BoundConfig BOUND_CONFIG;
 
     static {
-        boundConfig = SkBee.getPlugin().getBoundConfig();
+        BOUND_CONFIG = SkBee.getPlugin().getBoundConfig();
         Skript.registerSection(EffSecBoundCopy.class,
-                "create [a] copy of [bound] %bound% (with|using) [the] id %string%");
+            "create [a] copy of [bound] %bound% (with|using) [the] id %string%");
     }
 
     private Expression<Bound> bound;
@@ -48,11 +49,11 @@ public class EffSecBoundCopy extends EffectSection {
     @SuppressWarnings({"unchecked", "NullableProblems"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult, @Nullable SectionNode sectionNode, @Nullable List<TriggerItem> list) {
-        bound = (Expression<Bound>) exprs[0];
-        id = (Expression<String>) exprs[1];
+        this.bound = (Expression<Bound>) exprs[0];
+        this.id = (Expression<String>) exprs[1];
         if (sectionNode != null) {
             AtomicBoolean delayed = new AtomicBoolean(false);
-            trigger = loadCode(sectionNode, "bound copy", BoundCreateEvent.class);
+            this.trigger = loadCode(sectionNode, "bound copy", BoundCreateEvent.class);
             if (delayed.get()) {
                 Skript.error("Delays can't be within a Copy Bound section.");
                 return false;
@@ -61,29 +62,30 @@ public class EffSecBoundCopy extends EffectSection {
         return true;
     }
 
+    @SuppressWarnings("NullableProblems")
     @Override
     protected @Nullable TriggerItem walk(Event event) {
         Object localVars = Variables.copyLocalVariables(event);
 
-        Bound bound = this.bound.getSingle(event);
+        Bound boundToCopy = this.bound.getSingle(event);
         String id = this.id.getSingle(event);
-        if (id == null || bound == null) return super.walk(event, false);
-        bound = bound.copy(bound, id);
-        ExprLastCreatedBound.lastCreated = bound;
-        boundConfig.saveBound(bound);
-        if (trigger != null) {
-            BoundCreateEvent boundCreateEvent = new BoundCreateEvent(bound);
+        if (id == null || boundToCopy == null) return super.walk(event, false);
+        Bound newBound = boundToCopy.copy(id);
+        ExprLastCreatedBound.lastCreated = newBound;
+        if (this.trigger != null) {
+            BoundCreateEvent boundCreateEvent = new BoundCreateEvent(newBound);
             Variables.setLocalVariables(boundCreateEvent, localVars);
-            TriggerItem.walk(trigger, boundCreateEvent);
+            TriggerItem.walk(this.trigger, boundCreateEvent);
             Variables.setLocalVariables(event, Variables.copyLocalVariables(boundCreateEvent));
             Variables.removeLocals(boundCreateEvent);
         }
+        BOUND_CONFIG.saveBound(newBound);
         return super.walk(event, false);
     }
 
     @Override
-    public String toString(@Nullable Event event, boolean debug) {
-        return "create a copy of bound " + bound.toString(event, debug) + " with id " + id.toString(event,debug);
+    public @NotNull String toString(Event event, boolean debug) {
+        return "create a copy of bound " + this.bound.toString(event, debug) + " with id " + this.id.toString(event, debug);
     }
 
 }
