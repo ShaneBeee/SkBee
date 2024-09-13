8551ba1007521a15842df21fc28ac3a7c8364bb0
shanebeee <shanebolenback@me.com>
2024-09-13 11:44:49 -0700

    ExprBoundEntities - cleanup

--- src/main/java/com/shanebeestudios/skbee/elements/bound/expressions/ExprBoundEntities.java
+++ src/main/java/com/shanebeestudios/skbee/elements/bound/expressions/ExprBoundEntities.java
@@ -8,7 +8,6 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.entity.EntityData;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
@@ -22,17 +21,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Name("Bound - Entities")
-@Description("Get all the entities within a bound. NOTE: If the chunk in a bound is unloaded, entities will also be unloaded.")
+@Description({"Get all the entities within a bound.",
+    "NOTE: If the chunk in a bound is unloaded, entities will also be unloaded."})
 @Examples({"set {_b} to bound with id \"my-bound\"",
-        "loop entities in bound {_b}:",
-        "\tif loop-entity is a cow or pig:",
-        "\t\tkill loop-entity"})
+    "loop entities in bound {_b}:",
+    "\tif loop-entity is a cow or pig:",
+    "\t\tkill loop-entity"})
 @Since("1.15.0")
 public class ExprBoundEntities extends SimpleExpression<Entity> {
 
     static {
         Skript.registerExpression(ExprBoundEntities.class, Entity.class, ExpressionType.SIMPLE,
-                "[(all [[of] the]|the)] %*entitydatas% (of|in|within) bound[s] %bounds%");
+            "[(all [[of] the]|the)] %*entitydatas% (of|in|within) bound[s] %bounds%");
     }
 
     private Expression<EntityData<?>> entityDatas;
@@ -41,8 +41,8 @@ public class ExprBoundEntities extends SimpleExpression<Entity> {
     @SuppressWarnings({"unchecked", "NullableProblems"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        entityDatas = (Expression<EntityData<?>>) exprs[0];
-        bounds = (Expression<Bound>) exprs[1];
+        this.entityDatas = (Expression<EntityData<?>>) exprs[0];
+        this.bounds = (Expression<Bound>) exprs[1];
         return true;
     }
 
@@ -51,8 +51,8 @@ public class ExprBoundEntities extends SimpleExpression<Entity> {
     @Override
     protected Entity[] get(Event event) {
         List<Entity> entities = new ArrayList<>();
-        for (Bound bound : bounds.getArray(event)) {
-            for (EntityData<?> entityData : entityDatas.getArray(event)) {
+        for (Bound bound : this.bounds.getArray(event)) {
+            for (EntityData<?> entityData : this.entityDatas.getArray(event)) {
                 Class<? extends Entity> type = entityData.getType();
                 entities.addAll(bound.getEntities(type));
             }
@@ -71,7 +71,7 @@ public class ExprBoundEntities extends SimpleExpression<Entity> {
     }
 
     @Override
-    public @NotNull String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         return this.entityDatas.toString(e, d) + " of bound[s] " + this.bounds.toString(e, d);
     }
 
