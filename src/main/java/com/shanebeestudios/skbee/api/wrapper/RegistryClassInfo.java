34e3cbbfc654fd15d3c7c4cdf92aec31e60632dd
ShaneBeee <shanebolenback@me.com>
2024-12-14 17:52:49 -0800

    RegistryClassInfo - sort supplier

--- src/main/java/com/shanebeestudios/skbee/api/wrapper/RegistryClassInfo.java
+++ src/main/java/com/shanebeestudios/skbee/api/wrapper/RegistryClassInfo.java
@@ -19,6 +19,7 @@ import org.skriptlang.skript.lang.comparator.Relation;
 import java.io.StreamCorruptedException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -102,7 +103,7 @@ public class RegistryClassInfo<T extends Keyed> extends ClassInfo<T> {
             Comparators.registerComparator(registryClass, registryClass, (o1, o2) -> Relation.get(o1.equals(o2)));
         }
         if (usage) this.usage(getNames());
-        this.supplier(registry::iterator);
+        this.supplier(() -> registry.stream().sorted(Comparator.comparing(Keyed::getKey)).iterator());
         this.parser(new Parser<>() {
             @SuppressWarnings("NullableProblems")
             @Override
