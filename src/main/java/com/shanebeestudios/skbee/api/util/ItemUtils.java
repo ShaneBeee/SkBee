1243e6bddbcab0570f168b026bcfb81002094442
ShaneBeee <shanebolenback@me.com>
2025-01-04 12:34:54 -0800

    ItemUtils - add quick method for modifying components

--- src/main/java/com/shanebeestudios/skbee/api/util/ItemUtils.java
+++ src/main/java/com/shanebeestudios/skbee/api/util/ItemUtils.java
@@ -2,13 +2,17 @@ package com.shanebeestudios.skbee.api.util;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.util.slot.InventorySlot;
 import ch.njol.skript.util.slot.Slot;
+import io.papermc.paper.datacomponent.DataComponentType;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -167,6 +171,28 @@ public class ItemUtils {
         } else if (object instanceof Slot slot && slot instanceof InventorySlot) {
             slot.setItem(itemStack);
         }
+    }
+
+    /**
+     * Quick method to modify {@link DataComponentType DataComponents} of items
+     *
+     * @param objects Item based objects to modify (Accepts can be {@link ItemStack}, {@link ItemType} or {@link Slot})
+     * @param mode    ChangeMode passed from expression
+     * @param type    DataComponentType to modify
+     * @param value   Value to set
+     * @param <T>     DataComponentType value
+     */
+    @SuppressWarnings("UnstableApiUsage")
+    public static <T> void modifyComponent(Object[] objects, ChangeMode mode, @NotNull DataComponentType.Valued<T> type, @Nullable T value) {
+        modifyItems(objects, itemStack -> {
+            if (mode == ChangeMode.SET && value != null) {
+                itemStack.setData(type, value);
+            } else if (mode == ChangeMode.DELETE) {
+                itemStack.unsetData(type);
+            } else if (mode == ChangeMode.RESET) {
+                itemStack.resetData(type);
+            }
+        });
     }
 
 }
