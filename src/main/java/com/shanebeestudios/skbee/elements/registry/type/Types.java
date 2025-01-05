25441f5c62faa31a9abab1378f2e857aac62e3a0
ShaneBeee <shanebolenback@me.com>
2025-01-04 23:25:25 -0800

    Registry/Types - add parsers for TagKey and TypedKey

--- src/main/java/com/shanebeestudios/skbee/elements/registry/type/Types.java
+++ src/main/java/com/shanebeestudios/skbee/elements/registry/type/Types.java
@@ -1,6 +1,8 @@
 package com.shanebeestudios.skbee.elements.registry.type;
 
 import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.Parser;
+import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import com.shanebeestudios.skbee.SkBee;
 import com.shanebeestudios.skbee.api.registry.RegistryUtils;
@@ -32,13 +34,45 @@ public class Types {
             .user("tag ?keys?")
             .name("Registry - Tag Key")
             .description("Represents a key for a Minecraft tag.")
-            .since("INSERT VERSION"));
+            .since("INSERT VERSION")
+            .parser(new Parser<>() {
+                @Override
+                public boolean canParse(ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                public String toString(TagKey tagKey, int flags) {
+                    return "#" + tagKey.key() + " (in " + Classes.toString(tagKey.registryKey()) + ")";
+                }
+
+                @Override
+                public String toVariableNameString(TagKey tagKey) {
+                    return toString(tagKey, 0);
+                }
+            }));
 
         Classes.registerClass(new ClassInfo<>(TypedKey.class, "typedkey")
             .user("typed ?keys?")
             .name("Registry - Typed Key")
             .description("Represents the key for a value in a Minecraft registry.")
-            .since("INSERT VERSION"));
+            .since("INSERT VERSION")
+            .parser(new Parser<>() {
+                @Override
+                public boolean canParse(ParseContext context) {
+                    return false;
+                }
+
+                @Override
+                public String toString(TypedKey typedKey, int flags) {
+                    return typedKey.key() + " (in " + Classes.toString(typedKey.registryKey()) + ")";
+                }
+
+                @Override
+                public String toVariableNameString(TypedKey typedKey) {
+                    return toString(typedKey, 0);
+                }
+            }));
     }
 
 }
