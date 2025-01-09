81cc2706fa8364a5188cc50da7887543a5cf2c33
ShaneBeee <shanebolenback@me.com>
2025-01-08 18:03:06 -0800

    RegistryHolders - add comparators

--- src/main/java/com/shanebeestudios/skbee/api/registry/RegistryHolders.java
+++ src/main/java/com/shanebeestudios/skbee/api/registry/RegistryHolders.java
@@ -3,12 +3,18 @@ package com.shanebeestudios.skbee.api.registry;
 import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
+import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.TypedKey;
+import io.papermc.paper.registry.tag.TagKey;
+import net.kyori.adventure.key.Key;
 import org.bukkit.GameEvent;
 import org.bukkit.Keyed;
 import org.bukkit.Particle;
+import org.bukkit.Registry;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Biome;
+import org.bukkit.block.BlockType;
 import org.bukkit.damage.DamageType;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -19,6 +25,9 @@ import org.bukkit.inventory.meta.trim.TrimPattern;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.lang.comparator.Comparator;
+import org.skriptlang.skript.lang.comparator.Comparators;
+import org.skriptlang.skript.lang.comparator.Relation;
 import org.skriptlang.skript.lang.converter.Converter;
 
 import java.util.ArrayList;
@@ -27,26 +36,38 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Supplier;
 
-@SuppressWarnings({"UnstableApiUsage", "rawtypes", "deprecation", "removal"})
+@SuppressWarnings({"UnstableApiUsage", "rawtypes", "deprecation", "removal", "NullableProblems", "unchecked", "DataFlowIssue"})
 public class RegistryHolders {
 
+    private static final Registry<BlockType> BLOCK_REGISTRY = RegistryUtils.getRegistry(RegistryKey.BLOCK);
+    private static final Registry<org.bukkit.inventory.ItemType> ITEM_REGISTRY = RegistryUtils.getRegistry(RegistryKey.ITEM);
     private static final Map<String, RegistryHolder> REGISTRY_HOLDERS_BY_NAME = new HashMap<>();
     private static final Map<RegistryKey, RegistryHolder> REGISTRY_HOLDERS_BY_REGISTRY_KEY = new HashMap<>();
 
     static {
+        // Only register once
+        Comparator<TagKey, ItemType> itemTypeComparator = (tagKey, itemType) -> {
+            Key key = itemType.getMaterial().key();
+
+            if (tagKey.registryKey() == RegistryKey.BLOCK) {
+                TypedKey<BlockType> typedKey = TypedKey.create(RegistryKey.BLOCK, key);
+                return Relation.get(BLOCK_REGISTRY.getTag(tagKey).contains(typedKey));
+            } else if (tagKey.registryKey() == RegistryKey.ITEM) {
+                TypedKey<org.bukkit.inventory.ItemType> typedKey = TypedKey.create(RegistryKey.ITEM, key);
+                return Relation.get(ITEM_REGISTRY.getTag(tagKey).contains(typedKey));
+            }
+            return null;
+        };
         register(RegistryKey.ATTRIBUTE, Attribute.class);
         register(RegistryKey.BIOME, Biome.class);
-        register(RegistryKey.BLOCK, ItemType.class, blockType ->
-            new ItemType(Objects.requireNonNull(blockType.asMaterial())));
+        register(RegistryKey.BLOCK, ItemType.class, blockType -> new ItemType(blockType.asMaterial()), itemTypeComparator);
         register(RegistryKey.ENCHANTMENT, Enchantment.class);
         register(RegistryKey.ENTITY_TYPE, EntityType.class);
         register(RegistryKey.DAMAGE_TYPE, DamageType.class);
         register(RegistryKey.GAME_EVENT, GameEvent.class);
-        register(RegistryKey.ITEM, ItemType.class, itemType ->
-            new ItemType(Objects.requireNonNull(itemType.asMaterial())));
+        register(RegistryKey.ITEM, ItemType.class, itemType -> new ItemType(itemType.asMaterial()));
         register(RegistryKey.MOB_EFFECT, PotionEffectType.class);
         register(RegistryKey.PARTICLE_TYPE, Particle.class);
         register(RegistryKey.SOUND_EVENT, String.class, soundEvent -> soundEvent.key().toString());
@@ -57,17 +78,29 @@ public class RegistryHolders {
         register(RegistryKey.VILLAGER_TYPE, Villager.Type.class);
     }
 
-    private static <F extends Keyed, T> void register(RegistryKey<F> key, Class<T> returnType) {
-        register(key, returnType, null);
+    private static <F extends Keyed, T extends Keyed> void register(RegistryKey<F> key, Class<T> returnType) {
+        register(key, returnType, null, (o1, o2) -> {
+            TypedKey<F> typedKey = TypedKey.create(o1.registryKey(), o2.key());
+            return Relation.get(RegistryAccess.registryAccess().getRegistry(o1.registryKey()).getTag(o1).contains(typedKey));
+        });
     }
 
     private static <F extends Keyed, T> void register(RegistryKey<F> key, Class<T> returnType, @Nullable Converter<F, T> converter) {
+        register(key, returnType, converter, null);
+    }
+
+    private static <F extends Keyed, T> void register(RegistryKey<F> key, Class<T> returnType, @Nullable Converter<F, T> converter, @Nullable Comparator<TagKey, T> tagComparator) {
         String name = key.key().value();
         name = name.substring(name.lastIndexOf("/") + 1);
         name = name + " registry";
         RegistryHolder<F, T> registryHolder = new RegistryHolder<>(key, returnType, name, converter);
         REGISTRY_HOLDERS_BY_NAME.put(name, registryHolder);
         REGISTRY_HOLDERS_BY_REGISTRY_KEY.put(key, registryHolder);
+
+        Class<TagKey> tkc = TagKey.class;
+        if (tagComparator != null) {
+            Comparators.registerComparator(tkc, returnType, tagComparator);
+        }
     }
 
     public static String getDocUsage() {
