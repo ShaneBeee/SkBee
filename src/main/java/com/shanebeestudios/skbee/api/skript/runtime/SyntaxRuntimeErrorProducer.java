e1c57fee5a0cc302658ee0f0d75faef46d4ade5d
ShaneBeee <shanebolenback@me.com>
2025-01-15 14:34:43 -0800

    SyntaxRuntimeErrorProducer - add regex option

--- src/main/java/com/shanebeestudios/skbee/api/skript/runtime/SyntaxRuntimeErrorProducer.java
+++ src/main/java/com/shanebeestudios/skbee/api/skript/runtime/SyntaxRuntimeErrorProducer.java
@@ -5,6 +5,9 @@ import ch.njol.skript.test.runner.TestMode;
 import com.shanebeestudios.skbee.elements.testing.elements.ExprLastRuntimeLogs;
 import com.shanebeestudios.skbee.elements.testing.elements.SecTryCatch;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Extension of Skript's SyntaxRuntimeErrorProducer which creates runtime errors
  * for use in {@link SecTryCatch} and {@link ExprLastRuntimeLogs}
@@ -17,10 +20,24 @@ public interface SyntaxRuntimeErrorProducer extends org.skriptlang.skript.log.ru
         error(message, null);
     }
 
+    default void errorRegex(String message, String regex) {
+        if (TestMode.ENABLED) {
+            Skript.error(message);
+            return;
+        }
+        String fullLine = getNode().save().trim();
+        Pattern pattern = Pattern.compile("(" + regex + ")");
+        Matcher matcher = pattern.matcher(fullLine);
+        if (matcher.find()) {
+            regex = matcher.group(1);
+        }
+        org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer.super.error(message, regex);
+    }
+
     @Override
     default void error(String message, String highlight) {
         if (TestMode.ENABLED) {
-            Skript.error( message);
+            Skript.error(message);
             return;
         }
         org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer.super.error(message, highlight);
@@ -31,10 +48,24 @@ public interface SyntaxRuntimeErrorProducer extends org.skriptlang.skript.log.ru
         warning(message, null);
     }
 
+    default void warningRegex(String message, String regex) {
+        if (TestMode.ENABLED) {
+            Skript.error(message);
+            return;
+        }
+        String fullLine = getNode().save().trim();
+        Pattern pattern = Pattern.compile("(" + regex + ")");
+        Matcher matcher = pattern.matcher(fullLine);
+        if (matcher.find()) {
+            regex = matcher.group(1);
+        }
+        org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer.super.warning(message, regex);
+    }
+
     @Override
     default void warning(String message, String highlight) {
         if (TestMode.ENABLED) {
-            Skript.error( message);
+            Skript.error(message);
             return;
         }
         org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer.super.warning(message, highlight);
