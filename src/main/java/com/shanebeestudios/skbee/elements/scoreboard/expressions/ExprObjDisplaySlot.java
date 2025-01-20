17af1e5bd24148cfcb01df8e82d761e9d5aae233
ShaneBeee <shanebolenback@me.com>
2025-01-19 16:02:21 -0800

    ExprObjDisplaySlot - cleanup

--- src/main/java/com/shanebeestudios/skbee/elements/scoreboard/expressions/ExprObjDisplaySlot.java
+++ src/main/java/com/shanebeestudios/skbee/elements/scoreboard/expressions/ExprObjDisplaySlot.java
@@ -9,14 +9,15 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.registrations.Classes;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import com.shanebeestudios.skbee.api.skript.base.SimpleExpression;
 import org.bukkit.event.Event;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Name("Scoreboard - Objective DisplaySlot")
 @Description("Get/Set the display slot of an objective.")
@@ -25,40 +26,55 @@ import org.jetbrains.annotations.NotNull;
 public class ExprObjDisplaySlot extends SimpleExpression<DisplaySlot> {
 
     static {
-        Skript.registerExpression(ExprObjDisplaySlot.class, DisplaySlot.class, ExpressionType.COMBINED, "" +
-                "display[ ]slot of %objective%");
+        Skript.registerExpression(ExprObjDisplaySlot.class, DisplaySlot.class, ExpressionType.COMBINED,
+            "[objective] display[ ]slot of %objective%");
     }
 
     private Expression<Objective> objective;
 
-    @SuppressWarnings({"NullableProblems", "unchecked"})
+    @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         this.objective = (Expression<Objective>) exprs[0];
         return true;
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     protected @Nullable DisplaySlot[] get(Event event) {
         Objective objective = this.objective.getSingle(event);
-        if (objective == null) return null;
+        if (objective == null) {
+            error("Objective is not set: " + this.objective.toString(event, true));
+            return null;
+        }
         return new DisplaySlot[]{objective.getDisplaySlot()};
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
         if (mode == ChangeMode.SET) return CollectionUtils.array(DisplaySlot.class);
+        else if (mode == ChangeMode.DELETE) return CollectionUtils.array();
         return null;
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
         Objective objective = this.objective.getSingle(event);
-        if (delta[0] instanceof DisplaySlot displaySlot && mode == ChangeMode.SET && objective != null) {
-            objective.setDisplaySlot(displaySlot);
+        if (objective == null) {
+            error("Objective is not set: " + this.objective.toString(event, true));
+            return;
+        }
+        if (mode == ChangeMode.DELETE) {
+            objective.setDisplaySlot(null);
+        } else if (mode == ChangeMode.SET) {
+            if (delta == null) {
+                error("Change value is not set");
+                return;
+            }
+            if (delta[0] instanceof DisplaySlot displaySlot) {
+                objective.setDisplaySlot(displaySlot);
+            } else {
+                error("Invalid object for display slot: " + Classes.toString(delta[0]));
+            }
         }
     }
 
@@ -74,7 +90,7 @@ public class ExprObjDisplaySlot extends SimpleExpression<DisplaySlot> {
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean d) {
-        return "display slot of objective " + this.objective.toString(e,d);
+        return "objective display slot of " + this.objective.toString(e, d);
     }
 
 }
