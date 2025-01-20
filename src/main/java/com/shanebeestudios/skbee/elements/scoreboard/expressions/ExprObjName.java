011662982246a618cd7a30bf6213c4de66c08831
ShaneBeee <shanebolenback@me.com>
2025-01-19 16:02:50 -0800

    ExprObjName - add support for components

--- src/main/java/com/shanebeestudios/skbee/elements/scoreboard/expressions/ExprObjName.java
+++ src/main/java/com/shanebeestudios/skbee/elements/scoreboard/expressions/ExprObjName.java
@@ -9,9 +9,11 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import com.shanebeestudios.skbee.SkBee;
+import com.shanebeestudios.skbee.api.skript.base.SimpleExpression;
+import com.shanebeestudios.skbee.api.wrapper.ComponentWrapper;
 import org.bukkit.event.Event;
 import org.bukkit.scoreboard.Objective;
 import org.jetbrains.annotations.NotNull;
@@ -20,34 +22,52 @@ import org.jetbrains.annotations.Nullable;
 @Name("Scoreboard - Objective Name")
 @Description({"Represents the name/display name of an objective.",
     "- `name` = The name/id given to the objective (Cannot be changed).",
-    "- `display name` = The name the players will see (Can be changed)."})
+    "- `display name` = The name the players will see [as a string] (Can be changed).",
+    "- `component display name` = The name the players will see [as a text component] (Can be changed)."})
 @Examples("set objective display name of {_objective} to \"le-objective\"")
 @Since("2.6.0")
-public class ExprObjName extends SimpleExpression<String> {
+public class ExprObjName extends SimpleExpression<Object> {
+
+    private static final boolean HAS_COMP = SkBee.getPlugin().getAddonLoader().isTextComponentEnabled();
+    private static final Class<?>[] RETURN_CLASSES;
 
     static {
-        Skript.registerExpression(ExprObjName.class, String.class, ExpressionType.COMBINED,
+        if (HAS_COMP) {
+            RETURN_CLASSES = CollectionUtils.array(String.class, ComponentWrapper.class);
+        } else {
+            RETURN_CLASSES = CollectionUtils.array(String.class);
+        }
+        Skript.registerExpression(ExprObjName.class, Object.class, ExpressionType.COMBINED,
             "objective (name|id) of %objective%",
-            "objective display name of %objective%");
+            HAS_COMP ? "objective [:component] display name of %objective%" : "objective display name of %objective%");
     }
 
     private Expression<Objective> objective;
     private boolean display;
+    private boolean component;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         this.objective = (Expression<Objective>) exprs[0];
         this.display = matchedPattern == 1;
+        this.component = parseResult.hasTag("component");
         return true;
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    protected @Nullable String[] get(Event event) {
+    protected @Nullable Object[] get(Event event) {
         Objective objective = this.objective.getSingle(event);
-        if (objective == null) return null;
-        return new String[]{this.display ? objective.getDisplayName() : objective.getName()};
+        if (objective == null) {
+            error("Objective is not set: " + this.objective.toString(event, true));
+            return null;
+        }
+        if (this.display) {
+            if (this.component) return new ComponentWrapper[]{ComponentWrapper.fromComponent(objective.displayName())};
+            return new String[]{objective.getDisplayName()};
+        }
+        return new Object[]{objective.getName()};
     }
 
     @Override
@@ -57,17 +77,25 @@ public class ExprObjName extends SimpleExpression<String> {
                 Skript.error("Cannot change the name of an objective.");
                 return null;
             }
-            return CollectionUtils.array(String.class);
+            return RETURN_CLASSES;
         }
         return null;
     }
 
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({"deprecation", "DataFlowIssue"})
     @Override
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
         Objective objective = this.objective.getSingle(event);
         if (delta[0] instanceof String name && mode == ChangeMode.SET && objective != null) {
             objective.setDisplayName(name);
+        }
+
+        if (delta != null) {
+            if (delta[0] instanceof String string) {
+                objective.setDisplayName(string);
+            } else if (HAS_COMP && delta[0] instanceof ComponentWrapper cw) {
+                objective.displayName(cw.getComponent());
+            }
         }
     }
 
@@ -77,8 +105,8 @@ public class ExprObjName extends SimpleExpression<String> {
     }
 
     @Override
-    public @NotNull Class<? extends String> getReturnType() {
-        return String.class;
+    public @NotNull Class<?> getReturnType() {
+        return this.component ? ComponentWrapper.class : String.class;
     }
 
     @Override
