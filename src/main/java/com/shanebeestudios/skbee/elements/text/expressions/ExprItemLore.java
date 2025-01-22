8718f4e11a9f3917549c759f7205f5a2e383f708
ShaneBeee <shanebolenback@me.com>
2025-01-22 15:15:02 -0800

    TextComponent - more tests and a few minor bug fixes - Not really BUGs per se, but just odd behaviour

--- src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprItemLore.java
+++ src/main/java/com/shanebeestudios/skbee/elements/text/expressions/ExprItemLore.java
@@ -1,7 +1,6 @@
 package com.shanebeestudios.skbee.elements.text.expressions;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
@@ -11,14 +10,12 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.skript.util.slot.InventorySlot;
-import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import com.shanebeestudios.skbee.api.util.ItemUtils;
 import com.shanebeestudios.skbee.api.wrapper.ComponentWrapper;
 import net.kyori.adventure.text.Component;
 import org.bukkit.event.Event;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -34,100 +31,74 @@ public class ExprItemLore extends SimpleExpression<ComponentWrapper> {
 
     static {
         Skript.registerExpression(ExprItemLore.class, ComponentWrapper.class, ExpressionType.PROPERTY,
-                "[the] component [item] lore of %itemstack/itemtype/slot%",
-                "%itemstack/itemtype/slot%'[s] component [item] lore");
+            "[the] component [item] lore of %itemstack/itemtype/slot%",
+            "%itemstack/itemtype/slot%'[s] component [item] lore");
     }
 
     private Expression<?> item;
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         this.item = exprs[0];
         return true;
     }
 
-    @SuppressWarnings("NullableProblems")
+    @SuppressWarnings("DataFlowIssue")
     @Override
     protected ComponentWrapper @Nullable [] get(Event event) {
-        ItemMeta meta;
-        Object item = this.item.getSingle(event);
-        if (item instanceof ItemType itemType) {
-            meta = itemType.getItemMeta();
-        } else if (item instanceof ItemStack itemStack) {
-            meta = itemStack.getItemMeta();
-        } else if (item instanceof Slot slot) {
-            ItemStack slotItem = slot.getItem();
-            if (slotItem != null) {
-                meta = slotItem.getItemMeta();
-            } else {
-                return null;
+        List<ComponentWrapper> components = ItemUtils.getValue(this.item.getSingle(event), itemStack -> {
+            List<ComponentWrapper> lores = new ArrayList<>();
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            if (itemMeta.hasLore()) {
+                for (Component component : itemMeta.lore()) {
+                    lores.add(ComponentWrapper.fromComponent(component));
+                }
             }
-        } else {
-            return null;
-        }
-
-        List<ComponentWrapper> components = new ArrayList<>();
-        List<Component> lore = meta.lore();
-        if (lore != null) {
-            lore.forEach(component -> components.add(ComponentWrapper.fromComponent(component)));
-        }
+            return lores;
+        });
+        if (components == null) return null;
         return components.toArray(new ComponentWrapper[0]);
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
         if (mode == ChangeMode.SET || mode == ChangeMode.ADD) {
             return CollectionUtils.array(ComponentWrapper[].class, String[].class);
+        } else if (mode == ChangeMode.DELETE) {
+            return CollectionUtils.array();
         }
         return null;
     }
 
-    @SuppressWarnings("NullableProblems")
+    @SuppressWarnings("DataFlowIssue")
     @Override
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
-        Object item = this.item.getSingle(event);
-        ItemMeta itemMeta;
-        if (item instanceof ItemType itemType) {
-            itemMeta = itemType.getItemMeta();
-        } else if (item instanceof ItemStack itemStack) {
-            itemMeta = itemStack.getItemMeta();
-        } else if (item instanceof Slot slot) {
-            ItemStack slotItem = slot.getItem();
-            if (slotItem == null) return;
-            itemMeta = slotItem.getItemMeta();
-        } else {
-            return;
-        }
+        List<Component> lores = new ArrayList<>();
 
-        if (itemMeta == null) return;
-
-        List<Component> lores = mode == ChangeMode.ADD ? itemMeta.lore() : new ArrayList<>();
-        if (lores == null) {
-            lores = new ArrayList<>();
-        }
-
-        for (Object object : delta) {
-            if (object instanceof ComponentWrapper component) {
-                lores.add(component.getComponent());
-            } else if (object instanceof String string) {
-                lores.add(ComponentWrapper.fromText(string).getComponent());
+        if (delta != null) {
+            for (Object object : delta) {
+                if (object instanceof ComponentWrapper component) {
+                    lores.add(component.getComponent());
+                } else if (object instanceof String string) {
+                    lores.add(ComponentWrapper.fromText(string).getComponent());
+                }
             }
         }
-        itemMeta.lore(lores);
-        if (item instanceof ItemType itemType) {
-            itemType.setItemMeta(itemMeta);
-        } else if (item instanceof Slot slot) {
-            ItemStack slotItem = slot.getItem();
-            slotItem.setItemMeta(itemMeta);
-            if (slot instanceof InventorySlot invSlot) {
-                invSlot.setItem(slotItem);
+
+        ItemUtils.modifyItems(this.item.getSingle(event), itemStack -> {
+            List<Component> components = new ArrayList<>();
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            if (mode == ChangeMode.ADD && itemMeta.hasLore()) {
+                components.addAll(itemMeta.lore());
             }
-        } else {
-            ItemStack itemStack = (ItemStack) item;
+            if (!lores.isEmpty()) {
+                components.addAll(lores);
+            }
+            itemMeta.lore(components.isEmpty() ? null : components);
             itemStack.setItemMeta(itemMeta);
-        }
+        });
+
     }
 
     @Override
@@ -140,9 +111,8 @@ public class ExprItemLore extends SimpleExpression<ComponentWrapper> {
         return ComponentWrapper.class;
     }
 
-    @SuppressWarnings("DataFlowIssue")
     @Override
-    public @NotNull String toString(@Nullable Event e, boolean d) {
+    public @NotNull String toString(Event e, boolean d) {
         return "component item lore of " + this.item.toString(e, d);
     }
 
