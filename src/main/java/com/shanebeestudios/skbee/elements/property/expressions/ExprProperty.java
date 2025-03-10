86cb0b739c57bc295db51d563e727534dabde4be
Shane Bee <shanebolenback@me.com>
2025-03-10 10:13:28 -0700

    Dev/property - Add new property expression (#756)
    * Property - base setup
    
    * more properties
    
    * PropertyPrinter - add link for expression
    
    * ItemProperties - fix a typo
    
    * Property - add not for when property cannot be changed
    
    * ExprProperty - fix error with literals
    
    * properties.sk - add some tests
    
    * Property - add reset option
    
    * Property - some renaming
    
    * Property - more cleanup/refactoring

---
+++ src/main/java/com/shanebeestudios/skbee/elements/property/expressions/ExprProperty.java
@@ -0,0 +1,129 @@
+package com.shanebeestudios.skbee.elements.property.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.util.LiteralUtils;
+import ch.njol.util.Kleenean;
+import com.shanebeestudios.skbee.api.property.Property;
+import com.shanebeestudios.skbee.api.skript.base.SimpleExpression;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.lang.converter.Converters;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Name("Property - Property of Object")
+@Description({"Represents different properties of an object.",
+    "See [**Property Wiki**](https://github.com/ShaneBeee/SkBee/wiki/Properties) for available properties and examples."})
+@Examples("")
+@Since("INSERT VERSION")
+@SuppressWarnings("unchecked")
+public class ExprProperty<F, T> extends SimpleExpression<T> {
+
+    static {
+        Skript.registerExpression(ExprProperty.class, Object.class, ExpressionType.COMBINED,
+            "%*property% property of %objects%",
+            "%objects%'[s] %*property% property");
+    }
+
+    private Literal<Property<F, T>> property;
+    private Expression<F> objects;
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.property = (Literal<Property<F, T>>) exprs[matchedPattern];
+        this.objects = LiteralUtils.defendExpression( exprs[matchedPattern == 0 ? 1 : 0]);
+        if (this.objects.getReturnType().isAssignableFrom(this.property.getSingle().getPropertyHolder())) return true;
+        if (this.property.getSingle().getPropertyHolder().isAssignableFrom(this.objects.getReturnType())) return true;
+        if (Converters.converterExists(this.objects.getReturnType(), this.property.getSingle().getPropertyHolder()))
+            return true;
+
+        ClassInfo<F> propertyInfo = Classes.getExactClassInfo(this.property.getSingle().getPropertyHolder());
+        ClassInfo<? extends F> found = Classes.getExactClassInfo(this.objects.getReturnType());
+
+        Skript.error("Property '" + this.property.getSingle().getName() + "' can only be used on '" + propertyInfo + "' but found '" + found + "'");
+        return false;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    protected T @Nullable [] get(Event event) {
+        List<T> objects = new ArrayList<>();
+        Property<F, T> property = this.property.getSingle();
+        if (property == null) return null;
+
+        for (F from : this.objects.getArray(event)) {
+            if (!property.getPropertyHolder().isAssignableFrom(from.getClass())) {
+                // If the object types don't match, let's try convert
+                from = Converters.convert(from, property.getPropertyHolder());
+                if (from != null && !property.getPropertyHolder().isAssignableFrom(from.getClass())) continue;
+            }
+            T propertyReturn = property.get(from);
+            if (property.isArray()) {
+                objects.addAll(Arrays.asList((T[]) propertyReturn));
+            } else {
+                objects.add(propertyReturn);
+            }
+        }
+        return (T[]) objects.toArray(new Object[0]);
+    }
+
+    @Override
+    public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
+        if (this.property == null || this.property.getSingle() == null) return null;
+        return this.property.getSingle().acceptChange(mode);
+    }
+
+    @SuppressWarnings({"ConstantValue", "unchecked"})
+    @Override
+    public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+        Property<F, T> property = this.property.getSingle();
+        Object change = delta != null ? delta[0] : null;
+        if (property == null) return;
+
+        for (F object : this.objects.getArray(event)) {
+            if (!property.getPropertyHolder().isAssignableFrom(object.getClass())) {
+                // If the object types don't match, let's try convert
+                object = Converters.convert(object, property.getPropertyHolder());
+                if (object != null && !property.getPropertyHolder().isAssignableFrom(object.getClass())) continue;
+            }
+            switch (mode) {
+                case SET -> property.set(object, property.isArray() ? (T) delta : (T) change);
+                case ADD -> property.add(object, property.isArray() ? (T) delta : (T) change);
+                case REMOVE -> property.remove(object, property.isArray() ? (T) delta : (T) change);
+                case DELETE -> property.delete(object);
+                case RESET -> property.reset(object);
+            }
+        }
+    }
+
+    @Override
+    public boolean isSingle() {
+        return this.objects.isSingle() && !this.property.getSingle().getReturnType().isArray();
+    }
+
+    @Override
+    public @NotNull Class<? extends T> getReturnType() {
+        return this.property.getSingle().getReturnType();
+    }
+
+    @Override
+    public @NotNull String toString(Event e, boolean d) {
+        return this.property.toString(e, d) + " property of " + this.objects.toString(e, d);
+    }
+
+}
---
+++ src/main/java/com/shanebeestudios/skbee/elements/property/properties/EntityProperties.java
@@ -0,0 +1,193 @@
+package com.shanebeestudios.skbee.elements.property.properties;
+
+import ch.njol.skript.aliases.ItemType;
+import com.shanebeestudios.skbee.api.property.Property;
+import com.shanebeestudios.skbee.api.property.PropertyRegistry;
+import com.shanebeestudios.skbee.api.util.Util;
+import org.bukkit.attribute.Attributable;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Sittable;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"deprecation", "unused"})
+public class EntityProperties {
+
+    static {
+        PropertyRegistry.registerProperty("aggressive", new Property<>(Mob.class, Boolean.class) {
+                @Override
+                public Boolean get(Mob mob) {
+                    return mob.isAggressive();
+                }
+
+                @Override
+                public void set(Mob mob, Boolean value) {
+                    mob.setAggressive(value);
+                }
+            })
+            .description("Whether the mob is aggressive. This will not work on all mobs, only mobs that can actually be aggressive.")
+            .examples("set aggressive property of event-mob to true")
+            .since("INSERT VERSION");
+
+        PropertyRegistry.registerProperty("health", new Property<>(Entity.class, Number.class) {
+                @Override
+                public Number get(Entity entity) {
+                    if (entity instanceof Damageable damageable) return damageable.getHealth();
+                    return 0;
+                }
+
+                @Override
+                public void set(Entity entity, Number value) {
+                    if (entity instanceof Damageable damageable) damageable.setHealth(value.doubleValue());
+                }
+
+                @Override
+                public void add(Entity entity, Number value) {
+                    if (entity instanceof Damageable damageable) {
+                        damageable.setHealth(damageable.getHealth() + value.doubleValue());
+                    }
+                }
+
+                @Override
+                public void remove(Entity entity, Number value) {
+                    if (entity instanceof Damageable damageable) {
+                        double health = damageable.getHealth();
+                        health -= value.doubleValue();
+                        damageable.setHealth(Math.max(health, 0));
+                    }
+                }
+
+                @Override
+                public void reset(Entity entity) {
+                    if (entity instanceof Damageable damageable && entity instanceof Attributable attributable) {
+                        AttributeInstance instance = attributable.getAttribute(Attribute.MAX_HEALTH);
+                        if (instance != null) {
+                            double baseValue = instance.getBaseValue();
+                            damageable.setHealth(baseValue);
+                        } else {
+                            Util.log("NULL???");
+                        }
+                    } else {
+                        Util.log("Not instance?!?!?!");
+                    }
+                }
+            })
+            .description("Represents the health of an entity.")
+            .examples("set {_h} to health property of player",
+                "set health property of player to 10",
+                "add 1 to health property of player",
+                "remove 1 from health property of target entity")
+            .since("INSERT VERSION");
+
+        PropertyRegistry.registerProperty("inventory contents", new Property<>(InventoryHolder.class, ItemType[].class) {
+                @Override
+                public ItemType[] get(InventoryHolder inventoryHolder) {
+                    List<ItemType> items = new ArrayList<>();
+                    for (@Nullable ItemStack content : inventoryHolder.getInventory().getContents()) {
+                        if (content == null) continue;
+                        items.add(new ItemType(content));
+                    }
+                    return items.toArray(new ItemType[0]);
+                }
+
+                @Override
+                public void set(InventoryHolder inventoryHolder, ItemType[] itemTypes) {
+                    ItemStack[] items = new ItemStack[itemTypes.length];
+                    for (int i = 0; i < itemTypes.length; i++) {
+                        items[i] = itemTypes[i].getRandom();
+                    }
+                    inventoryHolder.getInventory().setContents(items);
+                }
+
+                @Override
+                public void add(InventoryHolder inventoryHolder, ItemType[] itemTypes) {
+                    Inventory inventory = inventoryHolder.getInventory();
+                    for (ItemType itemType : itemTypes) {
+                        itemType.addTo(inventory);
+                    }
+                }
+
+                @Override
+                public void remove(InventoryHolder inventoryHolder, ItemType[] itemTypes) {
+                    Inventory inventory = inventoryHolder.getInventory();
+                    for (ItemType itemType : itemTypes) {
+                        itemType.removeFrom(inventory);
+                    }
+                }
+
+                @Override
+                public void delete(InventoryHolder inventoryHolder) {
+                    inventoryHolder.getInventory().clear();
+                }
+            })
+            .description("Represents the contents of an object that holds an inventory.")
+            .examples("set {_i::*} to inventory contents property of player",
+                "add an apple to inventory contents property of player",
+                "remove all diamonds from inventory contents property of player")
+            .since("INSERT VERSION");
+
+        PropertyRegistry.registerProperty("name", new Property<>(Entity.class, String.class) {
+                @Override
+                public String get(Entity object) {
+                    return object.getName();
+                }
+
+                @Override
+                public void set(Entity object, String value) {
+                    object.setCustomName(value);
+                }
+
+                @Override
+                public void delete(Entity object) {
+                    object.setCustomName(null);
+                }
+            })
+            .description("Represents the name of an entity.")
+            .since("INSERT VERSION");
+
+        PropertyRegistry.registerProperty("persistence required",  new Property<>(LivingEntity.class, Boolean.class) {
+            @Override
+            public Boolean get(LivingEntity livingEntity) {
+                return !livingEntity.getRemoveWhenFarAway();
+            }
+
+                @Override
+                public void set(LivingEntity livingEntity, Boolean value) {
+                    livingEntity.setRemoveWhenFarAway(!value);
+                }
+            })
+            .description("Prevent mobs from despawning naturally.",
+                "See the [Despawning](https://minecraft.wiki/w/Mob_spawning#Despawning) section McWiki for further details about despawning.",
+                "A silly side effect of this is that some mobs (such as sheep) will stop their random stroll goal when more than " +
+                    "32 blocks away from a player, setting this to true will prevent that and the mob will forever roam the lands.")
+            .examples("set persistence required property of all mobs to true")
+            .since("INSERT VERSION");
+
+        PropertyRegistry.registerProperty("sitting", new Property<>(Sittable.class, Boolean.class) {
+                @Override
+                public Boolean get(Sittable sittable) {
+                    return sittable.isSitting();
+                }
+
+                @Override
+                public void set(Sittable sittable, Boolean value) {
+                    sittable.setSitting(value);
+                }
+            })
+            .description("Whether an entity is sitting. Currently supports Camel, Cat, Fox, Panda, Parrot, Wolf.")
+            .examples("set sitting property of target entity to true",
+                "if sitting property of event-mob is false:")
+            .since("INSERT VERSION");
+    }
+
+}
