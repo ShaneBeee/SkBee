67a6bc211a7f2afdc98e5f4bdde00f914e4d3780
ShaneBeee <shanebolenback@me.com>
2025-06-02 10:06:52 -0700

    EffOpenSign - add virtual signs

--- src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffOpenSign.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffOpenSign.java
@@ -12,6 +12,7 @@ import ch.njol.skript.util.Direction;
 import ch.njol.util.Kleenean;
 import com.shanebeestudios.skbee.api.skript.base.Effect;
 import com.shanebeestudios.skbee.api.util.Util;
+import io.papermc.paper.math.Position;
 import org.bukkit.Location;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
@@ -23,30 +24,34 @@ import org.jetbrains.annotations.Nullable;
 
 @Name("Open Sign")
 @Description({"Open a sign's GUI to a player, to allow them to edit it.",
-    "If it wasn't obvious, the sign must actually be placed in the world, it's NOT a virtual sign.",
-    "Requires Paper (not sure which version, but at least 1.12.x).",
-    "Spigot added support for this in MC 1.18.",
-    "Front/Back support added in MC 1.20.",
-    "\n**NOTE**: This appears to no longer work in 1.20+ with a distance of 7.5+ (client side issue)."})
+    "`virtual`: (Added in Paper 1.21.5)",
+    " - Allows opening a client side sign to a player.",
+    " - You will have to send a client side sign to the player.",
+    " - The sign must be within 9 blocks of the player.",
+    " - You can use the `unchecked sign change` event to listen for changes to virtual signs.",
+    "If not using `virtual`, the block must be placed in the world.",
+    "Front/Back support added in MC 1.20.",})
 @Examples({"open sign gui of target block to player",
     "open target block's sign gui to player",
     "open sign back of target block to player"})
-@Since("1.5.2, 2.14.0 (sides)")
+@Since({"1.5.2", "2.14.0 (sides)", "INSERT VERSION (virtual)"})
 public class EffOpenSign extends Effect {
 
     private static final boolean HAS_SIDES = Util.IS_RUNNING_MC_1_20;
+    private static final boolean HAS_VIRTUAL = Skript.classExists("io.papermc.paper.event.packet.UncheckedSignChangeEvent");
 
     static {
         String side = HAS_SIDES ? "[(front|back:back)] " : "";
         Skript.registerEffect(EffOpenSign.class,
-            "open sign [gui] " + side + "[(for|of)] [%direction%] %location% to %players%",
-            "open [%direction%] %location%'[s] sign [gui] " + side + "to %players%");
+            "open [:virtual] sign [gui] " + side + "[(for|of)] [%direction%] %location% to %players%",
+            "open [%direction%] %location%'[s] [:virtual] sign [gui] " + side + "to %players%");
     }
 
     @SuppressWarnings("null")
     private Expression<Location> locations;
     private Expression<Player> players;
     private boolean back;
+    private boolean virtual;
 
     @SuppressWarnings("unchecked")
     @Override
@@ -54,10 +59,15 @@ public class EffOpenSign extends Effect {
         this.locations = Direction.combine((Expression<? extends Direction>) exprs[0], (Expression<? extends Location>) exprs[1]);
         this.players = (Expression<Player>) exprs[2];
         this.back = parseResult.hasTag("back");
+        this.virtual = parseResult.hasTag("virtual");
+        if (this.virtual && !HAS_VIRTUAL) {
+            Skript.error("Virtual sign opening requires Paper 1.21.5+");
+            return false;
+        }
         return true;
     }
 
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({"deprecation", "UnstableApiUsage"})
     @Override
     protected void execute(@NotNull Event event) {
         Location location = this.locations.getSingle(event);
@@ -66,7 +76,11 @@ public class EffOpenSign extends Effect {
         }
 
         BlockState block = location.getBlock().getState();
-        if (block instanceof Sign sign) {
+        if (this.virtual) {
+            for (Player player : this.players.getArray(event)) {
+                player.openVirtualSign(Position.block(location), this.back ? Side.BACK : Side.FRONT);
+            }
+        } else if (block instanceof Sign sign) {
             for (Player player : this.players.getArray(event)) {
                 if (HAS_SIDES) {
                     player.openSign(sign, this.back ? Side.BACK : Side.FRONT);
@@ -82,7 +96,8 @@ public class EffOpenSign extends Effect {
     @Override
     public @NotNull String toString(@Nullable Event e, boolean d) {
         String side = HAS_SIDES ? (this.back ? "back " : "front ") : "";
-        return "open sign " + side + "for " + this.locations.toString(e, d) + " to " + this.players.toString(e, d);
+        String virtual = this.virtual ? "virtual " : "";
+        return "open " + virtual + "sign " + side + "for " + this.locations.toString(e, d) + " to " + this.players.toString(e, d);
     }
 
 }
