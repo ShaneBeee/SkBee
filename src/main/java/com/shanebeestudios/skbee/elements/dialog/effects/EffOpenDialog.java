86e735fd7d99b6d5e438d01b9095bb996eb2bed7
ShaneBeee <shanebolenback@me.com>
2026-01-09 12:32:31 -0800

    EffOpenDialog - add option to close

--- src/main/java/com/shanebeestudios/skbee/elements/dialog/effects/EffOpenDialog.java
+++ src/main/java/com/shanebeestudios/skbee/elements/dialog/effects/EffOpenDialog.java
@@ -19,56 +19,74 @@ import org.bukkit.Registry;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-@Name("Dialog - Open Dialog")
+@Name("Dialog - Open/Close Dialog")
 @Description({"Open a dialog to players.",
-    "You can use keys from your own custom dialogs, as well as dialogs from datapacks."})
+    "You can use keys from your own custom dialogs, as well as dialogs from datapacks.",
+    "You can also close the currently open dialog of a player."})
 @Examples({"open dialog with id \"minecraft:my_dialog\" to player",
-    "open dialog with id \"my_pack:some_dialog\" to all players"})
+    "open dialog with id \"my_pack:some_dialog\" to all players",
+    "close dialogs of all players",
+    "close dialogs of world \"world\" #closes dialogs of all players in that world"})
 @Since("INSERT VERSION")
 public class EffOpenDialog extends Effect {
 
     private static final Registry<Dialog> REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.DIALOG);
 
     static {
-        Skript.registerEffect(EffOpenDialog.class, "open dialog with id %string/namespacedkey% to %audiences%");
+        Skript.registerEffect(EffOpenDialog.class,
+            "close dialog[s] of %audiences%",
+            "open dialog with id %string/namespacedkey% to %audiences%");
     }
 
+    private boolean close;
     private Expression<?> id;
     private Expression<Audience> audiences;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        this.id = exprs[0];
-        this.audiences = (Expression<Audience>) exprs[1];
+        this.close = matchedPattern == 0;
+        if (this.close) {
+            this.id = exprs[0];
+        }
+        this.audiences = (Expression<Audience>) exprs[matchedPattern];
         return true;
     }
 
     @Override
     protected void execute(Event event) {
-        Object singleId = this.id.getSingle(event);
-
-        NamespacedKey key;
-        if (singleId instanceof String string) {
-            key = NamespacedKey.fromString(string);
-        } else if (singleId instanceof NamespacedKey nsk) {
-            key = nsk;
+        if (this.close) {
+            for (Audience audience : this.audiences.getArray(event)) {
+                audience.closeDialog();
+            }
         } else {
-            return;
-        }
-        if (key == null) return;
+            Object singleId = this.id.getSingle(event);
+
+            NamespacedKey key;
+            if (singleId instanceof String string) {
+                key = NamespacedKey.fromString(string);
+            } else if (singleId instanceof NamespacedKey nsk) {
+                key = nsk;
+            } else {
+                return;
+            }
+            if (key == null) return;
 
 
-        Dialog dialog = REGISTRY.get(key);
-        if (dialog == null) return;
+            Dialog dialog = REGISTRY.get(key);
+            if (dialog == null) return;
 
-        for (Audience audience : this.audiences.getArray(event)) {
-            audience.showDialog(dialog);
+            for (Audience audience : this.audiences.getArray(event)) {
+                audience.showDialog(dialog);
+            }
         }
     }
 
     @Override
     public String toString(@Nullable Event e, boolean d) {
+        if (this.close) {
+            return "close dialogs of " + this.audiences.toString(e, d);
+        }
         return new SyntaxStringBuilder(e, d)
             .append("open dialog with id", this.id)
             .append("to", this.audiences)
