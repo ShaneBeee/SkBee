adbcada5fdf07509c0f6477d32ef78435adf78a6
ShaneBeee <shanebolenback@me.com>
2026-02-02 07:28:24 -0800

    build.gradle.kts - attempt to fix jitpack part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ tasks {
     }
     shadowJar {
         archiveFileName = project.name + "-" + projectVersion + ".jar"
+        archiveClassifier.set("")
         manifest.attributes["Main-Class"] = "com.shanebeestudios.skbee.game.GamesMain"
         relocate("de.tr7zw.changeme.nbtapi", "com.shanebeestudios.skbee.api.nbt")
         relocate("de.tr7zw.annotations", "com.shanebeestudios.skbee.api.nbt.annotations")
@@ -103,7 +104,7 @@ tasks {
 }
 
 publishing.publications.create("maven", MavenPublication::class.java) {
-    artifact(tasks["jar"])
+    artifact(tasks["shadowJar"])
     groupId = "com.github.shanebeestudios"
     artifactId = "SkBee"
     version = projectVersion
