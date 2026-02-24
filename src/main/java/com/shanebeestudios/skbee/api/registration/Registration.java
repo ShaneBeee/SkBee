a372e140174ef7c2ad34132eff0d89cb0d25c724
ShaneBeee <shanebolenback@me.com>
2026-02-24 10:19:37 -0800

    SkBeeAddonModule - move registration to Registration class - this makes for easier copying into my other addons

--- src/main/java/com/shanebeestudios/skbee/api/registration/Registration.java
+++ src/main/java/com/shanebeestudios/skbee/api/registration/Registration.java
@@ -2,6 +2,7 @@ package com.shanebeestudios.skbee.api.registration;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer;
+import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Cloner;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.classes.Serializer;
@@ -15,21 +16,28 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.Section;
 import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.function.Functions;
+import ch.njol.skript.registrations.Classes;
 import com.shanebeestudios.skbee.SkBeeAddonModule;
 import com.shanebeestudios.skbee.api.util.Util;
 import com.shanebeestudios.skbee.api.wrapper.EnumWrapper;
+import com.shanebeestudios.skbee.api.wrapper.RegistryClassInfo;
 import org.bukkit.Keyed;
 import org.bukkit.Registry;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.skriptlang.skript.addon.SkriptAddon;
+import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos;
 import org.skriptlang.skript.common.function.DefaultFunction;
 import org.skriptlang.skript.lang.entry.EntryValidator;
 import org.skriptlang.skript.lang.structure.Structure;
+import org.skriptlang.skript.registration.DefaultSyntaxInfos;
 import org.skriptlang.skript.registration.SyntaxInfo;
+import org.skriptlang.skript.registration.SyntaxRegistry;
 import org.skriptlang.skript.util.Priority;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -411,19 +419,19 @@ public class Registration {
         }
     }
 
-    public <T, E extends Expression<T>> ExpressionRegistrar<T,E>  newExpression(Class<E> expressionClass, Class<T> returnType, Priority priority, String... patterns) {
+    public <T, E extends Expression<T>> ExpressionRegistrar<T, E> newExpression(Class<E> expressionClass, Class<T> returnType, Priority priority, String... patterns) {
         return new ExpressionRegistrar(expressionClass, returnType, priority, patterns);
     }
 
-    public <T, E extends Expression<T>> ExpressionRegistrar<T,E>  newSimpleExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
+    public <T, E extends Expression<T>> ExpressionRegistrar<T, E> newSimpleExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
         return new ExpressionRegistrar(expressionClass, returnType, SyntaxInfo.SIMPLE, patterns);
     }
 
-    public <T, E extends Expression<T>> ExpressionRegistrar<T,E>  newEventExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
+    public <T, E extends Expression<T>> ExpressionRegistrar<T, E> newEventExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
         return new ExpressionRegistrar(expressionClass, returnType, EventValueExpression.DEFAULT_PRIORITY, patterns);
     }
 
-    public <T, E extends Expression<T>> ExpressionRegistrar<T,E> newCombinedExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
+    public <T, E extends Expression<T>> ExpressionRegistrar<T, E> newCombinedExpression(Class<E> expressionClass, Class<T> returnType, String... patterns) {
         return new ExpressionRegistrar(expressionClass, returnType, SyntaxInfo.COMBINED, patterns);
     }
 
@@ -473,6 +481,196 @@ public class Registration {
 
     public <T> FunctionRegistrar<T> newFunction(DefaultFunction<T> function) {
         return new FunctionRegistrar<T>(function);
+    }
+
+    public void registerInit() {
+        // TYPES
+        for (TypeRegistrar type : getTypes()) {
+            if (!type.isRegistered()) {
+                Util.skriptError("Type '" + type.codename + "' is not registered!");
+                continue;
+            }
+            ClassInfo<?> classInfo;
+            if (type instanceof EnumTypeRegistrar<?> enumTypeRegistrar) {
+                classInfo = enumTypeRegistrar.enumWrapper.getClassInfo(enumTypeRegistrar.codename);
+            } else if (type instanceof RegistryTypeRegistrar<? extends Keyed> registryTypeRegistrar) {
+                Class<? extends Keyed> registryClass = registryTypeRegistrar.type;
+                Registry<? extends Keyed> registry = registryTypeRegistrar.registry;
+                String codename = registryTypeRegistrar.codename;
+                String prefix = registryTypeRegistrar.prefix;
+                String suffix = registryTypeRegistrar.suffix;
+                boolean usage = registryTypeRegistrar.createUsage;
+
+                classInfo = RegistryClassInfo.create(registry, registryClass, usage, codename, prefix, suffix);
+            } else {
+                classInfo = new ClassInfo<>(type.type, type.codename);
+            }
+            if (type.user != null) {
+                classInfo.user(type.user);
+            }
+            if (type.usage != null) {
+                classInfo.usage(type.usage);
+            }
+            if (type.before != null) {
+                classInfo.before(type.before);
+            }
+            if (type.after != null) {
+                classInfo.after(type.after);
+            }
+            if (type.defaultExpression != null) {
+                classInfo.defaultExpression(type.defaultExpression);
+            }
+            if (type.supplier != null) {
+                classInfo.supplier(type.supplier);
+            }
+            if (type.parser != null) {
+                classInfo.parser(type.parser);
+            }
+            if (type.serializer != null) {
+                classInfo.serializer(type.serializer);
+            }
+            if (type.cloner != null) {
+                classInfo.cloner(type.cloner);
+            }
+            if (type.changer != null) {
+                classInfo.changer(type.changer);
+            }
+            Classes.registerClass(classInfo);
+        }
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    public void registerLoad() {
+        SyntaxRegistry syntaxInfos = this.addon.syntaxRegistry();
+
+        // STRUCTURES
+        for (Registration.StructureRegistrar<?> structure : getStructures()) {
+            if (!structure.isRegistered()) {
+                Util.skriptError("Structure '%s' is not registered!", structure.structureClass.getSimpleName());
+                continue;
+            }
+
+            DefaultSyntaxInfos.Structure<Structure> build = DefaultSyntaxInfos.Structure.builder(
+                    (Class<Structure>) structure.structureClass)
+                .addPatterns(structure.patterns)
+                .entryValidator(structure.validator)
+                .build();
+
+            syntaxInfos.register(SyntaxRegistry.STRUCTURE, build);
+        }
+        // EVENTS
+        for (Registration.EventRegistrar event : getEvents()) {
+            if (!event.isRegistered()) {
+                Util.skriptError("Event '" + event.getDocumentation().getName() + "' is not registered!");
+                continue;
+            }
+            BukkitSyntaxInfos.Event.Builder<? extends BukkitSyntaxInfos.Event.Builder<?, SkriptEvent>, SkriptEvent> builder = BukkitSyntaxInfos.Event.builder(
+                (Class<SkriptEvent>) event.skriptEventClass,
+                event.getDocumentation().getName());
+            for (Class<? extends Event> eventClass : event.eventClasses) {
+                builder.addEvent(eventClass);
+            }
+            builder.addPatterns(event.patterns);
+
+            syntaxInfos.register(BukkitSyntaxInfos.Event.KEY, builder.build());
+        }
+
+        // SECTIONS
+        for (Registration.SectionRegistrar section : getSections()) {
+            if (!section.isRegistered()) {
+                Util.skriptError("Section '" + section.section.getSimpleName() + "' is not registered!");
+                continue;
+            }
+
+            Supplier<Section> supplier = () -> {
+                try {
+                    return section.section.getConstructor().newInstance();
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+
+            syntaxInfos.register(SyntaxRegistry.SECTION, SyntaxInfo.builder((Class<Section>) section.section)
+                .supplier(supplier)
+                .addPatterns(section.patterns)
+                .build());
+        }
+
+        // EFFECTS
+        for (EffectRegistrar effect : getEffects()) {
+            if (!effect.isRegistered()) {
+                Util.skriptError("Effect '" + effect.effect.getSimpleName() + "' is not registered!");
+                continue;
+            }
+            Supplier<Effect> supplier = () -> {
+                try {
+                    return effect.effect.getConstructor().newInstance();
+                } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            syntaxInfos.register(SyntaxRegistry.EFFECT,
+                SyntaxInfo.builder((Class<Effect>) effect.effect)
+                    .supplier(supplier)
+                    .addPatterns(effect.patterns)
+                    .build()
+            );
+        }
+
+        // EXPRESSIONS
+        for (Registration.ExpressionRegistrar<?, ?> expression : getExpressions()) {
+            if (!expression.isRegistered()) {
+                Util.skriptError("Expression '%s' is not registered!", expression.expressionClass.getSimpleName());
+                continue;
+            }
+            Supplier<Expression> supplier = () -> {
+                try {
+                    return expression.expressionClass.getConstructor().newInstance();
+                } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+
+            SyntaxInfo info = SyntaxInfo.Expression.builder((Class<? extends Expression>) expression.expressionClass, expression.returnType)
+                .supplier(supplier)
+                .addPatterns(expression.patterns).build();
+
+            syntaxInfos.register(SyntaxRegistry.EXPRESSION, (SyntaxInfo.Expression<?, ?>) info);
+        }
+
+        // FUNCTIONS
+        for (FunctionRegistrar function : getFunctions()) {
+            if (!function.isRegistered()) {
+                Util.skriptError("Function '%s' is not register", function.function.name());
+            }
+
+            Functions.register(function.function);
+        }
+
+        // CONDITIONS
+        for (ConditionRegistrar condition : getConditions()) {
+            if (!condition.isRegistered()) {
+                Util.skriptError("Condition '" + condition.condition.getSimpleName() + "' is not registered!");
+                continue;
+            }
+            Supplier<Condition> supplier = () -> {
+                try {
+                    return condition.condition.getConstructor().newInstance();
+                } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            syntaxInfos.register(SyntaxRegistry.CONDITION,
+                SyntaxInfo.builder((Class<Condition>) condition.condition)
+                    .supplier(supplier)
+                    .addPatterns(condition.patterns)
+                    .build()
+            );
+        }
     }
 
 }
