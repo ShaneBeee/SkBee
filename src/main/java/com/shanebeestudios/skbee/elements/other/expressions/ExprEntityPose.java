db8ebcf727386590068cc8db698ed60fbfd52803
ShaneBee <shanebolenback@me.com>
2026-04-14 09:47:14 -0700

    ExprEntityPose - add option for fixed

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprEntityPose.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprEntityPose.java
@@ -1,8 +1,10 @@
 package com.shanebeestudios.skbee.elements.other.expressions;
 
-import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 import com.shanebeestudios.skbee.api.registration.Registration;
 import org.bukkit.entity.Entity;
@@ -14,15 +16,26 @@ import org.jetbrains.annotations.Nullable;
 public class ExprEntityPose extends SimplePropertyExpression<Entity, Pose> {
 
     public static void register(Registration reg) {
-        reg.newPropertyExpression(ExprEntityPose.class, Pose.class, "pose", "entities")
+        reg.newPropertyExpression(ExprEntityPose.class, Pose.class,
+                "[:fixed] pose", "entities")
             .name("Entity Pose")
             .description("Get/set the pose of an entity.",
                 "Note: While poses affect some things like hitboxes, they do not change the entity's state",
-                "(e.g. having sneaking pose does not guarantee `is sneaking` being true). Set requires PaperMC.")
+                "(e.g. having sneaking pose does not guarantee `is sneaking` being true).",
+                "fixed = Forces the state to remain until manually changed (this will not work on players).")
             .examples("set {_pose} to pose of player",
-                "set pose of target entity to sleeping pose")
-            .since("3.5.4")
+                "set pose of target entity to sleeping pose",
+                "set fixed pose of target entity to swimming pose")
+            .since("3.5.4", "INSERT VERSION (fixed)")
             .register();
+    }
+
+    private boolean fixed;
+
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.fixed = parseResult.hasTag("fixed");
+        return super.init(expressions, matchedPattern, isDelayed, parseResult);
     }
 
     @Override
@@ -30,23 +43,17 @@ public class ExprEntityPose extends SimplePropertyExpression<Entity, Pose> {
         return entity.getPose();
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
-        if (!Skript.methodExists(Entity.class, "setPose", Pose.class)) {
-            Skript.error("Setting an entity's pose requires PaperMC.");
-            return null;
-        }
         if (mode == ChangeMode.SET) return CollectionUtils.array(Pose.class);
         return null;
     }
 
-    @SuppressWarnings({"ConstantValue", "NullableProblems"})
     @Override
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
         if (delta != null && delta[0] instanceof Pose pose) {
             for (Entity entity : getExpr().getArray(event)) {
-                entity.setPose(pose);
+                entity.setPose(pose, this.fixed);
             }
         }
     }
@@ -58,7 +65,7 @@ public class ExprEntityPose extends SimplePropertyExpression<Entity, Pose> {
 
     @Override
     protected @NotNull String getPropertyName() {
-        return "pose";
+        return this.fixed ? "fixed pose" : "pose";
     }
 
 }
