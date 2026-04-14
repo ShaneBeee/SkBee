eba67200314dc5b668b154dd4ebce5fa0b490340
ShaneBee <shanebolenback@me.com>
2026-04-14 08:42:04 -0700

    EffChunkLoad - add option to load within locations - Ref #943

--- src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffChunkLoad.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/effects/EffChunkLoad.java
@@ -3,18 +3,22 @@ package com.shanebeestudios.skbee.elements.other.effects;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.TriggerItem;
-import ch.njol.skript.paperlib.PaperLib;
 import ch.njol.skript.variables.Variables;
 import ch.njol.util.Kleenean;
 import com.shanebeestudios.skbee.SkBee;
 import com.shanebeestudios.skbee.api.registration.Registration;
 import com.shanebeestudios.skbee.api.skript.base.Effect;
+import com.shanebeestudios.skbee.api.util.Pair;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class EffChunkLoad extends Effect {
 
@@ -29,39 +33,48 @@ public class EffChunkLoad extends Effect {
                 "[:async] load chunk with [chunk ]key %number% (in|of) [world] %world% [ticket:with ticket]",
 
                 // Location
-                "[:async] load chunk at %location% [ticket:with ticket]")
+                "[:async] load chunk at %location% [ticket:with ticket]",
+
+                // Within Locations
+                "[:async] load [all] chunks within %location% and %location% [ticket:with ticket[s]]")
             .name("Chunk - Load")
             .description("Load a chunk.",
                 "**Options**:",
                 "- `%number%,[ ]%number%` = Represents the X/Z coords of a chunk. Not to be confused with a location. " +
                     "Chunk coords are essentially a location divided by 16, example: Chunk 1/1 = Location 16/16",
-                "- `async` = Will load the chunk off the main thread. Your code will halt whilst waiting for the chunk to load.",
+                " - `chunk key %number%` = Represents the key of a chunk (chunk's chunk coordinates packed into a long).",
+                "- `async` = Will load the chunk off the main thread. Your code will halt whilst waiting for the chunk[s] to load.",
                 "- `with ticket` = Will add a ticket to the chunk, preventing it from unloading until you explicitly unload it or the server stops.")
             .examples("load chunk at 1,1 in world \"world\"",
                 "load chunk at location(1,1,1, world \"world\")",
                 "load chunk at 150,150 in world \"world\"",
                 "load chunk at 150,150 in world \"world\" with ticket",
+                "load chunks within {_l1} and {_l2}",
+                "load chunks within {_l1} and {_l2} with tickets",
                 "async load chunk at {_loc}",
                 "async load chunk at 100,100 in world \"world\"",
-                "async load chunk at 1,1 in world of player with ticket")
+                "async load chunk at 1,1 in world of player with ticket",
+                "async load chunks within {_l1} and {_l2} with tickets")
             .since("INSERT VERSION")
             .register();
     }
 
     private int pattern;
-    private boolean ticket;
+    private boolean hasTicket;
     private boolean isAsync;
     private Expression<Number> x;
     private Expression<Number> z;
     private Expression<Number> key;
     private Expression<World> world;
     private Expression<Location> location;
+    private Expression<Location> locationOne;
+    private Expression<Location> locationTwo;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int pattern, Kleenean kleenean, ParseResult parseResult) {
         this.pattern = pattern;
-        this.ticket = parseResult.hasTag("ticket");
+        this.hasTicket = parseResult.hasTag("ticket");
         this.isAsync = parseResult.hasTag("async");
 
         if (pattern == 0) {
@@ -73,17 +86,49 @@ public class EffChunkLoad extends Effect {
             this.world = (Expression<World>) exprs[2];
         } else if (pattern == 2) {
             this.location = (Expression<Location>) exprs[0];
+        } else if (pattern == 3) {
+            this.locationOne = (Expression<Location>) exprs[0];
+            this.locationTwo = (Expression<Location>) exprs[1];
         }
         return true;
     }
 
     @Override
     protected void execute(Event event) {
+        // Walk instead
     }
 
     @Override
     protected @Nullable TriggerItem walk(Event event) {
         TriggerItem next = getNext();
+
+        if (this.locationOne != null) {
+            Queue<Pair<Integer, Integer>> posQueue = new LinkedList<>();
+            Location l1 = this.locationOne.getSingle(event);
+            Location l2 = this.locationTwo.getSingle(event);
+            if (l1 == null || l2 == null) {
+                return next;
+            }
+            World world = l1.getWorld();
+
+            int minX = Math.min(l1.getBlockX() >> 4, l2.getBlockX() >> 4);
+            int minZ = Math.min(l1.getBlockZ() >> 4, l2.getBlockZ() >> 4);
+            int maxX = Math.max(l1.getBlockX() >> 4, l2.getBlockX() >> 4);
+            int maxZ = Math.max(l1.getBlockZ() >> 4, l2.getBlockZ() >> 4);
+            for (int x = minX; x <= maxX; x++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    posQueue.add(new Pair<>(x, z));
+                }
+            }
+
+            // Let's save you guys for later after the chunk has loaded
+            Object localVars = Variables.removeLocals(event);
+
+            // Let's start loading chunks
+            loadChunkInQueue(posQueue, world, event, next, localVars);
+
+            return null;
+        }
 
         int x = 0;
         int z = 0;
@@ -123,30 +168,41 @@ public class EffChunkLoad extends Effect {
             }
         }
 
-        if (this.isAsync) {
-            // Let's save you guys for later after the chunk has loaded
-            Object localVars = Variables.removeLocals(event);
+        Queue<Pair<Integer, Integer>> posQueue = new LinkedList<>();
+        posQueue.add(new Pair<>(x, z));
 
-            // If running paper, get chunk async, otherwise will run sync
-            PaperLib.getChunkAtAsync(world, x, z).thenAccept(chunk -> {
-                if (this.ticket) chunk.addPluginChunkTicket(PLUGIN);
+        // Let's save you guys for later after the chunk has loaded
+        Object localVars = Variables.removeLocals(event);
+        loadChunkInQueue(posQueue, world, event, next, localVars);
 
-                // re-set local variables
-                if (localVars != null) Variables.setLocalVariables(event, localVars);
+        return null;
+    }
 
-                // walk next trigger
-                if (next != null) TriggerItem.walk(next, event);
+    private void loadChunkInQueue(Queue<Pair<Integer, Integer>> posQueue, World world, Event event, TriggerItem next, Object localVars) {
+        if (posQueue.isEmpty()) {
+            // re-set local variables
+            if (localVars != null) Variables.setLocalVariables(event, localVars);
 
-                // remove local vars as we're now done
-                Variables.removeLocals(event);
-            });
-            return null;
+            // walk next trigger
+            if (next != null) TriggerItem.walk(next, event);
+
+            // remove local vars as we're now done
+            Variables.removeLocals(event);
         } else {
-            world.getChunkAt(x, z);
-            if (this.ticket) world.addPluginChunkTicket(x, z, PLUGIN);
+            Pair<Integer, Integer> pos = posQueue.poll();
+            int x = pos.first();
+            int z = pos.second();
+            if (this.isAsync) {
+                world.getChunkAtAsync(x, z).thenAccept(chunk -> {
+                    if (this.hasTicket) chunk.addPluginChunkTicket(PLUGIN);
+                    loadChunkInQueue(posQueue, world, event, next, localVars);
+                });
+            } else {
+                Chunk chunk = world.getChunkAt(x, z);
+                if (this.hasTicket) chunk.addPluginChunkTicket(PLUGIN);
+                loadChunkInQueue(posQueue, world, event, next, localVars);
+            }
         }
-
-        return next;
     }
 
     @Override
@@ -154,11 +210,12 @@ public class EffChunkLoad extends Effect {
         String chunk = switch (this.pattern) {
             case 1 -> "with key " + this.key.toString(e, d) + " in world " + this.world.toString(e, d);
             case 2 -> "at " + this.location.toString(e, d);
+            case 3 -> "within " + this.locationOne.toString(e, d) + " and " + this.locationTwo.toString(e, d);
             default -> "at " + this.x.toString(e, d) + ","
                 + this.z.toString(e, d) + " in world " + this.world.toString(e, d);
         };
         String async = this.isAsync ? "async" : "";
-        String ticket = this.ticket ? " with ticket" : "";
+        String ticket = this.hasTicket ? " with ticket" : "";
         return String.format("%s load chunk %s %s", async, chunk, ticket);
     }
 
