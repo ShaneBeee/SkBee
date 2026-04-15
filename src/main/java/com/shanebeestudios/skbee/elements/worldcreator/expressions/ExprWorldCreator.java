aed3296e574dc1b55d9fb88accc9ba2a1e9bec85
ShaneBeee <shanebolenback@me.com>
2026-04-15 16:30:41 -0700

    ExprWorldCreator - update docs and add usage of strings for keys

--- src/main/java/com/shanebeestudios/skbee/elements/worldcreator/expressions/ExprWorldCreator.java
+++ src/main/java/com/shanebeestudios/skbee/elements/worldcreator/expressions/ExprWorldCreator.java
@@ -19,24 +19,29 @@ public class ExprWorldCreator extends SimpleExpression<BeeWorldCreator> {
     public static void register(Registration reg) {
         reg.newSimpleExpression(ExprWorldCreator.class, BeeWorldCreator.class,
                 "[a] [new] world creator named:(with name|named) %string%",
-                "[a] [new] world creator with key %namespacedkey%",
+                "[a] [new] world creator with key %namespacedkey/string%",
                 "[a] [new] world creator (with name|named) %string% to (copy|:clone) %world% [no-save:without saving]",
-                "[a] [new] world creator with key %namespacedkey% to (copy|:clone) %world% [no-save:without saving]")
+                "[a] [new] world creator with key %namespacedkey/string% to (copy|:clone) %world% [no-save:without saving]")
             .name("World Creator")
             .description("Create a new world creator. This will be used to create a new world.",
-                "Name will be the name of your new world.",
-                "Key will be how your world is represented to Minecraft. Be default this will use the Minecraft namespace. (Keys require PaperMC.)",
-                "You can not use the name of one of the default worlds, or a world created by another plugin, such as MultiVerse.",
-                "Copy will create a fresh new world with the same seed and settings. Clone will create a carbon copy of your world, if the world",
-                "is large, this process may take a while. The file copying process will happen on another thread, so this won't freeze your server.",
-                "You can optionally add `without saving` to prevent saving of the world that will be cloned. Saving freezes the server for a bit",
-                "so this can help speed up the process... if you don't need that world saved at this moment in time.",
-                "After creating a world creator you will need to load the world.")
-            .examples("set {_w} to a new world creator named \"my-world\"",
+                "After creating a world creator you will need to load the world.",
+                "",
+                "**Name/Key**:",
+                " - Name: This is the name of your new world (As of Minecraft 26.1, names are discouraged and keys are suggested.",
+                " - Key: This is the key of your new world, and is used to identify it.",
+                "   - As of Minecraft 26.1, the key will determine where your world folder is saved within the `world/dimensions` folder.",
+                "",
+                "**Other**:",
+                " - Copy will create a fresh new world with the same seed and settings.",
+                " - Clone will create a carbon copy of your world, if the world is large, this process may take a while. " +
+                    "The file copying process will happen on another thread, so this won't freeze your server.",
+                " - `without saving` = Optionally prevent saving of the world that will be cloned. Saving freezes the server for a bit " +
+                    "so this can help speed up the process... if you don't need that world saved at this moment in time.")
+            .examples("set {_w} to a new world creator with key \"my_stuff:my_world\"",
                 "set environment of {_w} to nether",
                 "load world from creator {_w}",
                 "",
-                "set {_clone} to a new world creator named \"world_clone\" to clone world \"world\" without saving",
+                "set {_clone} to a new world creator with key \"clones:world\" to clone world \"world\" without saving",
                 "load world from creator {_clone}")
             .since("1.8.0")
             .register();
@@ -44,7 +49,7 @@ public class ExprWorldCreator extends SimpleExpression<BeeWorldCreator> {
 
     private int pattern;
     private Expression<String> name;
-    private Expression<NamespacedKey> key;
+    private Expression<?> key;
     private Expression<World> world;
     private boolean clone;
     private boolean save;
@@ -59,7 +64,7 @@ public class ExprWorldCreator extends SimpleExpression<BeeWorldCreator> {
                 Skript.warning("Named worlds are highly discouraged in Minecraft 26.1+ and you should be using a key instead.");
             }
         } else {
-            this.key = (Expression<NamespacedKey>) exprs[0];
+            this.key = exprs[0];
         }
         this.world = this.pattern > 1 ? (Expression<World>) exprs[1] : null;
         this.clone = this.pattern > 1 && parseResult.hasTag("clone");
@@ -73,7 +78,14 @@ public class ExprWorldCreator extends SimpleExpression<BeeWorldCreator> {
         NamespacedKey key = null;
         World world = null;
         if (this.name != null) name = this.name.getSingle(e);
-        if (this.key != null) key = this.key.getSingle(e);
+        if (this.key != null) {
+            Object o = this.key.getSingle(e);
+            if (o instanceof NamespacedKey) {
+                key = (NamespacedKey) o;
+            } else if (o instanceof String s) {
+                key = Util.getNamespacedKey(s, false);
+            }
+        }
         if (this.world != null) world = this.world.getSingle(e);
 
         if (name == null && key == null) {
