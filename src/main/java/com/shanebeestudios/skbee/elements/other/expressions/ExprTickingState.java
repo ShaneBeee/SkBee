4f54de424346bfe6a0463ebb5811931daf0f4be0
ShaneBeee <shanebolenback@me.com>
2026-04-28 08:58:49 -0700

    EntityTickingState - cleanup

--- src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprTickingState.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/expressions/ExprTickingState.java
@@ -1,6 +1,5 @@
 package com.shanebeestudios.skbee.elements.other.expressions;
 
-import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
@@ -13,13 +12,11 @@ import org.jetbrains.annotations.Nullable;
 
 public class ExprTickingState extends SimplePropertyExpression<Entity, Boolean> {
 
-    private static final boolean ARMOR_STAND_HAS_TICKING = Skript.methodExists(ArmorStand.class, "canTick");
-
     public static void register(Registration reg) {
         reg.newPropertyExpression(ExprTickingState.class, Boolean.class, "tick[ing] state", "entities")
             .name("Entity Ticking State")
             .description("Represents whether or not an entity will tick.",
-                "Currently this only works for ArmorStands and requies a PaperMC server.")
+                "Currently this only works for ArmorStands.")
             .examples("spawn an armor stand at player:",
                 "\tset ticking state of entity to false")
             .since("2.13.0")
@@ -28,25 +25,23 @@ public class ExprTickingState extends SimplePropertyExpression<Entity, Boolean> 
 
     @Override
     public @Nullable Boolean convert(Entity entity) {
-        if (entity instanceof ArmorStand armorStand && ARMOR_STAND_HAS_TICKING) {
+        if (entity instanceof ArmorStand armorStand) {
             return armorStand.canTick();
         }
         return null;
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
         if (mode == ChangeMode.SET) return CollectionUtils.array(Boolean.class);
         return null;
     }
 
-    @SuppressWarnings({"ConstantValue", "NullableProblems"})
     @Override
     public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
         if (delta != null && delta[0] instanceof Boolean canTick) {
             for (Entity entity : getExpr().getArray(e)) {
-                if (entity instanceof ArmorStand armorStand && ARMOR_STAND_HAS_TICKING) {
+                if (entity instanceof ArmorStand armorStand) {
                     armorStand.setCanTick(canTick);
                 }
             }
