2cd8bdf530120fe0cd5c7cfcc1e6f2bdabcb149d
ShaneBeee <shanebolenback@me.com>
2026-05-04 16:00:29 -0700

    build.gradle.kts - update SKR

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     compileOnly("com.github.SkriptLang:Skript:2.15.0")
 
     // SkriptRegistration
-    implementation("com.github.ShaneBeee:SkriptRegistration:1.1.0")
+    implementation("com.github.ShaneBeee:SkriptRegistration:1.2.0")
 
     // commons-io
     compileOnly("commons-io:commons-io:2.14.0")
