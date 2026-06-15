13124cc301a53e19392888a1dae6d9657d0b2b26
ShaneBee <shanebolenback@me.com>
2026-06-15 06:45:40 -0700

    CondPlayerIsTransferred - add support for connection

--- src/main/java/com/shanebeestudios/skbee/elements/other/conditions/CondPlayerIsTransferred.java
+++ src/main/java/com/shanebeestudios/skbee/elements/other/conditions/CondPlayerIsTransferred.java
@@ -1,14 +1,24 @@
 package com.shanebeestudios.skbee.elements.other.conditions;
 
-import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
 import com.github.shanebeee.skr.Registration;
+import com.google.common.base.Predicate;
+import com.shanebeestudios.skbee.api.skript.base.Condition;
+import io.papermc.paper.connection.PlayerConnection;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.jetbrains.annotations.Nullable;
 
-public class CondPlayerIsTransferred extends PropertyCondition<Player> {
+@SuppressWarnings("UnstableApiUsage")
+public class CondPlayerIsTransferred extends Condition {
 
     public static void register(Registration reg) {
-        reg.newPropertyCondition(CondPlayerIsTransferred.class, "transferred", "players")
+        reg.newCondition(CondPlayerIsTransferred.class,
+                "%players/playerconnections% (is|are) transferred",
+                "%players/playerconnections% (is|are)(n't| not) transferred")
             .name("Is Transferred")
             .description("Check if a player transferred servers.")
             .examples("on join:",
@@ -19,14 +29,38 @@ public class CondPlayerIsTransferred extends PropertyCondition<Player> {
 
     }
 
+    private Expression<?> players;
+
     @Override
-    public boolean check(Player player) {
-        return player.isTransferred();
+    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+        this.players = expressions[0];
+        setNegated(matchedPattern == 1);
+        return true;
     }
 
     @Override
-    protected @NotNull String getPropertyName() {
-        return "transferred";
+    public boolean check(Event event) {
+        if (event instanceof AsyncPlayerPreLoginEvent preLoginEvent) {
+            if (isNegated()) {
+                return !preLoginEvent.isTransferred();
+            } else {
+                return preLoginEvent.isTransferred();
+            }
+        }
+        return this.players.check(event, (Predicate<Object>) input -> {
+            if (input instanceof Player player) {
+                return player.isTransferred();
+            } else if (input instanceof PlayerConnection playerConnection) {
+                return playerConnection.isTransferred();
+            }
+            return false;
+        }, isNegated());
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        String neg = isNegated() ? " is not " : " is ";
+        return this.players.toString(event,debug) + neg + "transferred";
     }
 
 }
